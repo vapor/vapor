5850acdfcc15ad61520a34e5cec87d65e00cf3be
Tanner Nelson <tanner@bluebite.com>
2016-01-18 22:08:33 -0500

    updates

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Vapor",
-    dependencies: [
-        .Package(url: "https://github.com/tannernelson/swifter.git", majorVersion: 1),
-    ]
+    name: "Vapor"
 )
-
