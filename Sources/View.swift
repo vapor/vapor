df5dcc4ece6fde572b42a678f0326d6de51a5a2f
Tanner Nelson <tanner@bluebite.com>
2016-01-18 22:37:05 -0500

    updates

--- Sources/View.swift
+++ Sources/View.swift
@@ -1,18 +1,23 @@
 public class View {
 
 	public let resourceDir = "Resources"
+	let bytes: [UInt8]
 
-	public init(path: String) -> HttpResponse {
+	public init(path: String) {
         let filesPath = View.resourceDir + "/" + path
         
         guard let fileBody = NSData(contentsOfFile: filesPath) else {
             return HttpResponse.NotFound
         }
-        
+      
 		//TODO: Implement range
         var array = [UInt8](count: fileBody.length, repeatedValue: 0)
         fileBody.getBytes(&array, length: fileBody.length)
-        return HttpResponse.RAW(200, "OK", nil, { $0.write(array) })
+        self.bytes = array
+	}
+
+	public func render() {
+        return HttpResponse.RAW(200, "OK", nil, { $0.write(self.bytes) })
 	}
 
 }
