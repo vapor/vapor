9b4446fb5d2d4c6de2b7c9e0aac18c5f3dad2e0e
Tanner Nelson <tanner@bluebite.com>
2016-01-18 20:51:03 -0500

    updates

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,19 +4,15 @@ import Swifter
 let server = HttpServer()
 
 server["/"] = { request in
-	return .MovedPermanently("http://tanner.xyz/index.html")
+	return .OK(.Html("welcome"))
 }
-server["/images/:path"] = HttpHandlers.directory("/home/tanner/website/images")
-server["/scripts/:path"] = HttpHandlers.directory("/home/tanner/website/scripts")
-server["/styles/:path"] = HttpHandlers.directory("/home/tanner/website/styles")
-server["/:path"] = HttpHandlers.directory("/home/tanner/website")
 
 server["/heartbeat"] = { request in 
     return .OK(.Html("{lub:dub}"))
 }
 
 do {
-	let port: in_port_t = 80
+	let port: in_port_t = 8080
 	try server.start(port)
 	print("Server has started on port \(port)")
 
