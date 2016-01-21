760b8cc0539059ba2fcd1bb7780d322d1ef049d1
Tanner Nelson <tanner@bluebite.com>
2016-01-20 23:35:56 -0500

    1.0.5

--- Sources/Server.swift
+++ Sources/Server.swift
@@ -32,7 +32,7 @@ public class Server: SocketServer {
         }
     }
 
-    public func run(port: Int = 80) {
+    public func run(port port: Int = 80) {
         self.parseRoutes()
 
         do {
