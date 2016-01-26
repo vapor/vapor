111016f43bdde56b35a5b45372af3022f072c349
Tanner Nelson <tanner@bluebite.com>
2016-01-25 22:49:03 -0500

    response convertible

---
+++ Sources/ResponseConvertible.swift
@@ -0,0 +1,23 @@
+//
+// Based on comment from ElvishJerricco <https://www.reddit.com/r/swift/comments/42n46u/a_laravellumen_inspired_web_framework_for_swift/czc3nw8>
+//
+
+public protocol ResponseConvertible {
+	func response() -> Response
+}
+
+extension String: ResponseConvertible {
+	public func response() -> Response {
+		return Response(status: .OK, text: self)
+	}
+}
+
+extension Dictionary: ResponseConvertible {
+	public func response() -> Response {
+		do {
+            return try Response(status: .OK, jsonObject: (self as! AnyObject))    
+        } catch {
+            return Response(error: "JSON serialization error: \(error)")
+        }
+	}
+}
