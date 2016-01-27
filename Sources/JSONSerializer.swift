2db13f862a535bb27e146384bde51d13040ac973
Tanner Nelson <tanner@bluebite.com>
2016-01-26 23:20:27 -0500

    update json serializer

--- Sources/JSONSerializer.swift
+++ Sources/JSONSerializer.swift
@@ -2,58 +2,47 @@ import Foundation
 
 class JSONSerializer {
 
+    class func serializeDict(dict: [String: Any]) -> String {
+        var s = "{"
+        var i = 0
+
+        for (key, val) in dict {
+            s += "\"\(key)\":\(self.serialize(val))"
+            if i != (dict.count - 1) {
+                s += ","
+            }
+            i += 1
+        }
+
+        return s + "}"
+    }
+
+    class func serializeArr(arr: [Any]) -> String {
+        var s = "["
+
+        for i in 0 ..< arr.count {
+            s += self.serialize(arr[i])
+
+            if i != (arr.count - 1) {
+                s += ","
+            }
+        }
+
+        return s + "]"
+    }
+
     class func serialize(object: Any) -> String {
 
         if let dict = object as? [String: Any] {
-            var s = "{"
-            var i = 0
-
-            for (key, val) in dict {
-                s += "\"\(key)\":\(self.serialize(val))"
-                if i != (dict.count - 1) {
-                    s += ","
-                }
-                i += 1
-            }
-
-            return s + "}"
-        } else if let dict = object as? NSDictionary {
-            var s = "{"
-            var i = 0
-
-            for (key, val) in dict {
-                s += "\"\(key)\":\(self.serialize(val))"
-                if i != (dict.count - 1) {
-                    s += ","
-                }
-                i += 1
-            }
-
-            return s + "}"
+            return self.serializeDict(dict)
+        } else if let dict = object as? [String: String] {
+            return self.serializeDict(dict)
         } else if let array = object as? [Any] {
-            var s = "["
-
-            for i in 0 ..< array.count {
-                s += self.serialize(array[i])
-
-                if i != (array.count - 1) {
-                    s += ","
-                }
-            }
-
-            return s + "]"
-        } else if let array = object as? NSArray {
-            var s = "["
-
-            for i in 0 ..< array.count {
-                s += self.serialize(array[i])
-
-                if i != (array.count - 1) {
-                    s += ","
-                }
-            }
-
-            return s + "]"
+            return self.serializeArr(array)
+        } else if let array = object as? [String] {
+            return self.serializeArr(array)
+        } else if let array = object as? [Int] {
+            return self.serializeArr(array)
         } else if let string = object as? String {
             return "\"\(string)\""
         } else if let number = object as? Int {
@@ -61,7 +50,7 @@ class JSONSerializer {
         } else {
             print(object)
             print(Mirror(reflecting: object))
-            return ""
+            return "\"\""
         }
 
     }
