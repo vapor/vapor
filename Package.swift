449a8f1952bfd2942ca32f63d2d80ff297d18b71
Tanner Nelson <tanner@bluebite.com>
2016-02-03 18:02:33 -0500

    add fluent as commented dependency

--- Package.swift
+++ Package.swift
@@ -2,8 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Vapor",
-    dependencies: [ 
-        //.Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0) //testing
-        //.Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0)
+    dependencies: [
+        //.Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
     ]
 )
