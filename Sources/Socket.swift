d0b57c0c1bf4229cacd74eb04a3f03880bd6ea6c
Tanner Nelson <me@tanner.xyz>
2016-02-05 17:58:43 -0500

    htons compile fix

--- Sources/Socket.swift
+++ Sources/Socket.swift
@@ -203,7 +203,7 @@ public class Socket: Hashable, Equatable {
     
     private class func htonsPort(port: in_port_t) -> in_port_t {
         #if os(Linux)
-            return htons(port)
+            return port.bigEndian
         #else
             let isLittleEndian = Int(OSHostByteOrder()) == OSLittleEndian
             return isLittleEndian ? _OSSwapInt16(port) : port
