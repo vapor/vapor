f6cad1b3b64aabde53b2dc3fe96116790ec8cf7d
Tanner Nelson <tanner@bluebite.com>
2016-02-05 17:48:23 -0500

    port fix

--- Sources/Socket.swift
+++ Sources/Socket.swift
@@ -202,13 +202,9 @@ public class Socket: Hashable, Equatable {
     }
     
     private class func htonsPort(port: in_port_t) -> in_port_t {
-        #if os(Linux)
-            let fixedPort = htons(port)
-            return fixedPort
-        #else
+
             let isLittleEndian = Int(OSHostByteOrder()) == OSLittleEndian
             return isLittleEndian ? _OSSwapInt16(port) : port
-        #endif
     }
 }
 
