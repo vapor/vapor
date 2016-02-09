69968ea1d0a145af8d704e3a218c56716b30e3e3
Tanner Nelson <tanner@bluebite.com>
2016-02-08 20:12:14 -0500

    remove stencil dep

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Vapor",
     dependencies: [
         //.Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
-        .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0)
+        //.Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0)
     ]
 )
