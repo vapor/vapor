43e429f991ea42132b3d11a6f331237df0b835a0
Art Kay <art@artkay.nyc>
2016-02-17 20:01:55 -0500

    add hostname to RouterDriver

--- Sources/RouterDriver.swift
+++ Sources/RouterDriver.swift
@@ -3,6 +3,6 @@ import Foundation
 public protocol RouterDriver {
     
     func route(request: Request) -> (Request -> Response)?
-    func register(method: Request.Method, path: String, handler: (Request -> Response))
+    func register(hostname hostname: String?, method: Request.Method, path: String, handler: (Request -> Response))
     
 }
