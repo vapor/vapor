7e329e6cdb2e9c64e60babae6809bf4b470077af
Art Kay <art@artkay.nyc>
2016-02-17 19:08:18 -0500

    routing based on hostname

--- Sources/Route.swift
+++ Sources/Route.swift
@@ -1,40 +1,51 @@
 public class Route {
 
 	static var routes: [Route] = []
+    static var hostname: String?
 
 	public typealias Closure = ((request: Request) throws -> ResponseConvertible) 
 
 	let method: Request.Method
 	let path: String
 	let closure: Closure
+    var hostname: String?
 
 	init(method: Request.Method, path: String, closure: Closure) {
 		self.method = method
 		self.path = path
 		self.closure = closure
-
+        
 		Route.routes.append(self)
 	}
+    
+    public class func add(method method: Request.Method, path: String, closure: Closure) {
+        let route = Route(method: method, path: path, closure: closure)
+        route.hostname = self.hostname
+    }
 
 	public class func get(path: String, closure: Closure) {
-		let _ = Route(method: .Get, path: path, closure: closure)
+        self.add(method: .Get, path: path, closure: closure)
 	}
 
 	public class func post(path: String, closure: Closure) {
-		let _ = Route(method: .Post, path: path, closure: closure)
+		self.add(method: .Post, path: path, closure: closure)
 	}
 
 	public class func put(path: String, closure: Closure) {
-		let _ = Route(method: .Put, path: path, closure: closure)
+		self.add(method: .Put, path: path, closure: closure)
 	}
 
 	public class func patch(path: String, closure: Closure) {
-		let _ = Route(method: .Patch, path: path, closure: closure)
+		self.add(method: .Patch, path: path, closure: closure)
 	}
 
 	public class func delete(path: String, closure: Closure) {
-		let _ = Route(method: .Delete, path: path, closure: closure)
+		self.add(method: .Delete, path: path, closure: closure)
 	}
+    
+    public class func options(path: String, closure: Closure) {
+        self.add(method: .Options, path: path, closure: closure)
+    }
 
 	public class func any(path: String, closure: Closure) {
 		self.get(path, closure: closure)
@@ -53,5 +64,17 @@ public class Route {
 		self.put("\(path)/:id", closure: controller.update)
 		self.delete("\(path)/:id", closure: controller.destroy)
 	}
+    
+    public class func host(hostname: String, closure: () -> ()) {
+        self.hostname = hostname
+        closure()
+        self.hostname = nil
+    }
 
+}
+
+extension Route: CustomStringConvertible {
+    public var description: String {
+        return "\(self.method) \(self.path) \(self.hostname)"
+    }
 }
