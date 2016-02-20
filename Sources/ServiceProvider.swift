7282f012a63cbfe1e0074b07ef134c5ae640e564
shnhrrsn <shnhrrsn@gmail.com>
2016-02-20 11:30:02 -0500

    Added service providers

---
+++ Sources/ServiceProvider.swift
@@ -0,0 +1,16 @@
+public class ServiceProvider {
+	public let application: Application
+
+	public required init(application: Application) {
+		self.application = application
+	}
+
+	public func register() {
+
+	}
+
+	public func boot() {
+
+	}
+
+}
