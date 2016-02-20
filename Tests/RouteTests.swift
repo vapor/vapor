8b22104c358f104a3dd81eede77892143aa46519
Matthew Clarkson <matt@hilenium.com>
2016-02-20 21:29:26 +1000

    nested resources working

--- Tests/RouteTests.swift
+++ Tests/RouteTests.swift
@@ -11,29 +11,4 @@ import XCTest
 
 class RouteTests: XCTestCase {
 
-    class DummyController: Controller {}
-    
-    let controller = DummyController()
-    let router = NodeRouter()
-
-    func testSinglePath() {
-        
-        let path = "foo"
-        
-        let url = "http://tanner.xyz"
-        
-        let redirect = Redirect(to: url)
-        XCTAssert(redirect.redirectLocation == url, "redirect location should be url")
-        
-        
-        var found = false
-        for (key, val) in redirect.headers {
-            if key == "Location" && val == url {
-                found = true
-            }
-        }
-        XCTAssert(found, "Location header should be in headers")
-    }
-
-    
 }
