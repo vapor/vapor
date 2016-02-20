561c53fc15903c47a8af6954d15428f8e8052fb7
shnhrrsn <shnhrrsn@gmail.com>
2016-02-20 11:30:02 -0500

    Added service providers

--- Sources/Application.swift
+++ Sources/Application.swift
@@ -6,6 +6,7 @@ public class Application {
 	public static let VERSION = "0.1.9"
 	private static var instance: Application?
 
+	private var serviceProviders = Array<ServiceProvider>()
 	public private(set) var booted = false
 	public let server: Server
 
@@ -32,12 +33,51 @@ public class Application {
 		self.instance = instance
 	}
 
+	public func register(providers: [ServiceProvider.Type]) {
+		for provider in providers {
+			self.register(provider)
+		}
+	}
+
+	public func register<T: ServiceProvider>(provider: T.Type) -> T {
+		if let registered = self.getProvider(provider) {
+			return registered
+		}
+
+		let provider = provider.init(application: self)
+		provider.register()
+
+		if self.booted {
+			self.bootProvider(provider)
+		}
+
+		return provider
+	}
+
+	public func getProvider<T: ServiceProvider>(provider: T.Type) -> T? {
+		for value in self.serviceProviders {
+			if value.dynamicType == provider {
+				return value as? T
+			}
+		}
+
+		return nil
+	}
+
 	public func boot() {
 		if self.booted {
 			return
 		}
 
+		for provider in self.serviceProviders {
+			self.bootProvider(provider)
+		}
+
 		self.booted = true
+	}
+
+	public func bootProvider(provider: ServiceProvider) {
+		provider.boot()
 	}
 
 }
