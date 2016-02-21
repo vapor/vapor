658d68348c711f1ee027a3fbafdc74295596d75c
Tanner Nelson <tanner@bluebite.com>
2016-02-21 17:26:11 -0500

    naming

--- Package.swift
+++ Package.swift
@@ -3,7 +3,5 @@ import PackageDescription
 let package = Package(
     name: "Vapor",
     dependencies: [
-        //.Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
-        //.Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0)
     ]
 )
