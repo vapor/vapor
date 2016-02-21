3258375dbd519654d97fd7eca04f6ecf36568a04
Matthew Clarkson <matt@hilenium.com>
2016-02-22 09:15:13 +1000

    enable and disable levels

--- Sources/Log.swift
+++ Sources/Log.swift
@@ -10,10 +10,13 @@ import Foundation
 
 public enum LogLevel {
     case Verbose, Debug, Info, Warning, Error, Fatal, Custom(String)
+   
+    public static var all: [LogLevel] {
+        return [.Verbose, .Debug, .Info, .Warning, .Error, .Fatal]
+    }
 }
 
 extension LogLevel: CustomStringConvertible {
-    
     public var description: String {
         switch self {
         case Verbose: return "VERBOSE"
@@ -27,34 +30,54 @@ extension LogLevel: CustomStringConvertible {
     }
 }
 
+extension LogLevel: Equatable {}
+
+public func ==(lhs: LogLevel, rhs: LogLevel) -> Bool {
+    return lhs.description == rhs.description
+}
+
+
 /* Log class
 */
 public class Log {
     
     public static var driver: Logger = ConsoleLogger()
+    public static var enabledLevels: [LogLevel] = LogLevel.all
     
     public static func verbose(message: String) {
-       driver.log(.Verbose, message: message)
+        if Log.enabledLevels.contains(.Verbose) {
+            driver.log(.Verbose, message: message)
+        }
     }
     
     public static func debug(message: String) {
-        driver.log(.Debug, message: message)
+        if Log.enabledLevels.contains(.Debug) {
+            driver.log(.Debug, message: message)
+        }
     }
     
     public static func info(message: String) {
-        driver.log(.Info, message: message)
+        if Log.enabledLevels.contains(.Info) {
+            driver.log(.Info, message: message)
+        }
     }
     
     public static func warning(message: String) {
-        driver.log(.Warning, message: message)
+        if Log.enabledLevels.contains(.Warning) {
+             driver.log(.Warning, message: message)
+        }
     }
     
     public static func error(message: String) {
-        driver.log(.Error, message: message)
+        if Log.enabledLevels.contains(.Error) {
+            driver.log(.Error, message: message)
+        }
     }
     
     public static func fatal(message: String) {
-        driver.log(.Fatal, message: message)
+        if Log.enabledLevels.contains(.Fatal) {
+            driver.log(.Fatal, message: message)
+        }
     }
     
     public static func custom(message: String, label: String) {
