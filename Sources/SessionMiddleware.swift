c8f4cb1e63873a8ca85c7c8b49932f59ddfd186a
shnhrrsn <shnhrrsn@gmail.com>
2016-02-22 00:48:36 -0500

    Updated SessionMiddleware to conform to updated Middleware protocol

--- Sources/SessionMiddleware.swift
+++ Sources/SessionMiddleware.swift
@@ -1,15 +1,15 @@
-import Foundation
-
 class SessionMiddleware: Middleware {
-    class func handle(handler: Request.Handler) -> Request.Handler {
+
+    static func handle(handler: Request.Handler) -> Request.Handler {
         return { request in
             Session.start(request)
-            
+
             let response = try handler(request: request)
-            
+
             Session.close(request: request, response: response)
-            
+
             return response
         }
     }
+
 }
