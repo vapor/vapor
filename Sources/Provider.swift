a42c608733a41a53ae97c495d86a5d7fa1bc87a4
shnhrrsn <shnhrrsn@gmail.com>
2016-02-21 22:49:03 -0500

    Refactored providers to use a single boot(application:) class method instead of instantiating them

--- Sources/Provider.swift
+++ Sources/Provider.swift
@@ -1,15 +1,6 @@
 public class Provider {
-	public let application: Application
 
-	public required init(application: Application) {
-		self.application = application
-	}
-
-	public func register() {
-
-	}
-
-	public func boot() {
+	public class func boot(application: Application) {
 
 	}
 
