55d0da1f6d11062564ba887303a243b0f3493872
Tanner Nelson <tanner@bluebite.com>
2016-02-23 18:43:50 -0500

    fix generator

--- Sources/BranchRouter.swift
+++ Sources/BranchRouter.swift
@@ -53,8 +53,25 @@ public final class BranchRouter: RouterDriver {
     }
 }
 
+/**
+ *  Until Swift api is stable for AnyGenerator, using this in interim to allow compiling Swift 2 and 2.2+
+ */
+public struct CompatibilityGenerator<T>: GeneratorType {
+    public typealias Element = T
+    
+    private let closure: () -> T?
+    
+    init(closure: () -> T?) {
+        self.closure = closure
+    }
+    
+    public func next() -> Element? {
+        return closure()
+    }
+}
+
 extension String {
-    private func pathComponentGenerator() -> AnyGenerator<String> {
+    private func pathComponentGenerator() -> CompatibilityGenerator<String> {
         
         let components = self.characters.split { character in
             //split on slashes
@@ -64,15 +81,13 @@ extension String {
             return String(item)
         }
         
-        var counter = 0
-        return AnyGenerator<String> {
-            guard counter < components.count else {
+        var idx = 0
+        return CompatibilityGenerator<String> {
+            guard idx < components.count else {
                 return nil
             }
-            
-            let next = components[counter]
-            counter += 1
-            
+            let next = components[idx]
+            idx += 1
             return next
         }
     }
