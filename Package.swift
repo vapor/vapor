4273aa8208c97a982d7f051a296b5ae8270cba85
Tanner Nelson <tanner@bluebite.com>
2016-02-22 19:10:45 -0500

    adding cryptoswift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Vapor",
     dependencies: [
-        .Package(url: "https://github.com/gfx/Swift-PureJsonSerializer.git", majorVersion: 1)
+        .Package(url: "https://github.com/gfx/Swift-PureJsonSerializer.git", majorVersion: 1),
+        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
     ]
 )
