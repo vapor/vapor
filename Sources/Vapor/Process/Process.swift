f39450402a7611fb2ef19f8a0ab6883aaf43faa8
Logan Wright <logan.william.wright@gmail.com>
2016-02-27 14:46:24 -0500

    adding process argument tests

--- Sources/Vapor/Process/Process.swift
+++ Sources/Vapor/Process/Process.swift
@@ -9,17 +9,22 @@
 import Foundation
 
 extension Process {
-    
+
     /**
      Returns the string value of an
      argument passed to the executable
      in the format --name=value
+     
+     - parameter argument: the name of the argument to get the value for
+     - parameter arguments: arguments to search within.  Primarily used for testing through injection
+     
+     - returns: the value matching the argument if possible
      */
-    static func valueFor(argument name: String) -> String? {
+    internal static func valueFor(argument name: String, inArguments arguments: [String] = Process.arguments) -> String? {
         for argument in arguments where argument.hasPrefix("--\(name)=") {
             return argument.split("=").last
         }
         return nil
     }
-    
+
 }
