27c657dbf47789c4fbb52a40d5807ade33dff85d
Logan Wright <logan.william.wright@gmail.com>
2016-03-07 08:55:35 -0500

    fixing memory session driver

--- Sources/Vapor/Session/MemorySessionDriver.swift
+++ Sources/Vapor/Session/MemorySessionDriver.swift
@@ -8,38 +8,38 @@ import Foundation
 public class MemorySessionDriver: SessionDriver {
     var sessions = [String: [String: String]]()
     private var sessionsLock = NSLock()
-
+    
     public init() { }
-
+    
     public func valueFor(key key: String, inSession session: Session) -> String? {
         guard let sessionIdentifier = session.identifier else {
             Log.warning("Unable to read a value for '\(key)': The session has not been registered yet")
             return nil
         }
-
+        
         var value: String?
         sessionsLock.locked {
             value = sessions[sessionIdentifier]?[key]
         }
-
+        
         return value
     }
-
+    
     public func set(value: String?, forKey key: String, inSession session: Session) {
         guard let sessionIdentifier = session.identifier else {
             Log.warning("Unable to store a value for '\(key)': The session has not been registered yet")
             return
         }
-
+        
         sessionsLock.locked {
             if sessions[sessionIdentifier] == nil {
                 sessions[sessionIdentifier] = [String: String]()
             }
-
+            
             sessions[sessionIdentifier]?[key] = value
         }
     }
-
+    
     public func makeSessionIdentifier() -> String {
         var identifier = String(NSDate().timeIntervalSinceNow)
         identifier += "v@p0r"
@@ -50,13 +50,13 @@ public class MemorySessionDriver: SessionDriver {
         identifier += String(Int.random(min: 0, max: 9999))
         return Hash.make(identifier)
     }
-
+    
     public func destroy(session: Session) {
         guard let sessionIdentifier = session.identifier else {
             Log.warning("Unable to destroy the session: The session has not been registered yet")
             return
         }
-
+        
         sessionsLock.locked {
             sessions[sessionIdentifier] = nil
         }
