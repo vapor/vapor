19d917f042687cb9a6b2540286070e3d6cbf257c
Logan Wright <logan.william.wright@gmail.com>
2016-03-13 15:20:14 -0400

    conforming jeeves to server driver updates

--- Sources/Vapor/Server/Jeeves.swift
+++ Sources/Vapor/Server/Jeeves.swift
@@ -2,31 +2,29 @@
 import Foundation
 
 public class Jeeves<Socket: Vapor.Socket>: ServerDriver {
-    
+
     // MARK: Delegate
-    
+
     public var delegate: ServerDriverDelegate?
-    
+
     // MARK: Sockets
-    
+
     private var streamSocket: Socket?
     private var activeSockets = ThreadSafeSocketStore()
-    
+
     // MARK: Init
-    
+
     public init() {}
-    
+
     // MARK: ServerDriver
-    
-    public func boot(port port: Int) throws {
+
+
+    public func boot(ip ip: String, port: Int) throws {
         halt()
         streamSocket = try Socket.streamSocket()
-        
-        let p = Process.valueFor(argument: "port") ?? "\(port)"
-        let a = Process.valueFor(argument: "ip") ?? "0.0.0.0"
-        try streamSocket?.bind(a, port: p)
+        try streamSocket?.bind(ip, port: "\(port)")
         try streamSocket?.listen(100)
-        
+
         Background {
             do {
                 try self.streamSocket?.accept(self.handle)
@@ -35,7 +33,7 @@ public class Jeeves<Socket: Vapor.Socket>: ServerDriver {
             }
         }
     }
-    
+
     public func halt() {
         activeSockets.forEach { socket in
             // Individually so one failure doesn't prevent others from running
@@ -46,20 +44,20 @@ public class Jeeves<Socket: Vapor.Socket>: ServerDriver {
             }
         }
         activeSockets = ThreadSafeSocketStore()
-        
+
         do {
             try streamSocket?.close()
         } catch {
             Log.error("Failed to halt: \(streamSocket) error: \(error)")
         }
     }
-    
+
     private func handle(socket: Vapor.Socket) {
         activeSockets.insert(socket)
         defer {
             activeSockets.remove(socket)
         }
-        
+
         do {
             var keepAlive = false
             repeat {
@@ -68,7 +66,7 @@ public class Jeeves<Socket: Vapor.Socket>: ServerDriver {
                 try socket.write(response)
                 keepAlive = request.supportsKeepAlive
             } while keepAlive
-            
+
             try socket.close()
         } catch {
             Log.error("Request Handle Failed: \(error)")
