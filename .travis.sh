0210aa391d480a49585e8f39b485c4459c82d086
Tanner Nelson <tanner@bluebite.com>
2016-03-22 19:37:28 -0400

    update script

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,11 @@
+platform=`uname`
+
 swift build --fetch
 rm -rf Packages/*/Tests
 swift build
 swift test
 
-if [[ $platform == 'linux' ]]; then
+if [[ $platform == 'Linux' ]]; then
 	wget https://swift.org/builds/swift-2.2-release/ubuntu1404/swift-2.2-RELEASE/swift-2.2-RELEASE-ubuntu14.04.tar.gz
 	export PATH=swift-2.2-RELEASE-ubuntu14.04/usr/bin/:"${PATH}"
 	make
