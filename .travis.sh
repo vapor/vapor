11ff5d1b46735bbc9ca9d4523ae56679ec276509
Tanner Nelson <tanner@bluebite.com>
2016-03-23 23:52:15 -0400

    remove extra make

--- .travis.sh
+++ .travis.sh
@@ -3,6 +3,4 @@ platform=`uname`
 if [[ $platform == 'Linux' ]]; then
 	wget https://swift.org/builds/swift-2.2-release/ubuntu1404/swift-2.2-RELEASE/swift-2.2-RELEASE-ubuntu14.04.tar.gz
 	tar -zxvf swift-2.2-RELEASE-ubuntu14.04.tar.gz
-	export PATH=swift-2.2-RELEASE-ubuntu14.04/usr/bin/:"${PATH}"
-	make
 fi
