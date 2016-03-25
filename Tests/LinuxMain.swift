32569d6b017d18bd3bc2a9f6dff78399ad696603
Tanner Nelson <tanner@bluebite.com>
2016-03-24 23:21:11 -0400

    cli + test move

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+XCTMain([
+    testCase(ConfigTests.allTests),
+    testCase(ControllerTests.allTests),
+    testCase(EnvironmentTests.allTests),
+    testCase(HashTests.allTests),
+    testCase(LogTests.allTests),
+    testCase(MemorySessionDriverTests.allTests),
+    testCase(ResponseTests.allTests),
+    testCase(ProcessTests.allTests),
+    testCase(RouterTests.allTests),
+    testCase(RouteTests.allTests),
+    testCase(QueryParameterTests.allTests),
+    testCase(SessionTests.allTests),
+    testCase(TypedRouteTests.allTests),
+    testCase(JeevesTests.allTests)
+])
