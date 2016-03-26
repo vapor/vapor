ae48e270db80de1d74ae79ff45ef913dc372727a
Tanner Nelson <tanner@bluebite.com>
2016-03-26 16:00:48 -0400

    remove 2.2 compat

--- .travis.sh
+++
@@ -1,6 +0,0 @@
-platform=`uname`
-
-if [[ $platform == 'Linux' ]]; then
-	wget https://swift.org/builds/swift-2.2-release/ubuntu1404/swift-2.2-RELEASE/swift-2.2-RELEASE-ubuntu14.04.tar.gz
-	tar -zxvf swift-2.2-RELEASE-ubuntu14.04.tar.gz
-fi
