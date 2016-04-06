a3f523f98991c0e91866eee12d180fca57351430
Logan Wright <logan.william.wright@gmail.com>
2016-04-06 19:43:19 -0400

    adding event tests

---
+++ Tests/Vapor/EventTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventTests.swift
+//  Vapor
+//
+//  Created by Logan Wright on 4/6/16.
+//
+//
+
+import XCTest
+@testable import Vapor
+
+class EventTests: XCTestCase {
+    static var allTests: [(String, EventTests -> () throws -> Void)] {
+        return [
+        ]
+    }
+}
