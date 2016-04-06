839ceb472fc463607767b84dad3b9f21b376c39c
Logan Wright <logan.william.wright@gmail.com>
2016-04-06 19:38:58 -0400

    adding basic event functionality

---
+++ Sources/Vapor/Events/Events.swift
@@ -0,0 +1,29 @@
+
+public final class Subscription {
+    private var completion: Void -> Void = {}
+    deinit {
+        completion()
+    }
+}
+
+public final class Event<T> {
+    public typealias Handler = T -> Void
+    private typealias Subscriber = (token: Subscription, handler: Handler)
+    private var subscribers: [Subscriber] = []
+
+    public func subscribe(handler: Handler) -> Subscription {
+        let newToken = Subscription()
+        subscribers.append((newToken, handler))
+        newToken.completion = { [weak self] in
+            guard let welf = self else { return }
+            welf.subscribers = welf.subscribers.filter { token, _ in
+                return token !== newToken
+            }
+        }
+        return newToken
+    }
+
+    public func post(data: T) {
+        subscribers.forEach { _, handler in handler(data) }
+    }
+}
