c6eaacf3eb422e5da958ff2df291032fcb6fad18
Logan Wright <logan.william.wright@gmail.com>
2016-04-07 20:08:00 -0400

    moving subscription to own file

--- Sources/Vapor/Event/Event.swift
+++ Sources/Vapor/Event/Event.swift
@@ -1,20 +1,3 @@
-/**
- This class is used to keep a subscription to an event active.
-
- Set all references to `nil` to no longer receive events
- */
-public final class Subscription {
-    /**
-     Completion to run on deinit.
-
-     - Warning: This should only be used by an event to clear the subscription on deinitialization
-     */
-    private var completion: Void -> Void = {}
-    deinit {
-        completion()
-    }
-}
-
 extension Subscription {
     /**
      This is used to contain subscriptions w/o reference
---
+++ Sources/Vapor/Event/Subscription.swift
@@ -0,0 +1,16 @@
+/**
+ This class is used to keep a subscription to an event active.
+
+ Set all references to `nil` to no longer receive events
+ */
+public final class Subscription {
+    /**
+     Completion to run on deinit.
+
+     - Warning: This should only be used by an event to clear the subscription on deinitialization
+     */
+    internal var completion: Void -> Void = {}
+    deinit {
+        completion()
+    }
+}
