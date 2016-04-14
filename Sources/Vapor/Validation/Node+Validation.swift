a323f191efd84b9d04181f78296722e0c0d830f8
Logan Wright <logan.william.wright@gmail.com>
2016-04-13 23:25:34 -0400

    tweaking extractable to not require an additional error type

--- Sources/Vapor/Validation/Node+Validation.swift
+++ Sources/Vapor/Validation/Node+Validation.swift
@@ -20,26 +20,29 @@ extension Node {
 
 public protocol Extractable {
     associatedtype Wrapped
-    func extract() throws -> Wrapped
+    func extract() -> Wrapped?
 }
 
 extension Extractable where Wrapped == Node {
     public func validated<
-        T: ValidationSuite
-        where T.InputType: NodeInitializable>(by suite: T.Type = T.self)
-        throws -> Valid<T> {
-            let wrapped = try extract()
-            let value = try T.InputType.makeWith(wrapped)
+        V: ValidationSuite
+        where V.InputType: NodeInitializable>(by suite: V.Type = V.self)
+        throws -> Valid<V> {
+            guard let wrapped = extract() else {
+                throw ValidationFailure<V>(input: nil)
+            }
+            let value = try V.InputType.makeWith(wrapped)
             return try value.validated(by: suite)
     }
 }
 
 extension Optional: Extractable {
-    public func extract() throws -> Wrapped {
-        guard let value = self else {
-            // TODO: Better Error
-            throw NodeExtraction()
-        }
-        return value
+    public func extract() -> Wrapped? {
+        return self
+//        guard let value = self else {
+//            // TODO: Better Error
+//            throw NodeExtraction()
+//        }
+//        return value
     }
 }
