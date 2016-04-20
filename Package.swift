4144052225f66969e527112d46969ce6743362a7
Logan Wright <logan.william.wright@gmail.com>
2016-04-19 20:11:50 -0400

    resolving dependency tree

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@ let package = Package(
     name: "Vapor",
     dependencies: [
         //Standards package. Contains protocols for cross-project compatability.
-        .Package(url: "https://github.com/swiftx/s4.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/swiftx/s4.git", majorVersion: 0, minor: 4),
 
         //Provides critical String functions Foundation is missing on Linux
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
 
         //Parses and serializes JSON
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 5),
 
         //Swift wrapper around Sockets, used for built-in HTTP server
-        .Package(url: "https://github.com/qutheory/Hummingbird.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/ketzusaka/Hummingbird.git", majorVersion: 1, minor: 3),
 
         //SHA2 + HMAC hashing. Used by the core to create session identifiers.
-        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/qutheory/SHA2.git", majorVersion: 0, minor: 3)
     ],
     exclude: [
         "XcodeProject",
