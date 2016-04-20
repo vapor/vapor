ea033855ec1b28ab0145e739ff827bbb8d27ebf9
Logan Wright <logan.william.wright@gmail.com>
2016-04-20 18:55:03 -0400

    testing nsdata load over manual

--- Sources/Vapor/Fixes/FileManager.swift
+++ Sources/Vapor/Fixes/FileManager.swift
@@ -1,3 +1,4 @@
+import Foundation
 import libc
 
 /**
@@ -11,53 +12,55 @@ class FileManager {
     }
 
     static func readBytesFromFile(_ path: String) throws -> [UInt8] {
-        let fd = open(path, O_RDONLY)
-
-        if fd < 0 {
-            throw Error.CouldNotOpenFile
-        }
-        defer {
-            close(fd)
-        }
-
-        var info = stat()
-        let ret = withUnsafeMutablePointer(&info) { infoPointer -> Bool in
-            if fstat(fd, infoPointer) < 0 {
-                return false
-            }
-            return true
-        }
-
-        if !ret {
-            print("Read bytes 1")
-            throw Error.Unreadable
-        }
-
-        let length = Int(info.st_size)
-
-        let rawData = malloc(length)
-        var remaining = Int(info.st_size)
-        var total = 0
-        while remaining > 0 {
-            let advanced = rawData.advanced(by: total)
-
-            let amt = read(fd, advanced, remaining)
-            if amt < 0 {
-                break
-            }
-            remaining -= amt
-            total += amt
-        }
-
-        if remaining != 0 {
-            print("Read bytes 2")
-            throw Error.Unreadable
-        }
-
-        //thanks @Danappelxx
-        let data = UnsafeMutablePointer<UInt8>(rawData)
-        let buffer = UnsafeMutableBufferPointer<UInt8>(start: data, count: length)
-        return Array(buffer)
+        let data = NSData(contentsOfFile: path)
+        return data?.byteArray ?? []
+//        let fd = open(path, O_RDONLY)
+//
+//        if fd < 0 {
+//            throw Error.CouldNotOpenFile
+//        }
+//        defer {
+//            close(fd)
+//        }
+//
+//        var info = stat()
+//        let ret = withUnsafeMutablePointer(&info) { infoPointer -> Bool in
+//            if fstat(fd, infoPointer) < 0 {
+//                return false
+//            }
+//            return true
+//        }
+//
+//        if !ret {
+//            print("Read bytes 1")
+//            throw Error.Unreadable
+//        }
+//
+//        let length = Int(info.st_size)
+//
+//        let rawData = malloc(length)
+//        var remaining = Int(info.st_size)
+//        var total = 0
+//        while remaining > 0 {
+//            let advanced = rawData.advanced(by: total)
+//
+//            let amt = read(fd, advanced, remaining)
+//            if amt < 0 {
+//                break
+//            }
+//            remaining -= amt
+//            total += amt
+//        }
+//
+//        if remaining != 0 {
+//            print("Read bytes 2")
+//            throw Error.Unreadable
+//        }
+//
+//        //thanks @Danappelxx
+//        let data = UnsafeMutablePointer<UInt8>(rawData)
+//        let buffer = UnsafeMutableBufferPointer<UInt8>(start: data, count: length)
+//        return Array(buffer)
     }
 
     static func fileAtPath(_ path: String) -> (exists: Bool, isDirectory: Bool) {
