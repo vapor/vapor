fe04fb36947803fa8b4949571b4235dd77f792b4
Logan Wright <logan.william.wright@gmail.com>
2016-05-04 23:35:32 -0400

    adding 'In' validator

--- Sources/Vapor/Validation/Convenience/Unique.swift
+++ Sources/Vapor/Validation/Convenience/Unique.swift
@@ -18,3 +18,26 @@ public struct Unique<
         }
     }
 }
+
+public struct In<
+    T where
+    T: Validatable,
+    T: Equatable>: Validator {
+
+    private let iteratorFactory: Void -> AnyIterator<T>
+
+    public init<S: Sequence where S.Iterator.Element == T>(_ sequence: S) {
+        iteratorFactory = {
+            var iterator = sequence.makeIterator()
+            return AnyIterator { iterator.next() }
+        }
+    }
+
+    public func validate(input value: T) throws {
+        let iterator = iteratorFactory()
+        for next in iterator where next == value {
+            return
+        }
+        throw error(with: value)
+    }
+}
