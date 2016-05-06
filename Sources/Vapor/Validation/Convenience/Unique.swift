77c76d0dfc9c79292b386da11e69112c7e7d47da
Logan Wright <logan.william.wright@gmail.com>
2016-05-05 20:44:45 -0400

    added contains

--- Sources/Vapor/Validation/Convenience/Unique.swift
+++ Sources/Vapor/Validation/Convenience/Unique.swift
@@ -41,3 +41,35 @@ public struct In<
         throw error(with: value)
     }
 }
+
+public struct Contains<
+    T where
+    T: Sequence,
+    T: Validatable,
+    T.Iterator.Element: Equatable>: Validator {
+
+    /**
+        The value expected to be in sequence
+    */
+    public let expecting: T.Iterator.Element
+
+    /**
+        Create a validator to check that a sequence contains the given value
+
+        - parameter expecting: the value expected to be in sequence
+     */
+    init(_ expecting: T.Iterator.Element) {
+        self.expecting = expecting
+    }
+
+    /**
+        validate
+    */
+    public func validate(input sequence: T) throws {
+        for element in sequence where element == expecting {
+            return
+        }
+
+        throw error(with: sequence)
+    }
+}
