84ce63efc5e3714693de3b0d1a04562d9a8649f1
Logan Wright <logan.william.wright@gmail.com>
2016-05-08 15:42:58 -0400

    updating cryptokitten to 0.7.x

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         .Package(url: "https://github.com/ketzusaka/Hummingbird.git", majorVersion: 1, minor: 7),
 
         //SHA2 + HMAC hashing. Used by the core to create session identifiers.
-        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 7),
 
         //Determines Content-Type for file extensions
         .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 6)
