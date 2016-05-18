c5bd5becce91df2623d4d80061ec8e2093e2afde
Tanner Nelson <tanner@bluebite.com>
2016-05-18 16:44:29 -0400

    add fluent to package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 7),
 
         //Determines Content-Type for file extensions
-        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 6),
+
+        //ORM for interacting with databases
+        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 3)
     ],
     exclude: [
         "XcodeProject"
