5b3c63aa03f18d6aba71f43216152eb371d459e3
Evertt <ecvanbrussel@gmail.com>
2016-05-20 00:19:27 +0200

    Authorizables should automatically be Models

--- Sources/Vapor/Auth/Authorizable.swift
+++ Sources/Vapor/Auth/Authorizable.swift
@@ -4,7 +4,7 @@
  * And it comes with a handy method for checking a permission.
  * So you can write `if user.can(.update, this: post) { ... }`
  */
-public protocol Authorizable {
+public protocol Authorizable: Model {
     func can(_ action: Ability.Action, this model: Model) -> Bool
     func cannot(_ action: Ability.Action, this model: Model) -> Bool
 }
