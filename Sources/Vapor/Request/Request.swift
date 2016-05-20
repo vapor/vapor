460ee358ad5891df05508de17b96339520b120f7
Tanner Nelson <tanner@bluebite.com>
2016-05-20 12:08:27 -0400

    whitespace

--- Sources/Vapor/Request/Request.swift
+++ Sources/Vapor/Request/Request.swift
@@ -80,7 +80,7 @@ extension Request {
                 formEncoded[key] = .input(value)
             }
         }
-        
+
         return formEncoded
     }
 
@@ -113,9 +113,9 @@ extension Request {
                 Log.warning("Invalid boundary")
                 return Request.Content(query: queries, json: json, formEncoded: formEncoded)
             }
-            
+
             let boundary = boundaryPieces[1]
-            
+
             do {
                 let data = try mutableBody.becomeBuffer()
                 formEncoded = self.parseMultipartForm(data, boundary: boundary)
@@ -177,14 +177,14 @@ extension Data {
 
         // "\r\n\r\n\r\n".split(separator: "\r\n\r\n") would break without this because it occurs twice in the same place
         var highestOccurence = -1
-        
+
         // Find occurences of boundries
         for (index, element) in self.enumerated() where index > highestOccurence {
             // If this first element matches and there are enough bytes left
             guard element == separator.first && self.count >= index + separator.count else {
                 continue
             }
-            
+
             // Take the last byte of where the end of the separator would be and check it
             guard self[index + separator.count - 1] == separator.bytes.last else {
                 continue
@@ -201,19 +201,19 @@ extension Data {
             // Increase the highest occurrence to prevent a crash as described above
             highestOccurence = index + separator.count
         }
-        
+
         // The first data (before the first separator)
         if let firstRange = ranges.first where !excludingFirst {
             parts.append(Data(self[0..<firstRange.from]))
         }
-        
+
         // Loop over the ranges
         for (pos, range) in ranges.enumerated() {
             // If this is before the last separator
             if pos < ranges.count - 1 {
                 // Take the data inbetween this and the next boundry
                 let nextRange = ranges[pos + 1]
-                
+
                 parts.append(Data(self[range.to..<nextRange.from]))
 
             // If this is after the last separator and shouldn't be thrown away
@@ -221,7 +221,7 @@ extension Data {
                 parts.append(Data(self[range.to..<self.count]))
             }
         }
-        
+
         return parts
     }
 }
