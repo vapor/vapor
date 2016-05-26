87590cb215ff78428b30bb2fa4e070b9098bae96
Tanner Nelson <tanner@bluebite.com>
2016-05-26 15:44:16 -0400

    rename file + `Vapor.`

--- Sources/Vapor/Fluent/Model+StringInitializable.swift
+++ Sources/Vapor/Fluent/Model+StringInitializable.swift
@@ -2,7 +2,7 @@ import Fluent
 
 public protocol Model: Fluent.Model, StringInitializable {}
 
-extension Model {
+extension Vapor.Model {
     public init?(from string: String) throws {
         guard let model = try Self.find(string) else {
             return nil
