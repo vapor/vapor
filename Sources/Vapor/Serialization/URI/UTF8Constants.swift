4e07780efe5b494abd16b381eaf3441c23f44d5e
Logan Wright <logan.william.wright@gmail.com>
2016-06-09 16:56:26 -0400

    breaking out static data buffer and utf8 constants into their own files

---
+++ Sources/Vapor/Serialization/URI/UTF8Constants.swift
@@ -0,0 +1,15 @@
+extension Byte {
+    static let forwardSlash: Byte = 0x2F // '/'
+    static let atSign: Byte = 0x40 // '@'
+    static let colon: Byte = 0x3A // ':'
+    static let leftSquareBracket: Byte = 0x5B // '['
+    static let rightSquareBracket: Byte = 0x5D // ']'
+    static let period: Byte = 0x2E // '.' -- Full Stop
+    static let questionMark: Byte = 0x3F // '?'
+    static let numberSign: Byte = 0x23 // '#'
+    static let percentSign: Byte = 0x25 // '%'
+    static let plusSign: Byte = 0x2B // '+'
+    static let minusSign: Byte = 0x2D // '-' -- Hyphen Minus
+    static let zeroCharacter: Byte = 0x30
+    static let space: Byte = 0x20
+}
