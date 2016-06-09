021ebce1ca1b6cb2d7afbf62200e745cee19c9cb
Logan Wright <logan.william.wright@gmail.com>
2016-06-09 17:07:39 -0400

    making some notes for myself in http parser

--- Sources/Vapor/Server/HTTP/HTTPParser.swift
+++ Sources/Vapor/Server/HTTP/HTTPParser.swift
@@ -38,6 +38,7 @@ final class HTTPParser: StreamParser {
             throw Error.streamEmpty
         }
 
+        // TODO: Actually parse
         let requestLine = try RequestLine(requestLineString)
 
         var headers: [Request.Headers.Key: String] = [:]
