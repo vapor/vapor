74b27d6d8a8b374ac7abf07b7507c00cc2311411
Tanner Nelson <me@tanner.xyz>
2016-06-21 12:00:44 -0400

    cleanup merge

--- Sources/Development/main.swift
+++ Sources/Development/main.swift
@@ -46,30 +46,19 @@ app.get("pokemon") { req in
 }
 
 app.get("pokemon-multi") { req in
-    let pokemonClient = try app.client("http://pokeapi.co")
-    let responder: (ChunkStream) throws -> Void = { stream in
+    return Response { chunker in
+        let pokemonClient = try app.client("http://pokeapi.co")
+
         for i in 0 ..< 2 {
-            let response = try pokemonClient.get("/api/v2/pokemon", query: ["limit": "\(20)", "offset": "\(i)"])
+            let response = try pokemonClient.get("/api/v2/pokemon", query: ["limit": 20, "offset": i])
 
-<<<<<<< HEAD
-    var names: [String] = []
-
-    for i in 0 ..< 2 {
-        let response = try pokemonClient.get("/api/v2/pokemon", query: ["limit": 20, "offset": i])
-
-        if let n = response.data["results", "name"].array?.flatMap({ $0.string }) {
-            names += n
-=======
             if let n = response.data["results", "name"].array?.flatMap({ $0.string }) {
-                try stream.send(n.joined(separator: "\n"))
+                try chunker.send(n.joined(separator: "\n"))
             }
->>>>>>> f65a73558c04714daf6c8975b98185850609149b
         }
 
-        try stream.close()
+        try chunker.close()
     }
-
-    return Response(chunked: responder)
 }
 
 app.get("test") { request in
