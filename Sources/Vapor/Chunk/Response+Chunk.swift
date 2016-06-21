06cbb907129272b8afaa62218765adb71a74326e
Tanner Nelson <tanner@bluebite.com>
2016-06-20 20:02:17 -0400

    put chunked back

---
+++ Sources/Vapor/Chunk/Response+Chunk.swift
@@ -0,0 +1,16 @@
+extension HTTPResponse {
+    /**
+        Send chunked data with the
+        `Transfer-Encoding: Chunked` header.
+
+        Chunked uses the Transfer-Encoding HTTP header in
+        place of the Content-Length header.
+
+        https://en.wikipedia.org/wiki/Chunked_transfer_encoding
+    */
+    public convenience init(status: Status = .ok, headers: Headers = [:], chunked closure: ((ChunkStream) throws -> Void)) {
+        var headers = headers
+        headers.setTransferEncodingChunked()
+        self.init(status: status, headers: headers, body: .chunked(closure))
+    }
+}
--- Sources/Vapor/HTTP/Messages/Response/HTTPResponse.swift
+++ Sources/Vapor/HTTP/Messages/Response/HTTPResponse.swift
@@ -46,23 +46,6 @@ extension HTTPResponse {
 
 extension HTTPResponse {
     /**
-         Send chunked data with the
-         `Transfer-Encoding: Chunked` header.
-
-         Chunked uses the Transfer-Encoding HTTP header in
-         place of the Content-Length header.
-
-         https://en.wikipedia.org/wiki/Chunked_transfer_encoding
-    */
-    public convenience init(status: Status = .ok, headers: Headers = [:], chunked closure: ((ChunkStream) throws -> Void)) {
-        var headers = headers
-        headers.setTransferEncodingChunked()
-        self.init(status: status, headers: headers, body: .chunked(closure))
-    }
-}
-
-extension HTTPResponse {
-    /**
          Convenience Initializer
 
          - parameter status: the http status
