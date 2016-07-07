efbf3d2bf67fbd3610cc69f9d79923cc2907db71
Logan Wright <logan.william.wright@gmail.com>
2016-07-06 21:19:12 -0400

    removing weirdness for now

--- Sources/Vapor/Content/Request+Content.swift
+++ Sources/Vapor/Content/Request+Content.swift
@@ -1,27 +1,9 @@
 extension HTTPRequest {
-
-    // TODO: Weird but solves problem, keep?
-    public typealias ContentDidLoad = (request: HTTPRequest, data: Content) -> Void
-
-    // extensible content loading
-    private static var contentDidLoad: ContentDidLoad = { _ in }
-
-    public static func register(_ newLoader: ContentDidLoad) {
-        let currentLoader = contentDidLoad
-        contentDidLoad = { request, content in
-            // FIFO
-            currentLoader(request: request, data: content)
-            newLoader(request: request, data: content)
-        }
-    }
-
-
     public var data: Content {
         if let data = storage["data"] as? Content {
             return data
         } else {
             let data = Content()
-            HTTPRequest.contentDidLoad(request: self, data: data)
 
             // in closures for weak lazy load, external implementations can use `data.append(self.json)`
             data.append { [weak self] in self?.query }
