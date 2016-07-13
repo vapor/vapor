0f92d537e3b47b4bee7de085535ad3cbf4a7bbba
Tanner Nelson <me@tanner.xyz>
2016-07-12 20:03:36 -0400

    remove comment

--- Sources/Vapor/Routing/Controller.swift
+++ Sources/Vapor/Routing/Controller.swift
@@ -1,8 +1,6 @@
 /**
     Base controller class
 */
-//public typealias Controller = protocol<DropletInitializable, ResourceController>
-
 public class Controller: DropletInitializable {
     var droplet: Droplet
     public required init(droplet: Droplet) {
