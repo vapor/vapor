198c9d4d296c98496d6c650731f819dfe006abc2
Logan Wright <logan.william.wright@gmail.com>
2016-07-26 17:28:55 -0400

    concrete crypto

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: targets,
     dependencies: [
         //SHA2 + HMAC hashing. Used by the core to create session identifiers.
-        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/CryptoKitten/HMAC.git", majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/CryptoKitten/SHA2.git", majorVersion: 0, minor: 9),
 
         //ORM for interacting with databases
         .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 0),
