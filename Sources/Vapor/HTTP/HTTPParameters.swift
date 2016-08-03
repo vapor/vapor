3dd8d111ce6d9d7bf5168b25b0f91c0a7c62ffa3
Logan Wright <logan.william.wright@gmail.com>
2016-08-03 14:30:09 -0400

    throw on

--- Sources/Vapor/HTTP/HTTPParameters.swift
+++ Sources/Vapor/HTTP/HTTPParameters.swift
@@ -1,11 +1,18 @@
 import HTTP
 
+enum ExtractError: Swift.Error {
+    case unexpectedNil
+}
+
 extension KeyAccessible where Key == String, Value == String {
     /**
         Extract and transform values from [String: String] dictionaries 
-        to StringInitializable types
+         tto StringInitializable types
     */
-    public func extract<S: StringInitializable>(_ key: String) throws -> S? {
-        return try self[key].flatMap { try S(from: $0) }
+    public func extract<S: StringInitializable>(_ key: String) throws -> S {
+        guard let val = try self[key].flatMap({ try S(from: $0) }) else {
+            throw ExtractError.unexpectedNil
+        }
+        return val
     }
 }
