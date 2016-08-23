8c09c6bef1a3460b5258d67fc3a593b0b779d9f5
Tanner Nelson <me@tanner.xyz>
2016-08-23 13:22:40 -0400

    fix ordering of tests

--- Tests/Cookies/SerializingTests.swift
+++ Tests/Cookies/SerializingTests.swift
@@ -15,7 +15,8 @@ class SerializingTests: XCTestCase {
         cookies["leet"] = "1337"
 
         let serialized = cookies.serialize(for: .request)
-        XCTAssertEqual(serialized, "leet=1337; life=42")
+        XCTAssert(serialized.contains("leet=1337"))
+        XCTAssert(serialized.contains("life=42"))
     }
 
     func testResponse() throws {
@@ -25,7 +26,9 @@ class SerializingTests: XCTestCase {
         cookies["leet"] = "1337"
 
         let serialized = cookies.serialize(for: .response)
-        XCTAssertEqual(serialized, "leet=1337; Path=/\r\nSet-Cookie: life=42; Path=/")
+
+        XCTAssert(serialized.contains("leet=1337; Path=/"))
+        XCTAssert(serialized.contains("life=42; Path=/"))
     }
 
     func testEmpty() throws {
