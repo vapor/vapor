8564dd1774471d2cbc7ea6f27ccf5e31a539aa9e
Tanner Nelson <me@tanner.xyz>
2016-08-25 16:02:50 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -26,29 +26,28 @@ let package = Package(
         Target(name: "Cache"),
         Target(name: "Cookies"),
 
-
         // Development and Testing
         Target(name: "Development", dependencies: ["Vapor"]),
         Target(name: "Performance", dependencies: ["Vapor"]),
     ],
     dependencies: [
         // SHA2 + HMAC hashing. Used by the core to create session identifiers.
-        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 0),
+        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 0, minor: 2),
 
         // ORM for interacting with databases
-        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 10),
 
         // Core vapor transport layer
-        .Package(url: "https://github.com/vapor/engine.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/vapor/engine.git", majorVersion: 0, minor: 6),
 
         // Console protocol and implementation for powering command line interface.
-        .Package(url: "https://github.com/vapor/console.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/vapor/console.git", majorVersion: 0, minor: 6),
 
         // JSON enum wrapper around Foundation JSON
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 0, minor: 5),
 
         // A security framework for Swift.
-        .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 0, minor: 1),
+        // .Package(url: "https://github.com/stormpath/Turnstile.git", majorVersion: 0, minor: 1),
     ],
     exclude: [
         // No excludes currently
