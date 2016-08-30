f1cfb82bf2f60580002f73dd78a05c363275d8e5
Kyle Rokita <kyle.rokita@gmail.com>
2016-08-29 21:20:31 -0700

    Update DateMiddleware.swift

--- Sources/Vapor/Date/DateMiddleware.swift
+++ Sources/Vapor/Date/DateMiddleware.swift
@@ -3,6 +3,9 @@ import HTTP
 import Core
 
 public final class DateMiddleware: Middleware {
+
+    public init() {}
+
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
 
