4672ca2c7f9ea576f9a39706775bf50f07eb7ef1
Logan Wright <logan.william.wright@gmail.com>
2016-09-15 15:46:10 -0400

    use appropriate path, not uri

--- Tests/VaporTests/DropletTests.swift
+++ Tests/VaporTests/DropletTests.swift
@@ -21,7 +21,7 @@ class DropletTests: XCTestCase {
     func testMediaType() throws {
         let drop = Droplet(workDir: workDir, serverMiddleware: ["file"])
 
-        let request = try Request(method: .get, uri: "styles/app.css")
+        let request = try Request(method: .get, path: "styles/app.css")
 
         guard let response = try? drop.respond(to: request) else {
             XCTFail("drop could not respond")
