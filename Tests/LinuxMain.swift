6813191ef130900a0a64ed97b464fe3a06fa02c3
Logan Wright <logan.william.wright@gmail.com>
2016-10-13 14:51:33 -0400

    add middleware tests to linuxmain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -41,6 +41,7 @@ XCTMain([
     testCase(EnvironmentTests.allTests),
     testCase(EventTests.allTests),
     testCase(FileManagerTests.allTests),
+    testCase(FileMiddlewareTests.allTests),
     testCase(HashTests.allTests),
     testCase(LocalizationTests.allTests),
     testCase(LogTests.allTests),
