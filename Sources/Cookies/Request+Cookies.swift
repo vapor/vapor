8570b38d753a4dc4b0038d06eb8441542822fbc1
Steven Roebert <steven@icemobile.com>
2016-10-22 20:00:52 +0200

    Also fixed empty cookie header for requests

--- Sources/Cookies/Request+Cookies.swift
+++ Sources/Cookies/Request+Cookies.swift
@@ -23,7 +23,14 @@ extension Request {
         }
         set(cookie) {
             storage["Cookie"] = cookie
-            headers["Cookie"] = cookie.serialize(for: .request)
+            
+            let cookieHeader = cookie.serialize(for: .request)
+            if !cookieHeader.isEmpty {
+                headers["Cookie"] = cookieHeader
+            }
+            else {
+                headers["Cookie"] = nil
+            }
         }
     }
 }
