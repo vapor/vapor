c903402aee72542e3595a973ea5abb984b4575e9
Steven Roebert <steven@icemobile.com>
2016-10-24 20:48:01 +0200

    Small change in colon placement style

--- Tests/AuthTests/Utilities/AuthMiddlewareResponser.swift
+++ Tests/AuthTests/Utilities/AuthMiddlewareResponser.swift
@@ -1,7 +1,7 @@
 import HTTP
 import Turnstile
 
-class AuthMiddlewareResponser : Responder {
+class AuthMiddlewareResponser: Responder {
     func respond(to request: Request) throws -> Response {
         try request.auth.login(UsernamePassword(username: "username", password: "password"))
         return "test".makeResponse()
