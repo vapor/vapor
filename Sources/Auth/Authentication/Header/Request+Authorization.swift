a2e106364a60c9bb6af4db4f98e59004be722a2c
Logan Wright <logan.william.wright@gmail.com>
2017-01-02 17:48:37 -0500

    fix auth/req reference cycle

--- Sources/Auth/Authentication/Header/Request+Authorization.swift
+++ Sources/Auth/Authentication/Header/Request+Authorization.swift
@@ -2,13 +2,13 @@ import HTTP
 import Turnstile
 
 public final class Helper {
-    public weak var request: Request?
+    public let request: Request
     public init(request: Request) {
         self.request = request
     }
 
     public var header: Authorization? {
-        guard let authorization = request?.headers["Authorization"] else {
+        guard let authorization = request.headers["Authorization"] else {
             return nil
         }
 
@@ -16,25 +16,34 @@ public final class Helper {
     }
 
     public func login(_ credentials: Credentials, persist: Bool = true) throws {
-        try request?.subject().login(credentials: credentials, persist: persist)
+        return try request.subject().login(credentials: credentials, persist: persist)
     }
-    
+
     public func logout() throws {
-        try request?.subject().logout()
+        return try request.subject().logout()
     }
 
     public func user() throws -> User {
-        let subject = try request?.subject()
+        let subject = try request.subject()
 
-        guard let details = subject?.authDetails else {
+        guard let details = subject.authDetails else {
             throw AuthError.notAuthenticated
         }
 
         guard let user = details.account as? User else {
             throw AuthError.invalidAccountType
         }
-        
+
         return user
+    }
+}
+
+// FilePrivate until we can move to Core
+fileprivate class Weak<T: AnyObject> {
+    private(set) weak var value: T?
+
+    init(_ value: T) {
+        self.value = value
     }
 }
 
@@ -42,12 +51,12 @@ extension Request {
     public var auth: Helper {
         let key = "auth"
 
-        guard let helper = storage[key] as? Helper else {
+        guard let wrapper = storage[key] as? Weak<Helper>, let helper = wrapper.value else {
             let helper = Helper(request: self)
-            storage[key] = helper
+            storage[key] = Weak(helper)
             return helper
         }
-
+        
         return helper
     }
 }
