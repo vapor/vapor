d9ca9898b2de090d0a854f70ab3bd7790d6fc9f8
Logan Wright <logan.william.wright@gmail.com>
2017-02-14 16:10:39 +0100

    temporarily store model in fluentvap file

--- Sources/Vapor/Fluent+Vapor.swift
+++ Sources/Vapor/Fluent+Vapor.swift
@@ -29,6 +29,42 @@ extension Droplet {
     }
 }
 
+//import JSON
+//import Fluent
+//import TypeSafeRouting
+//import HTTP
+//
+//@_exported import class Fluent.Database
+//
+//public protocol Modelz: Entity, JSONRepresentable, StringInitializable, ResponseRepresentable { }
+//
+//extension Modelz {
+//    public func makeResponse() throws -> Response {
+//        return try makeJSON().makeResponse()
+//    }
+//}
+//
+//// MARK: JSONRepresentable
+//
+//extension Modelz {
+//    public func makeJSON() throws -> JSON {
+//        let node = try makeNode()
+//        return try JSON(node: node)
+//    }
+//}
+//
+//// MARK: StringInitializable
+//
+//extension Modelz {
+//    public init?(from string: String) throws {
+//        if let model = try Self.find(string) {
+//            self = model
+//        } else {
+//            return nil
+//        }
+//    }
+//}
+
 fileprivate final class FluentVaporProvider: Provider {
 
     let servers: [String: ServerConfig]?
