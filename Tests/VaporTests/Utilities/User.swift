652e8d8316696bd0f0bbde07da555210e81557f1
Logan Wright <logan.william.wright@gmail.com>
2017-02-15 12:43:48 +0100

    return resource tests

--- Tests/VaporTests/Utilities/User.swift
+++ Tests/VaporTests/Utilities/User.swift
@@ -1,32 +1,28 @@
-//import Vapor
-//import Fluent
-//
-//final class Userz: Modelz {
-//    var id: Node?
-//    var name: String
-//
-//    init?(from string: String) throws {
-//        if string == "ERROR" {
-//            return nil
-//        }
-//        
-//        self.name = string
-//    }
-//    
-//    init(name: String) {
-//        self.id = nil
-//        self.name = name
-//    }
-//
-//    init(node: Node, in context: Context) throws {
-//        self.id = try node.extract("id")
-//        self.name = try node.extract("name")
-//    }
-//
-//    func makeNode(context: Context) throws -> Node {
-//        return try  Node(node:["name": name])
-//    }
-//
-//    static func prepare(_ db: Database) throws { }
-//    static func revert(_ db: Database) throws { }
-//}
+import Vapor
+
+final class User: StringInitializable, NodeConvertible {
+    var id: Node?
+    var name: String
+
+    init?(from string: String) throws {
+        if string == "ERROR" {
+            return nil
+        }
+        
+        self.name = string
+    }
+    
+    init(name: String) {
+        self.id = nil
+        self.name = name
+    }
+
+    init(node: Node, in context: Context) throws {
+        self.id = try node.extract("id")
+        self.name = try node.extract("name")
+    }
+
+    func makeNode(context: Context) throws -> Node {
+        return try  Node(node:["name": name])
+    }
+}
