bd834c90a3a1f9d4e67494aa36a93163f3940ac9
Logan Wright <logan.william.wright@gmail.com>
2017-02-16 11:33:46 +0100

    more support-- more deps to go

--- Package.swift
+++ Package.swift
@@ -24,13 +24,13 @@ let package = Package(
         .Package(url: "https://github.com/vapor/engine.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
 
         // Console protocol and implementation for powering command line interface.
-        .Package(url: "https://github.com/vapor/console.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/console.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
 
         // JSON enum wrapper around Foundation JSON
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/json.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
 
         // An extensible templating language built for Vapor. 🍃
-        .Package(url: "https://github.com/vapor/leaf.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/leaf.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
 
         // A type safe routing package including HTTP and TypeSafe routers.
         .Package(url: "https://github.com/vapor/routing.git", majorVersion: 1),
