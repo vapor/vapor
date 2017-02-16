34fb7aa01b77632c3a8e0209cacb27ed3d78f5cf
Logan Wright <logan.william.wright@gmail.com>
2017-02-16 14:16:54 +0100

    effing linuxmain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -8,7 +8,6 @@ import XCTest
 
 XCTMain([
     // Cache
-    testCase(FluentCacheTests.allTests),
     testCase(MemoryCacheTests.allTests),
 
     // Config
@@ -25,7 +24,6 @@ XCTMain([
     testCase(ConfigIntegrationTests.allTests),
     testCase(ConsoleTests.allTests),
     testCase(ContentTests.allTests),
-    testCase(CookieTests.allTests),
     testCase(CORSMiddlewareTests.allTests),
     testCase(DataSplitTests.allTests),
     testCase(DropletTests.allTests),
