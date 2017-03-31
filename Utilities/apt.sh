bc6b4fa4c16e7543d71422d1e615e3b0debebb21
tanner0101 <me@tanner.xyz>
2017-03-31 21:11:55 +0200

    trusty

--- Utilities/apt.sh
+++ Utilities/apt.sh
@@ -1,9 +1,9 @@
 function add_vapor_apt() {
     eval "$(cat /etc/lsb-release)"
 
-    if [[ "$DISTRIB_CODENAME" != "xenial" && "$DISTRIB_CODENAME" != "yakkety" ]];
+    if [[ "$DISTRIB_CODENAME" != "xenial" && "$DISTRIB_CODENAME" != "yakkety" && "$DISTRIB_CODENAME" != "trusty" ]];
     then
-        echo "Only Ubuntu 16.04 (Xenial) and 16.10 (Yakkety) are supported."
+        echo "Only Ubuntu 14.04, 16.04, and 16.10 are supported."
         echo "You are running $DISTRIB_RELEASE ($DISTRIB_CODENAME) [`uname`]"
         exit 1;
     fi
