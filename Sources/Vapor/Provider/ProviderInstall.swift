4edea89e405f3f370945c63949a56316a33cba63
tanner0101 <me@tanner.xyz>
2017-04-21 16:30:30 +0200

    update provider installer

--- Sources/Vapor/Provider/ProviderInstall.swift
+++ Sources/Vapor/Provider/ProviderInstall.swift
@@ -14,7 +14,7 @@ public final class ProviderInstall: Command {
         _ providers: [Provider],
         publicDir: String,
         viewsDir: String
-    ) {
+        ) {
         self.console = console
         self.providers = providers
         self.publicDir = publicDir
@@ -22,9 +22,19 @@ public final class ProviderInstall: Command {
     }
     
     public func run(arguments: [String]) throws {
-        for provider in providers {
+        console.print("This command copies resource files from your providers")
+        console.print("into your root project directories.")
+        console.warning("Any files with the same name will be replaced.")
+        console.print("You have \(providers.count) providers that will be installed.")
+        guard console.confirm("Would you like to continue?") else {
+            console.warning("Install cancelled.")
+            return
+        }
+        
+        for (i, provider) in providers.enumerated() {
             let type = type(of: provider)
-            console.info("Installing \(type)")
+            console.info("[\(i + 1)/\(providers.count)]", newLine: false)
+            console.print(" Installing \(type.repositoryName)")
             
             guard let root = type.providedDirectory else {
                 console.error("Could not find directory for \(type)")
@@ -34,13 +44,29 @@ public final class ProviderInstall: Command {
             let publicDir = root + type.publicDir.finished(with: "/")
             let viewsDir = root + type.viewsDir.finished(with: "/")
             
-            console.info("Copying public files...")
-            _ = try console.backgroundExecute(program: "/bin/sh", arguments: ["-c", "cp -rf \(publicDir)* \(publicDir)"])
+            var dirty = false
             
-            console.info("Copying resource files...")
-            _ = try console.backgroundExecute(program: "/bin/sh", arguments: ["-c", "cp -rf \(viewsDir)* \(viewsDir)"])
+            do {
+                _ = try console.backgroundExecute(program: "/bin/sh", arguments: ["-c", "cp -rf \(publicDir)* \(self.publicDir)"])
+                console.print("Copied public files")
+                dirty = true
+            } catch {
+                //
+            }
             
-            console.success("Installed \(type)")
+            do {
+                _ = try console.backgroundExecute(program: "/bin/sh", arguments: ["-c", "cp -rf \(viewsDir)* \(self.viewsDir)"])
+                console.print("Copied resource files")
+                dirty = true
+            } catch {
+                //
+            }
+            
+            if dirty {
+                console.success("Installed \(type.repositoryName)")
+            } else {
+                console.print("Nothing to install")
+            }
         }
     }
 }
