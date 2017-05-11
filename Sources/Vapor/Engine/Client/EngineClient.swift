fb3fc99c1a9783cab5320725aa8075e3527c1ec3
tanner0101 <me@tanner.xyz>
2017-05-11 22:30:00 +0100

    scheme fix

--- Sources/Vapor/Engine/Client/EngineClient.swift
+++ Sources/Vapor/Engine/Client/EngineClient.swift
@@ -46,7 +46,14 @@ public final class EngineClient: ClientProtocol {
     
     public func respond(to request: Request) throws -> Response {
         if proxy != nil {
-            request.uri.path = "http://\(hostname):\(port)" + request.uri.path
+            let scheme: String
+            switch securityLayer {
+            case .none:
+                scheme = "http"
+            case .tls:
+                scheme = "https"
+            }
+            request.uri.path = "\(scheme)://\(hostname):\(port)" + request.uri.path
         }
         return try client.respond(to: request)
     }
