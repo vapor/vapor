e83dd82bdb8c73ecc4d750d6759ed422fe427272
tanner0101 <me@tanner.xyz>
2017-05-15 18:53:07 +0100

    remove unnecessary files

--- Tests/VaporTests/DropletTests.swift
+++ Tests/VaporTests/DropletTests.swift
@@ -17,8 +17,7 @@ class DropletTests: XCTestCase {
         ("testProxy", testProxy),
         ("testDropletProxy", testDropletProxy),
         ("testWebsockets", testWebsockets),
-        ("testWebsocketsTLS", testWebsocketsTLS),
-        ("testFoundationClient", testFoundationClient)
+        ("testWebsocketsTLS", testWebsocketsTLS)
     ]
 
     func testData() {
