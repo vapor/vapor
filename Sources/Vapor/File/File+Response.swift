ba8da16f36b1177968ff65511afbbf8df24f5ff4
Logan Wright <logan.william.wright@gmail.com>
2017-06-06 11:58:14 +0100

    comments and rfc links

--- Sources/Vapor/File/File+Response.swift
+++ Sources/Vapor/File/File+Response.swift
@@ -2,6 +2,20 @@ import Foundation
 import HTTP
 
 public extension Response {
+    /// If you are simply looking to serve files from your public directory,
+    /// it may be useful to look at 'FileMiddleware' instead.
+    ///
+    /// Use this to initialize a file response for the exact file path.
+    /// If using from a public folder for example, the file name should be appended
+    /// to the public directory, ie: `drop.publicDir + "myFile.cool"`
+    ///
+    /// If none match represents an ETag that will be used to check if the file has
+    /// changed since the last load by the client. This allows clients like browsers
+    /// to cache their files and avoid downloading resources unnecessarily.
+    /// Most often calculated w/
+    /// https://tools.ietf.org/html/rfc7232#section-3.2
+    ///
+    /// For an example of how this is used, look at 'FileMiddleware'
     public convenience init(filePath: String, ifNoneMatch: String? = nil, chunkSize: Int = 2048) throws {
         guard
             let attributes = try? Foundation.FileManager.default.attributesOfItem(atPath: filePath),
