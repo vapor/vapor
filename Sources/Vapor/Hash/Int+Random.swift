4be0251b92f981580f22b98b9ba05cf837355444
Vitaliy V. Shopov <shopov.vitaliy@gmail.com>
2017-06-26 22:38:58 +1000

    Fix Int.random for Linux

--- Sources/Vapor/Hash/Int+Random.swift
+++ Sources/Vapor/Hash/Int+Random.swift
@@ -1,11 +1,20 @@
+import Foundation
 import libc
 
 extension Int {
     /// Generates a random number between (and inclusive of)
-    /// the given minimum and maxiumum.
+    /// the given minimum and maximum.
+    static private var randomInitialized: Bool = false
+
     public static func random(min: Int, max: Int) -> Int {
         let top = max - min + 1
         #if os(Linux)
+            if !Int.randomInitialized {
+                let current = Date().timeIntervalSinceReferenceDate
+                let salt = current.truncatingRemainder(dividingBy: 1) * 100000000
+                libc.srand(UInt32(current + salt))
+                Int.randomInitialized = true
+            }
             return Int(libc.random() % top) + min
         #else
             return Int(arc4random_uniform(UInt32(top))) + min
