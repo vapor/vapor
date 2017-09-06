48e11758e9723211126f4c72b48d5758ea72aef2
Joannis Orlandos <joannis@orlandos.nl>
2017-09-06 18:12:18 +0200

    test JWT edge cases

--- Tests/JWTTests/JWSTests.swift
+++ Tests/JWTTests/JWSTests.swift
@@ -2,21 +2,41 @@ import XCTest
 @testable import JWT
 
 class JWSTests: XCTestCase {
-    func testBasics() throws {
+    func testSuccess(for header: Header) throws {
         let secret = Data("aaaaaaaabvbcas".utf8)
         
         let signature = try JSONWebSignature(headers: [.hs256()], payload: AuthenticationMessage(token: "test"), secret: secret).sign()
         
-        let decoded = try JSONWebSignature<AuthenticationMessage>(from: signature, verifyingWith: secret)
+        let signedString = try JSONWebSignature(headers: [.hs256()], payload: AuthenticationMessage(token: "test"), secret: secret).signedString()
+        
+        XCTAssertEqual(Data(signedString.utf8), signature)
+        
+        var decoded = try JSONWebSignature<AuthenticationMessage>(from: signature, verifyingWith: secret)
+        XCTAssertEqual(decoded.payload.token, "test")
+        
+        decoded = try JSONWebSignature<AuthenticationMessage>(from: signedString, verifyingWith: secret)
         XCTAssertEqual(decoded.payload.token, "test")
     }
     
-    func testInvalidSignature() throws {
+    func invalidSignature(for header: Header) throws {
         let secret = Data("aaaaaaaabvbcas".utf8)
         
-        let signature = try JSONWebSignature(headers: [.hs256()], payload: AuthenticationMessage(token: "test"), secret: secret).sign()
+        let signature = try JSONWebSignature(headers: [header], payload: AuthenticationMessage(token: "test"), secret: secret).sign()
         
         XCTAssertThrowsError(try JSONWebSignature<AuthenticationMessage>(from: signature, verifyingWith: Data("dsadasdsad".utf8)))
+    }
+    
+    func testBasics() throws {
+        let headers: [Header] = [
+            .hs256(),
+            .hs384(),
+            .hs512()
+        ]
+        
+        for header in headers {
+            try testSuccess(for: header)
+            try invalidSignature(for: header)
+        }
     }
     
     static var allTests: [(String, (JWSTests) -> () throws -> Void)] = [
