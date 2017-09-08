f1d8a4dc18c0c9dc293543be6558a4749f8ee711
Joannis Orlandos <joannis@orlandos.nl>
2017-09-08 19:24:29 +0200

    Crypto cleanup

--- Sources/Crypto/StreamHasher.swift
+++ Sources/Crypto/StreamHasher.swift
@@ -4,11 +4,10 @@ import Core
 /// Hashes the contents of a byte stream
 ///
 /// When done hashing the stream, call `complete` to receive the hash and reset the hash to it's original state
-public class ByteStreamHasher<H: Hash> : Core.Stream {
+public class ByteStreamHasher<H: Hash> : Core.InputStream {
     /// See `InputStream` for details
     public func inputStream(_ input: ByteBuffer) {
         context.update(input)
-        self.outputStream?(input)
     }
     
     /// Creates a new ByteStreamHasher that can hash a stream of bytes
@@ -27,15 +26,28 @@ public class ByteStreamHasher<H: Hash> : Core.Stream {
     /// `ByteStreamHasher` accepts byte streams
     public typealias Input = ByteBuffer
     
-    /// The output is equal to the input
-    public typealias Output = ByteBuffer
-    
-    /// This handler will receive the hash's raw input
-    public var outputStream: OutputHandler?
-    
     /// Unused. Can be used to receive errors, although hashes don't generate errors
     public var errorStream: ErrorHandler?
     
     /// The hash context
     let context = H()
+}
+
+/// Hashes the contents of a byte stream
+///
+/// When done hashing the stream, call `complete` to receive the hash and reset the hash to it's original state
+///
+/// Cascades the inputstream to the output stream without any changes.
+public final class PassthroughByteStreamHasher<H: Hash> : ByteStreamHasher<H>, Core.OutputStream {
+    /// See `InputStream` for details
+    public override func inputStream(_ input: ByteBuffer) {
+        super.inputStream(input)
+        self.outputStream?(input)
+    }
+    
+    /// The output is equal to the input
+    public typealias Output = ByteBuffer
+    
+    /// This handler will receive the hash's raw input
+    public var outputStream: OutputHandler?
 }
