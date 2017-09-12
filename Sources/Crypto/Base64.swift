7d993959f982a9f8cd54fc323028f04db59bb861
Joannis Orlandos <joannis@orlandos.nl>
2017-09-12 09:41:58 +0200

    restructure and comment Base64

--- Sources/Crypto/Base64.swift
+++ Sources/Crypto/Base64.swift
@@ -27,19 +27,25 @@ extension Base64 {
     public func inputStream(_ input: ByteBuffer) {
         var input = input
         
+        // Continues processing the `ByteBuffer` at `input`
         func process() {
             self.remainder = []
             
             do {
+                // Process the bytes into the local buffer `pointer`
                 let (complete, capacity, consumed) = try Self.process(input, toPointer: pointer, capacity: allocatedCapacity, finish: false)
                 self.currentCapacity = capacity
                 
                 // Swift doesn't recognize that Output == ByteBuffer
+                // Create a buffer referencing the ouput pointer and the outputted capacity
                 let writeBuffer: Output = ByteBuffer(start: pointer, count: capacity) as! Self.Output
                 
+                // Write the output buffer to the output stream
                 self.outputStream?(writeBuffer)
                 
+                // If processing is complete
                 guard complete else {
+                    // Append any unprocessed data to the remainder storage
                     remainder.append(contentsOf: ByteBuffer(start: input.baseAddress?.advanced(by: consumed), count: input.count &- consumed))
                     return
                 }
@@ -48,19 +54,29 @@ extension Base64 {
             }
         }
         
+        // If the remainder from previous processing attempts is not empty
         guard remainder.count == 0 else {
-            let newPointer = MutableBytesPointer.allocate(capacity: remainder.count &+ input.count)
-            newPointer.initialize(to: 0, count: remainder.count &+ input.count)
+            // Create a new buffer for the input + the remainder
+            let newPointerLength = remainder.count &+ input.count
+            let newPointer = MutableBytesPointer.allocate(capacity: newPointerLength)
+            newPointer.initialize(to: 0, count: newPointerLength)
             
-            if input.count > 0 {
-                guard let inputPointer = input.baseAddress else {
-                    return
-                }
-                
-                newPointer.assign(from: remainder, count: remainder.count)
+            defer {
+                newPointer.deinitialize(count: newPointerLength)
+                newPointer.deallocate(capacity: newPointerLength)
+            }
+            
+            // Set the remainder
+            newPointer.assign(from: remainder, count: remainder.count)
+            
+            // Appends the input
+            if input.count > 0, let inputPointer = input.baseAddress {
                 newPointer.advanced(by: remainder.count).assign(from: inputPointer, count: input.count)
             }
             
+            input = ByteBuffer(start: newPointer, count: newPointerLength)
+            
+            // Processes this buffer
             process()
             return
         }
