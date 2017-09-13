e32a2b9ed98c1fe04b1fa43e3ee8e25bb51537b9
Joannis Orlandos <joannis@orlandos.nl>
2017-09-13 14:48:45 +0200

    type-safe controllers implemented

--- Sources/Vapor/Controller.swift
+++ Sources/Vapor/Controller.swift
@@ -15,10 +15,27 @@ open class AsyncController<Input: Codable, Output: Codable> {
     /// Responder closure
     public typealias Closure = (TypeSafeRequest<Input>) throws -> Future<TypeSafeResponse<Output>>
     
-    /// Registers a route handler at the supplied path.
+    let container: Application
+    
+    var routes = [Route]()
+    
+    init(for app: Application) {
+        self.container = app
+    }
+    
     @discardableResult
     public func on(_ method: Method, to path: PathComponentRepresentable..., use closure: @escaping Closure) -> Route {
-        fatalError()
+        let responder = BasicAsyncResponder { (request: Request) -> Future<Response> in
+            let typeSafeRequest = try TypeSafeRequest<Input>(request: request, for: self.container)
+            
+            return try closure(typeSafeRequest).map { response in
+                return try response.makeResponse(for: request)
+            }
+        }
+        
+        let route = Route(method: method, path: path.makePathComponents(), responder: responder)
+        routes.append(route)
+        return route
     }
 }
 
@@ -26,9 +43,24 @@ open class SyncController<Input: Codable, Output: Codable> {
     /// Responder closure
     public typealias Closure = (TypeSafeRequest<Input>) throws -> TypeSafeResponse<Output>
     
-    /// Registers a route handler at the supplied path.
+    let container: Application
+    
+    var routes = [Route]()
+    
+    init(for app: Application) {
+        self.container = app
+    }
+    
     @discardableResult
     public func on(_ method: Method, to path: PathComponentRepresentable..., use closure: @escaping Closure) -> Route {
-        fatalError()
+        let responder = BasicSyncResponder { (request: Request) -> Response in
+            let typeSafeRequest = try TypeSafeRequest<Input>(request: request, for: self.container)
+            
+            return try closure(typeSafeRequest).makeResponse(for: request)
+        }
+        
+        let route = Route(method: method, path: path.makePathComponents(), responder: responder)
+        routes.append(route)
+        return route
     }
 }
