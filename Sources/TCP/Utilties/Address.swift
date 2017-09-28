2ada302f62d1c93b687cb9ce85ab59b30773fd25
Joannis Orlandos <joannis@orlandos.nl>
2017-09-28 08:53:20 +0200

    more comments

--- Sources/TCP/Utilties/Address.swift
+++ Sources/TCP/Utilties/Address.swift
@@ -1,37 +1,44 @@
 import libc
 
+/// A socket address
 public struct Address {
+    /// The raw underlying storage
     public let storage: sockaddr_storage
     
+    /// Creates a new socket address
     init(storage: sockaddr_storage) {
         self.storage = storage
     }
-    
 }
 
 extension Address: Equatable {
+    /// Compares 2 addresses to be equal
     public static func ==(lhs: Address, rhs: Address) -> Bool {
         let lhs = lhs.storage
         let rhs = rhs.storage
         
+        // They must have the same family
         guard lhs.ss_family == rhs.ss_family else {
             return false
         }
         
         switch numericCast(lhs.ss_family) as UInt32 {
         case numericCast(AF_INET):
+            // If the family is IPv4, compare the 2 as IPv4
             return lhs.withIn_addr { lhs in
                 return rhs.withIn_addr { rhs in
                     return memcmp(&lhs, &rhs, MemoryLayout<in6_addr>.size) == 0
                 }
             }
         case numericCast(AF_INET6):
+            // If the family is IPv6, compare the 2 as IPv6
             return lhs.withIn6_addr { lhs in
                 return rhs.withIn6_addr { rhs in
                     return memcmp(&lhs, &rhs, MemoryLayout<in6_addr>.size) == 0
                 }
             }
         default:
+            // Impossible scenario
             fatalError()
         }
     }
@@ -47,18 +54,21 @@ extension Address {
         
         switch numericCast(self.storage.ss_family) as UInt32 {
         case numericCast(AF_INET):
+            // Extract the port from the struct cast as sockaddr_in
             val = withUnsafePointer(to: &copy) { pointer -> UInt16 in
                 pointer.withMemoryRebound(to: sockaddr_in.self, capacity: 1) { pointer -> UInt16 in
                     return pointer.pointee.sin_port
                 }
             }
         case numericCast(AF_INET6):
+            // Extract the port from the struct cast as sockaddr_in6
             val = withUnsafePointer(to: &copy) { pointer -> UInt16 in
                 pointer.withMemoryRebound(to: sockaddr_in6.self, capacity: 1) { pointer -> UInt16 in
                     return pointer.pointee.sin6_port
                 }
             }
         default:
+            // Impossible scenario
             fatalError()
         }
         
@@ -72,13 +82,19 @@ extension Address {
         
         switch numericCast(self.storage.ss_family) as UInt32 {
         case numericCast(AF_INET):
+            // Extract the remote IPv4 address
             maxStringLength = socklen_t(INET_ADDRSTRLEN)
+            
+            // Allocate an IPv4 address
             stringData = UnsafeMutablePointer<Int8>.allocate(capacity: numericCast(maxStringLength))
             
             _ = self.storage.withIn_addr { address in
                 inet_ntop(numericCast(self.storage.ss_family), &address, stringData, maxStringLength)
             }
         case numericCast(AF_INET6):
+            // Extract the remote IPv6 address
+            
+            // Allocate an IPv6 address
             maxStringLength = socklen_t(INET6_ADDRSTRLEN)
             stringData = UnsafeMutablePointer<Int8>.allocate(capacity: numericCast(maxStringLength))
             
@@ -86,10 +102,12 @@ extension Address {
                 inet_ntop(numericCast(self.storage.ss_family), &address, stringData, maxStringLength)
             }
         default:
+            // Impossible scenario
             fatalError()
         }
         
         defer {
+            // Clean up
             stringData.deallocate(capacity: numericCast(maxStringLength))
         }
         
@@ -99,6 +117,7 @@ extension Address {
 }
 
 extension sockaddr_storage {
+    // Accesses the sockaddr_storage as sockaddr_in
     fileprivate func withIn_addr<T>(call: ((inout in_addr)->(T))) -> T {
         var copy = self
         
@@ -111,6 +130,7 @@ extension sockaddr_storage {
         }
     }
     
+    // Accesses the sockaddr_storage as sockaddr_in6
     fileprivate func withIn6_addr<T>(call: ((inout in6_addr)->(T))) -> T {
         var copy = self
         
