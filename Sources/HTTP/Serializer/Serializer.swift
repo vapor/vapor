07097a7e01a9e26d31bcd4d5ad86457fa1a317b1
Joannis Orlandos <joannis@orlandos.nl>
2017-10-27 03:11:27 +0200

    Remove unused serialization "helper" functions

--- Sources/HTTP/Serializer/Serializer.swift
+++ Sources/HTTP/Serializer/Serializer.swift
@@ -7,42 +7,12 @@ import Foundation
 internal protocol Serializer: Async.Stream { }
 
 extension Serializer {
-    internal func serialize(_ body: Body) -> DispatchData {
+    internal func serialize(_ body: Body) -> Data {
         switch body.storage {
         case .dispatchData(let data):
+            return Data(data)
+        case .data(let data):
             return data
-        case .data(_):
-            return body.withUnsafeBytes { (pointer: BytesPointer) in
-                let bodyRaw = UnsafeRawBufferPointer(
-                    start: UnsafeRawPointer(pointer),
-                    count: body.count
-                )
-                return DispatchData(bytes: bodyRaw)
-            }
-        }
-    }
-    
-    internal func serialize(_ headers: Headers) -> DispatchData {
-        var data = Data()
-        
-        // Magic numer `64` is reserves space per header (seems sensible)
-        data.reserveCapacity(headers.storage.count * 64)
-        
-        for (name, value) in headers {
-            data.append(contentsOf: name.original.utf8)
-            data.append(headerKeyValueSeparator)
-            data.append(contentsOf: value.utf8)
-            data.append(eol)
-        }
-        data.append(eol)
-        
-        return data.withUnsafeBytes { (pointer: BytesPointer) in
-            let buffer = UnsafeRawBufferPointer(
-                start: UnsafeRawPointer(pointer),
-                count: data.count
-            )
-            
-            return DispatchData(bytes: buffer)
         }
     }
 }
