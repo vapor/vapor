bf2d9d9831f0b7cc49996d9fd2ae68de85189b10
John Connolly <connoljo2@gmail.com>
2017-11-02 19:44:27 -0300

    Fixed bugs with pipelining, changed api and concatenating commands now.

--- Sources/Redis/Pipeline.swift
+++ Sources/Redis/Pipeline.swift
@@ -1,52 +1,23 @@
 import Async
-import Bits
-import Foundation
 
-/// A pipeline which wraps a redis client
-public final class Pipeline<DuplexByteStream: Async.Stream> where DuplexByteStream.Input == ByteBuffer, DuplexByteStream.Output == ByteBuffer, DuplexByteStream: ClosableStream {
-    
-    public let client: RedisClient<DuplexByteStream>
-    private var commands = [RedisData]()
-    
-    
-    init(client: RedisClient<DuplexByteStream>) {
-        self.client = client
-    }
-    
-    /// Enqueues a command to be executed.  The commands will not be executed until execute() is
-    /// is called
-    @discardableResult
-    public func enqueue(command: String, arguments: [RedisData]? = nil) throws -> Pipeline<DuplexByteStream> {
-        if client.isSubscribed {
-            throw RedisError(.cannotReuseSubscribedClients)
+extension RedisClient {
+    /// Accepts an array of commands, to be sent in one request.
+    public func pipeline(_ commands: () -> ([RedisData])) -> Future<[RedisData]> {
+        if isSubscribed {
+            return Future(error: RedisError(.cannotReuseSubscribedClients))
         }
         
-        commands.append(RedisData.array([.bulkString(command)] + (arguments ?? [])))
-        return self
-    }
-    
-    /// Executes a series of commands and returns a future for the responses
-    @discardableResult
-    public func execute() throws -> Future<[RedisData]> {
-        defer {
-            commands = []
-        }
-        
-        guard commands.count > 0 else {
-            return Future(error: RedisError(.pipelineCommandsRequired))
-        }
+        let commands = commands()
+        dataSerializer.inputStream(commands)
         
         let promises = commands.map { _ in
             Promise<RedisData>()
         }
         
-        client.dataParser.responseQueue.append(contentsOf: promises)
-        
-        commands.forEach(client.dataSerializer.inputStream)
+        dataParser.responseQueue.append(contentsOf: promises)
         
         return promises
             .map { $0.future }
             .flatten()
     }
-    
 }
