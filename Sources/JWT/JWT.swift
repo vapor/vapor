f247680da7e8f3caa84a476687dc49ad74ef8055
Joannis Orlandos <joannis@orlandos.nl>
2017-11-03 14:42:05 +0100

    rename the JWT protocols

--- Sources/JWT/JWT.swift
+++ Sources/JWT/JWT.swift
@@ -3,10 +3,17 @@ import Foundation
 /// A JWT is a Publically Readable set of claims
 ///
 /// Each variable represents a claim
-public protocol JWT: Codable {}
+public protocol JWTPayload: Codable {
+    
+}
 
-/// Identifies by which application a JWT is issued
-public protocol IssuerClaim: JWT {
+/// A claim is a codable top-level property that can be verified against the current circumstances
+///
+/// Multiple claims form a payload
+public protocol Claim: Codable {}
+
+/// Identifies by which application a Claim is issued
+public protocol IssuerClaim: Claim {
     /// The identifier (or URI) of the issuer of the token
     var iss: String { get }
 }
@@ -14,37 +21,37 @@ public protocol IssuerClaim: JWT {
 /// Identifies the subject of a claim
 ///
 /// Such as the user in an authentication token
-public protocol SubjectClaim: JWT {
+public protocol SubjectClaim: Claim {
     /// The claim's subject's identifier
     var sub: String { get }
 }
 
-/// Identifies the destination application of the JWT
-public protocol AudienceClaim: JWT {
+/// Identifies the destination application of the Claim
+public protocol AudienceClaim: Claim {
     /// The identifier (or URI) of the destination application
     var aud: String { get }
 }
 
-/// Identifies the final date when a JWT becomes invalid
-public protocol ExpirationClaim: JWT {
+/// Identifies the final date when a Claim becomes invalid
+public protocol ExpirationClaim: Claim {
     /// The expiration `Date` of this token
     var exp: Date { get }
 }
 
-/// Identifies the first date at which a JWT becomes valid
-public protocol NotBeforeClaim: JWT {
+/// Identifies the first date at which a Claim becomes valid
+public protocol NotBeforeClaim: Claim {
     /// The first `Date` at which this claim becomes valid
     var nbf: Date { get }
 }
 
-/// Identifies the date at which a JWT was issued
-public protocol IssuedAtClaim: JWT {
+/// Identifies the date at which a Claim was issued
+public protocol IssuedAtClaim: Claim {
     /// The `Date` at which this claim was issued
     var iat: Date { get }
 }
 
-/// Identifies the date at which a JWT was issued
-public protocol IDClaim: JWT {
+/// Identifies the date at which a Claim was issued
+public protocol IDClaim: Claim {
     /// A unique identifier for this claim
     var jti: String { get }
 }
