63b188b31126af4483d635780f39170489633b98
Joannis Orlandos <joannis@orlandos.nl>
2017-11-21 21:58:20 +0100

    reverse stack overflow fix work

--- Sources/MySQL/Parsing/PacketParser.swift
+++ Sources/MySQL/Parsing/PacketParser.swift
@@ -15,8 +15,6 @@ internal final class PacketParser : Async.Stream {
     init() {}
     
     func inputStream(_ input: MutableByteBuffer) {
-        var input = input
-        
         // If there's no input pointer, throw an error
         guard var pointer = input.baseAddress else {
             errorStream?(MySQLError(.invalidPacket))
@@ -27,13 +25,11 @@ internal final class PacketParser : Async.Stream {
         var length = input.count
         
         // Parses the buffer
-        //
-        // returns true if parsing needs to continue
-        func parseInput(into buffer: MutableByteBuffer, alreadyContaining containing: Int, sequenceId: UInt8) -> Bool {
+        func parseInput(into buffer: MutableByteBuffer, alreadyContaining containing: Int, sequenceId: UInt8) {
             // If there's no input pointer, throw an error
             guard let destination = buffer.baseAddress?.advanced(by: containing) else {
                 errorStream?(MySQLError(.invalidPacket))
-                return false
+                return
             }
             
             // The rest of the packet
@@ -51,11 +47,11 @@ internal final class PacketParser : Async.Stream {
                 self.buffer = nil
                 
                 guard length &- needing > 0 else {
-                    return false
+                    return
                 }
                 
-                input = MutableByteBuffer(start: pointer.advanced(by: needing), count: length &- needing)
-                return true
+                self.inputStream(MutableByteBuffer(start: pointer.advanced(by: needing), count: length &- needing))
+                
                 // If we have exactly enough or too little
             } else {
                 memcpy(destination, pointer, length)
@@ -64,7 +60,7 @@ internal final class PacketParser : Async.Stream {
                 guard containing &+ length == buffer.count else {
                     // update the partial packet
                     self.buffer = (buffer, containing &+ length, sequenceId)
-                    return false
+                    return
                 }
                 
                 // Packet is complete, send it up
@@ -72,55 +68,48 @@ internal final class PacketParser : Async.Stream {
                 output(packet)
                 
                 self.buffer = nil
-                return false
             }
         }
         
-        var bufferSize: Int
-        var sequenceId: UInt8
-        var buffer: MutableByteBuffer
-        
-        repeat {
-            // If an existing packet it building
-            if let (_buffer, _containing, _sequenceId) = self.buffer {
-                // Continue parsing from the start
-                
-                buffer = _buffer
-                bufferSize = _containing
-                sequenceId = _sequenceId
-            } else {
-                // at least 4 packet bytes for new packets
-                // TODO: internal 3-byte buffer like MongoKitten's for this odd scenario
-                guard input.count > 3 else {
-                    if input.count == 0 {
-                        return
-                    }
-                    
-                    errorStream?(MySQLError(.invalidPacket))
+        // If an existing packet it building
+        if let (buffer, containing, sequenceId) = self.buffer {
+            // Continue parsing from the start
+            parseInput(into: buffer, alreadyContaining: containing, sequenceId: sequenceId)
+            // If a new packet is coming
+        } else {
+            // at least 4 packet bytes for new packets
+            // TODO: internal 3-byte buffer like MongoKitten's for this odd scenario
+            guard input.count > 3 else {
+                if input.count == 0 {
                     return
                 }
                 
-                // take the first 3 bytes
-                let byte0 = UInt32(input[0]).littleEndian
-                let byte1 = UInt32(input[1]).littleEndian << 8
-                let byte2 = UInt32(input[2]).littleEndian << 16
-                
-                // Parse buffer size
-                bufferSize = Int(byte0 | byte1 | byte2)
-                sequenceId = input[3]
-                
-                // Build a buffer size
-                let bufferPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
-                buffer = MutableByteBuffer(start: bufferPointer, count: bufferSize)
-                
-                // Advance the pointer by the header size
-                pointer = pointer.advanced(by: 4)
-                
-                // Decrease the pointer size by the header size
-                length = length &- 4
+                errorStream?(MySQLError(.invalidPacket))
+                return
             }
+            
+            // take the first 3 bytes
+            let byte0 = UInt32(input[0]).littleEndian
+            let byte1 = UInt32(input[1]).littleEndian << 8
+            let byte2 = UInt32(input[2]).littleEndian << 16
+            
+            // Parse buffer size
+            let bufferSize = Int(byte0 | byte1 | byte2)
+            let sequenceId = input[3]
+            
+            // Build a buffer size
+            let bufferPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+            let buffer = MutableByteBuffer(start: bufferPointer, count: bufferSize)
+            
+            // Advance the pointer by the header size
+            pointer = pointer.advanced(by: 4)
+            
+            // Decrease the pointer size by the header size
+            length = length &- 4
+            
             // Parse the packet contents
-        } while parseInput(into: buffer, alreadyContaining: 0, sequenceId: sequenceId)
+            parseInput(into: buffer, alreadyContaining: 0, sequenceId: sequenceId)
+        }
     }
 }
 
