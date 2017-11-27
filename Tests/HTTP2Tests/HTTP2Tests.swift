198a6e68d6d1bfdc534662593bf54641c71e442d
Joannis Orlandos <joannis@orlandos.nl>
2017-11-27 21:03:15 +0100

    make http2 client test linux only

--- Tests/HTTP2Tests/HTTP2Tests.swift
+++ Tests/HTTP2Tests/HTTP2Tests.swift
@@ -9,16 +9,20 @@ public class HTTP2Tests: XCTestCase {
     ]
     
     func testClient() throws {
-        let queue = DispatchQueue(label: "http2.client")
-        
-        let response = try HTTP2Client.connect(hostname: "google.com", worker: queue).flatMap { client -> Future<Response> in
-            let request = Request(method: .get, uri: "/", headers: [
-                .host: "www.google.com"
-            ], body: Body())
+        #if os(Linux)
+            let queue = DispatchQueue(label: "http2.client")
             
-            return try client.send(request)
-        }.blockingAwait()
-        
-        print(response)
+            let response = try HTTP2Client.connect(hostname: "google.com", worker: queue).flatMap { client -> Future<Response> in
+                let request = Request(method: .get, uri: "/", headers: [
+                    .host: "www.google.com"
+                ], body: Body())
+                
+                return try client.send(request)
+            }.blockingAwait()
+            
+            print(response)
+        #else
+            print("WARNING: For macOS, HTTP/2 needs Apple's ALPN support or an OpenSSL dependency")
+        #endif
     }
 }
