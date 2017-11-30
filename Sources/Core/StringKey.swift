1c8044dfa5e5cdce565f409c18f4afaf31a38ccf
Tanner Nelson <me@tanner.xyz>
2017-11-30 15:07:01 -0500

    additional test cleanup plus coder extensions

--- Sources/Core/StringKey.swift
+++ Sources/Core/StringKey.swift
@@ -1,15 +1,32 @@
-public struct StringKey: CodingKey {
+/// A basic string coding key
+public struct BasicKey: CodingKey {
+    /// the string value
     public var stringValue: String
-    public var intValue: Int? {
-        return Int(stringValue)
+
+    /// the optional int value
+    public var intValue: Int?
+
+    /// create a new string basic key
+    public init(_ string: String) {
+        self.stringValue = string
     }
 
+    /// create a new int basic key
+    /// note: keys with int values can trigger
+    /// array behavior in decoders
+    public init(_ int: Int) {
+        self.intValue = int
+        self.stringValue = int.description
+    }
+
+    /// See CodingKey.init
     public init?(stringValue: String) {
         self.stringValue = stringValue
     }
 
+    /// See CodingKey.init
     public init?(intValue: Int) {
+        self.intValue = intValue
         self.stringValue = intValue.description
     }
 }
-
--- Sources/Leaf/Data/LeafUnkeyedEncoder.swift
+++ Sources/Leaf/Data/LeafUnkeyedEncoder.swift
@@ -5,6 +5,11 @@ internal final class LeafUnkeyedEncoder: UnkeyedEncodingContainer {
     var codingPath: [CodingKey]
     var partialData: PartialLeafData
 
+    var index: CodingKey {
+        defer { count += 1 }
+        return BasicKey(count)
+    }
+
     init(codingPath: [CodingKey], partialData: PartialLeafData) {
         self.codingPath = codingPath
         self.partialData = partialData
@@ -12,45 +17,41 @@ internal final class LeafUnkeyedEncoder: UnkeyedEncodingContainer {
     }
 
     func encodeNil() throws {
-        partialData.set(to: .null, at: codingPath)
+        partialData.set(to: .null, at: codingPath + [index])
     }
 
     func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type) -> KeyedEncodingContainer<NestedKey>
         where NestedKey: CodingKey
     {
-        let container = LeafKeyedEncoder<NestedKey>(codingPath: codingPath, partialData: partialData)
+        let container = LeafKeyedEncoder<NestedKey>(codingPath: codingPath + [index], partialData: partialData)
         return KeyedEncodingContainer(container)
     }
 
     func nestedUnkeyedContainer() -> UnkeyedEncodingContainer {
-        return LeafUnkeyedEncoder(codingPath: codingPath, partialData: partialData)
+        return LeafUnkeyedEncoder(codingPath: codingPath + [index], partialData: partialData)
     }
 
     func superEncoder() -> Encoder {
-        let index = ArrayKey(index: count)
-        defer { count += 1 }
         return _LeafEncoder(partialData: partialData, codingPath: codingPath + [index])
     }
 
     func encode(_ value: Bool) throws {
-        partialData.set(to: .bool(value), at: codingPath)
+        partialData.set(to: .bool(value), at: codingPath + [index])
     }
 
     func encode(_ value: Int) throws {
-        partialData.set(to: .int(value), at: codingPath)
+        partialData.set(to: .int(value), at: codingPath + [index])
     }
 
     func encode(_ value: Double) throws {
-        partialData.set(to: .double(value), at: codingPath)
+        partialData.set(to: .double(value), at: codingPath + [index])
     }
 
     func encode(_ value: String) throws {
-        partialData.set(to: .string(value), at: codingPath)
+        partialData.set(to: .string(value), at: codingPath + [index])
     }
 
     func encode<T>(_ value: T) throws where T: Encodable {
-        let index = ArrayKey(index: count)
-        defer { count += 1 }
         let encoder = _LeafEncoder(partialData: partialData, codingPath: codingPath + [index])
         try value.encode(to: encoder)
     }
