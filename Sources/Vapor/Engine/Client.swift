8e62660c1d3f2175533301a63277bd11c45c0c21
tanner0101 <me@tanner.xyz>
2017-12-02 01:33:18 -0500

    comment cleanup

--- Sources/Vapor/Engine/Client.swift
+++ Sources/Vapor/Engine/Client.swift
@@ -1,12 +1,13 @@
 /// Responds to HTTP requests.
 public protocol Client: Responder {
+    /// The container to use for creating requests.
     var container: Container { get }
 }
 
 // MARK: Convenience
 
 extension Client {
-    /// Sends an HTTP request from the client using the method, url, and containter.
+    /// Sends an HTTP request from the client using the method and url.
     public func send(_ method: HTTPMethod, to url: String) -> Future<Response> {
         return then {
             let req = Request(using: self.container)
@@ -16,7 +17,7 @@ extension Client {
         }
     }
 
-    /// Sends an HTTP request from the client using the method, url, content, and containter.
+    /// Sends an HTTP request from the client using the method, url, and content.
     public func send<C>(_ method: HTTPMethod, to url: String, content: C) -> Future<Response>
         where C: Content
     {
