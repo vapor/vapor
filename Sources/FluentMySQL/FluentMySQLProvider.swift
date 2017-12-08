a5b6551e8a5e5d58ae7b3134205e54d2131b789b
Joannis Orlandos <joannis@orlandos.nl>
2017-12-08 17:04:21 +0100

    Add Fluent config helpers

---
+++ Sources/FluentMySQL/FluentMySQLProvider.swift
@@ -0,0 +1,43 @@
+import Service
+import SQLite
+
+/// Registers and boots MySQL services.
+public final class MySQLProvider: Provider {
+    /// See Provider.repositoryName
+    public static let repositoryName = "fluent-mysql"
+    
+    /// The hostname to which connections will be connected
+    let hostname: String
+    
+    /// The port to which connections will be connected
+    let port: UInt16
+    
+    /// The username to authenticate with
+    let user: String
+    
+    /// The password to authenticate with
+    let password: String?
+    
+    /// The database to select
+    let database: String
+    
+    public init(hostname: String, port: UInt16 = 3306, user: String, password: String?, database: String) {
+        self.hostname = hostname
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+    }
+    
+    /// See Provider.register
+    public func register(_ services: inout Services) throws {
+        services.register(FluentMySQLConfig())
+        services.register(MySQLDatabase.self) { container -> SQLiteDatabase in
+            let database = MySQLDatabase(hostname: hostname, port: port, user: user, password: password, database: database)
+        }
+    }
+    
+    /// See Provider.boot
+    public func boot(_ container: Container) throws {}
+}
+
