07660b6f39801452a70abac4e25194d6fc8850ac
Tanner Nelson <me@tanner.xyz>
2017-12-13 02:47:08 -0500

    cleanup tls files

---
+++ Sources/TLS/TLSSocket.swift
@@ -0,0 +1,15 @@
+import Bits
+import JunkDrawer
+
+public protocol TLSSocket: DispatchSocket {
+    func read(max: Int, into buffer: MutableByteBuffer) throws -> Int
+    func write(max: Int, from buffer: ByteBuffer) throws -> Int
+    func close()
+}
+
+/// MARK: ALPN
+
+public protocol ALPNSupporting: TLSSocket {
+    var ALPNprotocols: [String] { get set }
+    var selectedProtocol: String? { get }
+}
