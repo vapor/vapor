f7665a29922abb08e5976d82d22f1938830834c1
Joannis Orlandos <joannis@orlandos.nl>
2017-12-29 00:35:04 +0100

    remove testing code

--- Sources/Testing/ApplicationTester.swift
+++
@@ -1,9 +0,0 @@
-public final class ApplicationTester {
-    public init() {
-        var config = Config.default()
-        var env = Environment.detect()
-        var services = Services.default()
-        
-        
-    }
-}
