e5b1aebbb15a8c6d90c7fe8e7dab861655429109
tanner0101 <me@tanner.xyz>
2017-12-28 16:46:29 -0700

    encodable session

--- Sources/Vapor/Sessions/Session.swift
+++ Sources/Vapor/Sessions/Session.swift
@@ -4,11 +4,37 @@ public final class Session {
     public var cookie: Cookie.Value?
 
     /// This session's data
-    public var data: [String: Encodable]
+    public var data: Encodable
 
     /// Create a new session.
     public init() {
         cookie = nil
         data = [:]
+    }
+}
+
+
+extension Session {
+    /// Convenience [String: String] accessor.
+    public subscript(_ key: String) -> String? {
+        get {
+            let dict: [String: String]
+            if let existing = data as? [String: String] {
+                dict = existing
+            } else {
+                dict = [:]
+            }
+            return dict[key]
+        }
+        set {
+            var dict: [String: String]
+            if let existing = data as? [String: String] {
+                dict = existing
+            } else {
+                dict = [:]
+            }
+            dict[key] = newValue
+            data = dict
+        }
     }
 }
