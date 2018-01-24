e7de7c0064367c150dfaaa747d12dc7ca8b0b8c3
Joannis Orlandos <joannis@orlandos.nl>
2018-01-24 18:05:32 +0100

    remove file middleware as a default middleware

--- Sources/Vapor/Middleware/MiddlewareConfig.swift
+++ Sources/Vapor/Middleware/MiddlewareConfig.swift
@@ -44,7 +44,6 @@ extension MiddlewareConfig {
     
     public static func `default`() -> MiddlewareConfig {
         var config = MiddlewareConfig()
-        config.use(FileMiddleware.self)
         config.use(DateMiddleware.self)
         config.use(ErrorMiddleware.self)
         return config
