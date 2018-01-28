6ff4bfda93e5acaaf47d5c7e0c1b5c892a7c7f2a
Aaron Edwards <aaron.j.edwards@me.com>
2018-01-28 16:40:13 -0700

    Updated to override default imlementation of new RouteBuilder additions

--- Sources/Vapor/Routing/Droplet+Routing.swift
+++ Sources/Vapor/Routing/Droplet+Routing.swift
@@ -3,7 +3,11 @@ import HTTP
 @_exported import Routing
 
 extension Droplet: RouteBuilder {
-    public func register(host: String?, method: Method, path: [String], metadata: [String: String]? = nil, responder: Responder) {
+    public func register(host: String?, method: Method, path: [String], responder: Responder) {
+        router.register(host: host, method: method, path: path, responder: responder)
+    }
+    
+    public func register(host: String?, method: Method, path: [String], metadata: [String: Any], responder: Responder) {
         router.register(host: host, method: method, path: path, metadata: metadata, responder: responder)
     }
 }
