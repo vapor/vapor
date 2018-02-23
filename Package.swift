38629e04c880a648cc307cb8a299f5f98ee82ef4
Tanner Nelson <me@tanner.xyz>
2018-02-23 01:31:18 -0500

    metadata updates

--- Package.swift
+++ Package.swift
@@ -14,37 +14,37 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
 
         // Swift wrapper for Console I/O.
-        .package(url: "https://github.com/vapor/console.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/console.git", from: "3.0.0-rc"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
 
         // Cryptography modules
-        .package(url: "https://github.com/vapor/crypto.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc"),
 
         // Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", "1.0.0-beta.2"..<"1.0.0-beta.6"),
+        .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0-rc"),
 
         // Non-blocking networking for Swift (HTTP and WebSockets).
-        .package(url: "https://github.com/vapor/engine.git", "3.0.0-beta.3"..<"3.0.0-beta.4"),
+        .package(url: "https://github.com/vapor/engine.git", from: "3.0.0-rc"),
 
         // Easy-to-use foundation for building powerful templating languages in Swift.
-        .package(url: "https://github.com/vapor/template-kit.git", "1.0.0-beta.2"..<"1.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/template-kit.git", from: "1.0.0-rc"),
 
         // Service container and configuration system.
-        .package(url: "https://github.com/vapor/service.git", "1.0.0-beta.2"..<"1.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0-rc"),
 
         // Pure Swift (POSIX) TCP and UDP non-blocking socket layer, with event-driven Server and Client.
-        .package(url: "https://github.com/vapor/sockets.git", "3.0.0-beta.3"..<"3.0.0-beta.4"),
+        .package(url: "https://github.com/vapor/sockets.git", from: "3.0.0-rc"),
 
         // Swift OpenSSL & macOS Security TLS wrapper
-        .package(url: "https://github.com/vapor/tls.git", "3.0.0-beta.3"..<"3.0.0-beta.4"),
+        .package(url: "https://github.com/vapor/tls.git", from: "3.0.0-rc"),
 
         // Extensible data validation library (email, alphanumeric, UUID, etc)
-        .package(url: "https://github.com/vapor/validation.git", "2.0.0-beta.2"..<"2.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/validation.git", from: "2.0.0-rc"),
     ],
     targets: [
         // Boilerplate
