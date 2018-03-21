5f757a9a47c494af030d1ab7f82911e140682f9d
tanner0101 <me@tanner.xyz>
2018-03-21 17:13:45 -0400

    rc.2 updates

--- Package.swift
+++ Package.swift
@@ -14,31 +14,31 @@ let package = Package(
     ],
     dependencies: [
         // 💻 APIs for creating interactive CLI tools.
-        .package(url: "https://github.com/vapor/console.git", .branch("master")),
+        .package(url: "https://github.com/vapor/console.git", from: "3.0.0-rc.2"),
 
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", .branch("master")),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc.2"),
 
         // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
-        .package(url: "https://github.com/vapor/crypto.git", .branch("master")),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc.2"),
 
         // 🗄 Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0-rc.2"),
 
         // 🚀 Non-blocking, event-driven networking for Swift (HTTP and WebSockets).
-        .package(url: "https://github.com/vapor/engine.git", .branch("master")),
+        .package(url: "https://github.com/vapor/engine.git", from: "3.0.0-rc.2"),
 
         // 🚍 High-performance trie-node router.
-        .package(url: "https://github.com/vapor/routing.git", .branch("master")),
+        .package(url: "https://github.com/vapor/routing.git", from: "3.0.0-rc.2"),
 
         // 📦 Dependency injection / inversion of control framework.
-        .package(url: "https://github.com/vapor/service.git", .branch("master")),
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0-rc.2"),
 
         // 🖋 Easy-to-use foundation for building powerful templating languages in Swift.
-        .package(url: "https://github.com/vapor/template-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/template-kit.git", from: "1.0.0-rc.2"),
 
         // ✅ Extensible data validation library (email, alphanumeric, UUID, etc)
-        .package(url: "https://github.com/vapor/validation.git", .branch("master")),
+        .package(url: "https://github.com/vapor/validation.git", from: "2.0.0-rc.2"),
     ],
     targets: [
         // Boilerplate
