f371fac686a54fff5fc1c5ee8a8e1b2201427c36
tanner0101 <me@tanner.xyz>
2018-05-04 16:08:44 -0400

    update package swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,6 @@
 // swift-tools-version:4.0
 import PackageDescription
 
-#if os(macOS)
-    let tlsImpl: Target.Dependency = "AppleTLS"
-#else
-    let tlsImpl: Target.Dependency = "OpenSSL"
-#endif
-
 let package = Package(
     name: "Vapor",
     products: [
@@ -14,13 +8,13 @@ let package = Package(
     ],
     dependencies: [
         // 💻 APIs for creating interactive CLI tools.
-        .package(url: "https://github.com/vapor/console.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/console.git", from: "3.0.0"),
 
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
 
         // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
 
         // 🗄 Core services for creating database integrations.
         .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0"),
@@ -35,16 +29,16 @@ let package = Package(
         .package(url: "https://github.com/vapor/routing.git", from: "3.0.0"),
 
         // 📦 Dependency injection / inversion of control framework.
-        .package(url: "https://github.com/vapor/service.git", from: "1.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
 
         // 🖋 Easy-to-use foundation for building powerful templating languages in Swift.
-        .package(url: "https://github.com/vapor/template-kit.git", from: "1.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/template-kit.git", from: "1.0.0"),
 
         // 📝 Parses and serializes url-encoded form data with Codable support.
         .package(url: "https://github.com/vapor/url-encoded-form.git", from: "1.0.0"),
 
         // ✅ Extensible data validation library (email, alphanumeric, UUID, etc)
-        .package(url: "https://github.com/vapor/validation.git", from: "2.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/validation.git", from: "2.0.0"),
 
         // 🔌 Non-blocking, event-driven WebSocket client and server built on Swift NIO.
         .package(url: "https://github.com/vapor/websocket.git", from: "1.0.0"),
