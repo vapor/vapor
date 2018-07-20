12f00d2e2f240003fd1e6758b729ce8196c0706e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-20 11:13:25 -0500

    Conformed HTTPStatus to Codable protocol

--- Sources/Vapor/Response/HTTPStatus.swift
+++ Sources/Vapor/Response/HTTPStatus.swift
@@ -1,10 +1,25 @@
 /// Less verbose typealias for `HTTPResponseStatus`.
 public typealias HTTPStatus = HTTPResponseStatus
 
+extension HTTPStatus: Codable {
+    /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let code = try container.decode(Int.self)
+        self = .init(statusCode: code)
+    }
+    
+    /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode).
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.code)
+    }
+}
+
 extension HTTPStatus: ResponseEncodable {
     /// See `ResponseEncodable`.
     public func encode(for req: Request) throws -> Future<Response> {
         let res = Response(http: .init(status: self), using: req)
-        return req.eventLoop.newSucceededFuture(result: res)
+        return req.future(res)
     }
 }
