879d795ec2a3002befd4ff48e46beb6f132746c3
Graham Burgsma <graham.burgsma@gmail.com>
2018-11-09 08:42:20 -0400

    Add support for Ubuntu 18.0.4 (bionic) in apt utility script

--- Utilities/apt.sh
+++ Utilities/apt.sh
@@ -1,9 +1,9 @@
 function add_vapor_apt() {
     eval "$(cat /etc/lsb-release)"
 
-    if [[ "$DISTRIB_CODENAME" != "xenial" && "$DISTRIB_CODENAME" != "yakkety" && "$DISTRIB_CODENAME" != "trusty" ]];
+    if [[ "$DISTRIB_CODENAME" != "xenial" && "$DISTRIB_CODENAME" != "yakkety" && "$DISTRIB_CODENAME" != "trusty" && "$DISTRIB_CODENAME" != "bionic" ]];
     then
-        echo "Only Ubuntu 14.04, 16.04, and 16.10 are supported."
+        echo "Only Ubuntu 14.04, 16.04, 16.10 and 18.0.4 are supported."
         echo "You are running $DISTRIB_RELEASE ($DISTRIB_CODENAME) [`uname`]"
         return 1;
     fi
