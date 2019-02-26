2a61c8231042e92d7b2124613c5ce8750c179a55
Jari (LotU) <j.koopman@jarict.nl>
2019-02-26 20:14:58 +0100

    Update Contrib Tests to 4.2 (#1920)
    Signed-off-by: MrLotU <j.koopman@jarict.nl>

--- Utilities/contributor_test.sh
+++ Utilities/contributor_test.sh
@@ -9,7 +9,7 @@ echo "💧  Exporting docker-machine env"
 eval "$(docker-machine env default)"
 
 echo "💧  Running unit tests on Linux"
-docker run -it -v $PWD:/root/code -w /root/code norionomura/swift:swift-4.1-branch /usr/bin/swift test
+docker run -it -v $PWD:/root/code -w /root/code swift:4.2 /usr/bin/swift test
 LINUX_EXIT=$?
 
 if [[ $MACOS_EXIT == 0 ]];
