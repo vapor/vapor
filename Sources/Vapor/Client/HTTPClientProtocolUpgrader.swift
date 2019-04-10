26281827d009d91f8cb98275627113200ebefc14
Tanner Nelson <me@tanner.xyz>
2019-04-10 17:15:00 -0400

    small fixes

--- Sources/Vapor/Client/HTTPClientProtocolUpgrader.swift
+++ Sources/Vapor/Client/HTTPClientProtocolUpgrader.swift
@@ -2,11 +2,7 @@ import NIO
 import NIOHTTP1
 
 /// Can be used to upgrade `HTTPClient` requests using the static `HTTPClient.upgrade(...)` method.
-internal protocol HTTPClientProtocolUpgrader {
-    /// Builds the `HTTPHeaders` required for an upgrade.
+protocol HTTPClientProtocolUpgrader {
     func buildUpgradeRequest() -> HTTPHeaders
-    
-    /// Called if `isValidUpgradeResponse` returns `true`. This should return the `UpgradeResult`
-    /// that will ultimately be returned by `HTTPClient.upgrade(...)`.
     func upgrade(context: ChannelHandlerContext, upgradeResponse: HTTPResponseHead) -> EventLoopFuture<Void>
 }
