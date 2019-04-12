f95420f029703e229f72cf2ec06f4c8a9044ff34
Tanner Nelson <me@tanner.xyz>
2019-04-12 12:37:18 -0400

    type-safe console input

--- Sources/Vapor/Commands/ServeCommand.swift
+++ Sources/Vapor/Commands/ServeCommand.swift
@@ -5,21 +5,19 @@
 ///
 public final class ServeCommand: Command {
     /// See `Command`.
-    public var arguments: [CommandArgument] {
-        return []
+    public struct Signature: CommandSignature {
+        public let hostname = Option<String>(name: "hostname", short: "H", default: nil, help: "Set the hostname the server will run on.")
+        public let port = Option<Int>(name: "port", short: "p", default: nil, help: "Set the port the server will run on.")
+        public let bind = Option<String>(name: "bind", short: "b", default: nil, help: "Convenience for setting hostname and port together.")
     }
 
     /// See `Command`.
-    public var options: [CommandOption] {
-        return [
-            .value(name: "hostname", short: "H", help: ["Set the hostname the server will run on."]),
-            .value(name: "port", short: "p", help: ["Set the port the server will run on."]),
-            .value(name: "bind", short: "b", help: ["Convenience for setting hostname and port together."]),
-        ]
-    }
+    public static let signature = Signature()
 
     /// See `Command`.
-    public let help: [String] = ["Begins serving the app over HTTP."]
+    public var help: String? {
+        return "Begins serving the app over HTTP."
+    }
 
     private let server: Server
     private var signalSources: [DispatchSourceSignal]
@@ -31,14 +29,14 @@ public final class ServeCommand: Command {
     }
 
     /// See `Command`.
-    public func run(using context: CommandContext) throws {
+    public func run(using context: CommandContext<ServeCommand>) throws {
         try self.server.start(
-            hostname: context.options["hostname"]
+            hostname: context.option(\.hostname)
                 // 0.0.0.0:8080, 0.0.0.0, parse hostname
-                ?? context.options["bind"]?.split(separator: ":").first.flatMap(String.init),
-            port: context.options["port"].flatMap(Int.init)
+                ?? context.option(\.bind)?.split(separator: ":").first.flatMap(String.init),
+            port: context.option(\.port)
                 // 0.0.0.0:8080, :8080, parse port
-                ?? context.options["bind"]?.split(separator: ":").last.flatMap(String.init).flatMap(Int.init)
+                ?? context.option(\.bind)?.split(separator: ":").last.flatMap(String.init).flatMap(Int.init)
         )
         
         // setup signal sources for shutdown
