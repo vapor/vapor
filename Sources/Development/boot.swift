45cbd105581c7090206d6b4f75c92896396f646f
Tanner Nelson <me@tanner.xyz>
2019-06-04 11:33:22 -0400

    improve log bootstrapping

--- Sources/Development/boot.swift
+++ Sources/Development/boot.swift
@@ -1,15 +1,7 @@
 import Vapor
 
 public func boot(_ app: Application) throws {
-    let c = try app.makeContainer().wait()
-    defer { c.shutdown() }
-    
-    // bootstrap logging system
-    let console = try c.make(Console.self)
-    LoggingSystem.bootstrap(
-        console: console,
-        level: app.environment == .production ? .error : .info
-    )
-    
+    // let c = try app.makeContainer().wait()
+    // defer { c.shutdown() }
     // use container
 }
