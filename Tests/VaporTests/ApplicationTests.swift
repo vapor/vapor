68df41dd5c4f02f55147e785f6b2c07dd3d934e4
Tanner Nelson <me@tanner.xyz>
2019-08-26 10:55:25 -0400

    merge master, fix request tests, regen linuxmain

--- Tests/VaporTests/ApplicationTests.swift
+++ Tests/VaporTests/ApplicationTests.swift
@@ -97,6 +97,43 @@ final class ApplicationTests: XCTestCase {
         try XCTAssertEqual(request.query.get(String.self, at: "hello"), "world")
     }
 
+    func testQueryGet() throws {
+        var req: Request
+
+        //
+        req = Request(method: .GET, url: .init(string: "/path?foo=a"), on: EmbeddedEventLoop())
+
+        XCTAssertEqual(try req.query.get(String.self, at: "foo"), "a")
+        XCTAssertThrowsError(try req.query.get(Int.self, at: "foo")) { error in
+            if case .typeMismatch(_, let context) = error as? DecodingError {
+                XCTAssertEqual(context.debugDescription, "Data found at 'foo' was not Int")
+            } else {
+                XCTFail("Catched error \"\(error)\", but not the expected: \"DecodingError.typeMismatch\"")
+            }
+        }
+        XCTAssertThrowsError(try req.query.get(String.self, at: "bar")) { error in
+            if case .valueNotFound(_, let context) = error as? DecodingError {
+                XCTAssertEqual(context.debugDescription, "No String was found at 'bar'")
+            } else {
+                XCTFail("Catched error \"\(error)\", but not the expected: \"DecodingError.valueNotFound\"")
+            }
+        }
+
+        XCTAssertEqual(req.query[String.self, at: "foo"], "a")
+        XCTAssertEqual(req.query[String.self, at: "bar"], nil)
+
+        //
+        req = Request(method: .GET, url: .init(string: "/path"), on: EmbeddedEventLoop())
+        XCTAssertThrowsError(try req.query.get(Int.self, at: "foo")) { error in
+            if let error = error as? Abort {
+                XCTAssertEqual(error.status, .unsupportedMediaType)
+            } else {
+                XCTFail("Catched error \"\(error)\", but not the expected: \"\(Abort(.unsupportedMediaType))\"")
+            }
+        }
+        XCTAssertEqual(req.query[String.self, at: "foo"], nil)
+    }
+
     func testParameter() throws {
         let app = Application.create(routes: { r, c in
             r.get("hello", ":a") { req in
