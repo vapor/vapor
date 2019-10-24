d20a37e1b8fb88f587de52f7116dac5111a37cd2
tanner0101 <me@tanner.xyz>
2019-10-24 14:50:49 -0400

    uuid lossless string convertible

--- Sources/Vapor/Routing/RoutesBuilder.swift
+++ Sources/Vapor/Routing/RoutesBuilder.swift
@@ -1,3 +1,9 @@
 public protocol RoutesBuilder {
     func add(_ route: Route)
 }
+
+extension UUID: LosslessStringConvertible {
+    public init?(_ description: String) {
+        self.init(uuidString: description)
+    }
+}
