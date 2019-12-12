d3a664793c547bf30336e85eb4e13ed27bb6b2dc
MrLotU <j.koopman@jarict.nl>
2019-12-12 08:29:27 +0100

    Final cleanup to not break public API

--- Sources/Vapor/HTTP/ApplicationResponder.swift
+++ Sources/Vapor/HTTP/ApplicationResponder.swift
@@ -8,7 +8,7 @@ public struct ApplicationResponder: Responder {
     private let requestsErrorsLabel: String = "http_request_errors_total"
 
     /// Creates a new `ApplicationResponder`
-    public init(routes: Routes, middleware: [Middleware]) {
+    public init(routes: Routes, middleware: [Middleware] = []) {
         // We create & store this at init time to not impact performance later on in the application.
         // This `Route` is used to return a 404 response, instead of an error.
         let notFoundResponder = middleware.makeResponder(chainingTo: BasicResponder(closure: { _ in throw Abort(.notFound) }))
