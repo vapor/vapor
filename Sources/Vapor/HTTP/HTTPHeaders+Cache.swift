e50fbe2cf5d6ef20d01884e444c605f511a44293
Scott Grosch <Scott.Grosch@icloud.com>
2020-02-12 21:12:09 -0800

    Updates as per Tanner's notes in the pull request

--- Sources/Vapor/HTTP/HTTPHeaders+Cache.swift
+++ Sources/Vapor/HTTP/HTTPHeaders+Cache.swift
@@ -1,20 +1,4 @@
 extension HTTPHeaders {
-    /// Gets the value of the `Cache-Control` header, if present.
-    public func getCacheControl() -> HTTPHeaderCacheControl? {
-        guard let cacheControl = firstValue(name: .cacheControl) else {
-            return nil
-        }
-
-        return HTTPHeaderCacheControl(stringLiteral: cacheControl)
-    }
-
-    /// Gets the value of the `Expires` header, if present.
-    /// ### Note ###
-    /// `Expires` is legacy and you should switch to using `Cache-Control` if possible.
-    public func getExpires() -> HTTPHeaderExpires? {
-        HTTPHeaderExpires(headers: self)
-    }
-
     /// Determines when the cached data should be expired.
     ///
     /// This first checks to see if the `Cache-Control` header is present.  If it is, and `no-store`
@@ -23,10 +7,13 @@ extension HTTPHeaders {
     ///
     /// If no `Cache-Control` header is present then it will examine the `Expires` header.
     ///
+    /// If you need finer grained details about what type of caching, validation, etc... you should instead
+    /// grab the `cacheControl` and `expires` headers yourself.
+    ///
     /// - Parameter requestSentAt: Should be passed the `Date` when the request was sent.
     public func getExpirationDate(requestSentAt: Date) -> Date? {
         // Cache-Control header takes priority over the Expires header
-        if let cacheControl = getCacheControl() {
+        if let cacheControl = cacheControl {
             guard !cacheControl.contains(.noStore) else {
                 return nil
             }
@@ -36,7 +23,7 @@ extension HTTPHeaders {
             }
         }
 
-        if let expires = getExpires() {
+        if let expires = expires {
             return expires.expires
         }
 
