9f4404da33f061b0eea0b74f6ef23fb6e1d61543
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2020-05-25 21:35:42 +0200

    Add storage accessors that can set value

--- Sources/Vapor/Utilities/Storage.swift
+++ Sources/Vapor/Utilities/Storage.swift
@@ -34,6 +34,15 @@ public struct Storage {
         }
     }
 
+    public subscript<Key>(
+        _ key: Key.Type,
+        orSetDefault default: @autoclosure () -> Key.Value
+    ) -> Key.Value where Key: StorageKey {
+        mutating get {
+            self.get(Key.self, orSetDefault: default)
+        }
+    }
+
     public func contains<Key>(_ key: Key.Type) -> Bool {
         self.storage.keys.contains(ObjectIdentifier(Key.self))
     }
@@ -45,6 +54,18 @@ public struct Storage {
             return nil
         }
         return value.value
+    }
+
+    public mutating func get<Key>(
+        _ key: Key.Type, 
+        orSetDefault default: @autoclosure () -> Key.Value
+    ) -> Key.Value where Key: StorageKey {
+        guard let value = self.get(key) else {
+            let value = `default`()
+            self.set(key, to: value)
+            return value
+        }
+        return value
     }
 
     public mutating func set<Key>(
