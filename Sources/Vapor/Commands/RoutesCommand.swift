c60a484b8ef5e358f0e4105fd9e41ee748d77ac0
stevapple <stevapple@icloud.com>
2020-05-25 12:04:27 +0800

    Update RoutesCommand to align with RoutingKit

--- Sources/Vapor/Commands/RoutesCommand.swift
+++ Sources/Vapor/Commands/RoutesCommand.swift
@@ -10,7 +10,7 @@
 /// A colon preceding a path component indicates a variable parameter. A colon with no text following
 /// is a parameter whose result will be discarded.
 ///
-/// An asterisk indicates a catch-all. Any path components after a catch-all will be discarded and ignored.
+/// The path will be displayed with the same syntax that is used to register a route.
 public final class RoutesCommand: Command {
     public struct Signature: CommandSignature {
         public init() { }
@@ -25,25 +25,16 @@ public final class RoutesCommand: Command {
     public func run(using context: CommandContext, signature: Signature) throws {
         let routes = context.application.routes
         let includeDescription = !routes.all.filter { $0.userInfo["description"] != nil }.isEmpty
+        let pathSeparator = "/".consoleText()
         context.console.outputASCIITable(routes.all.map { route -> [ConsoleText] in
-            var pathText: ConsoleText = ""
-            if route.path.isEmpty {
-                pathText += "/".consoleText(.info)
-            }
-            for path in route.path {
-                pathText += "/".consoleText(.info)
-                switch path {
-                case .constant(let string):
-                    pathText += string.consoleText()
-                case .parameter(let name):
-                    pathText += ":".consoleText(.info)
-                    pathText += name.consoleText()
-                case .anything:
-                    pathText += ":".consoleText(.info)
-                case .catchall:
-                    pathText += "*".consoleText(.info)
+            let pathText = pathSeparator + route.path.map {
+                switch $0 {
+                case .constant:
+                    return $0.description.consoleText()
+                default:
+                    return $0.description.consoleText(.info)
                 }
-            }
+            }.joined(separator: pathSeparator)
             var column = [route.method.string.consoleText(), pathText]
             if includeDescription {
                 let desc = route.userInfo["description"]
@@ -53,6 +44,17 @@ public final class RoutesCommand: Command {
             }
             return column
         })
+    }
+}
+
+extension Collection where Element == ConsoleText {
+    func joined(separator: ConsoleText) -> ConsoleText {
+        guard let result: ConsoleText = self.first else {
+            return ""
+        }
+        return self.dropFirst().reduce(into: result) {
+            $0 += separator + $1
+        }
     }
 }
 
