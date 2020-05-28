6203700d4b588e4961372eedda5d5911c2317585
tanner0101 <me@tanner.xyz>
2020-05-28 14:02:02 -0400

    deprecate expectation alias

--- Sources/_Vapor3/Future.swift
+++ Sources/_Vapor3/Future.swift
@@ -8,13 +8,13 @@ extension EventLoopFuture {
     public typealias Expectation = Value
 
     @available(*, deprecated, message: "The `to` parameter has been removed and this method can no longer throw.")
-    public func map<T>(to type: T.Type, _ callback: @escaping (Expectation) throws -> T) -> EventLoopFuture<T> {
+    public func map<T>(to type: T.Type, _ callback: @escaping (Value) throws -> T) -> EventLoopFuture<T> {
         return self.flatMapThrowing(callback)
     }
 
 
     @available(*, deprecated, message: "The `to` parameter has been removed and this method can no longer throw.")
-    public func flatMap<T>(to type: T.Type, _ callback: @escaping (Expectation) throws -> EventLoopFuture<T>) -> EventLoopFuture<T> {
+    public func flatMap<T>(to type: T.Type, _ callback: @escaping (Value) throws -> EventLoopFuture<T>) -> EventLoopFuture<T> {
         return self.flatMap { input in
             do {
                 return try callback(input)
@@ -25,13 +25,13 @@ extension EventLoopFuture {
     }
 
     @available(*, deprecated, renamed: "flatMapErrorThrowing")
-    public func catchMap(_ callback: @escaping (Error) throws -> (Expectation)) -> EventLoopFuture<Expectation> {
+    public func catchMap(_ callback: @escaping (Error) throws -> (Value)) -> EventLoopFuture<Value> {
         return self.flatMapErrorThrowing(callback)
     }
 
 
     @available(*, deprecated, message: "Use `flatMapError` with internal do / catch that returns a failed future.")
-    public func catchFlatMap(_ callback: @escaping (Error) throws -> (EventLoopFuture<Expectation>)) -> EventLoopFuture<Expectation> {
+    public func catchFlatMap(_ callback: @escaping (Error) throws -> (EventLoopFuture<Value>)) -> EventLoopFuture<Value> {
         return self.flatMapError { inputError in
             do {
                 return try callback(inputError)
