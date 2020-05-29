317780b3fd06aa9001270d9b2e04fbced7d0c457
Tom Shen <tomshen2004@163.com>
2020-05-29 12:17:32 +0800

    Clean up code

--- Sources/Vapor/Authentication/RedirectMiddleware.swift
+++ Sources/Vapor/Authentication/RedirectMiddleware.swift
@@ -4,15 +4,14 @@ extension Authenticatable {
     /// - parameters:
     ///    - path: The path to redirect to if the request is not authenticated
     public static func redirectMiddleware(path: String) -> Middleware {
-        return RedirectMiddleware<Self>(Self.self, path: path)
+        return RedirectMiddleware<Self>(Self.self, pathClosure: { _ in return path })
     }
     
     /// Basic middleware to redirect unauthenticated requests to the supplied path
     ///
     /// - parameters:
     ///    - pathClosure: The closure that returns the redirect path based on the given URI path
-    ///    - input: The original `Request` object of the request
-    public static func redirectMiddleware(pathClosure: @escaping (_ input: Request) -> String) -> Middleware {
+    public static func redirectMiddleware(_ pathClosure: @escaping (Request) -> String) -> Middleware {
         return RedirectMiddleware<Self>(Self.self, pathClosure: pathClosure)
     }
 }
@@ -21,15 +20,9 @@ extension Authenticatable {
 private final class RedirectMiddleware<A>: Middleware
     where A: Authenticatable
 {
-    let pathClosure: PathClosure
+    let pathClosure: (Request) -> String
     
-    typealias PathClosure = (Request) -> String
-
-    init(_ authenticatableType: A.Type = A.self, path: String) {
-        self.pathClosure = { _ in return path }
-    }
-    
-    init(_ authenticatableType: A.Type = A.self, pathClosure: @escaping PathClosure) {
+    init(_ authenticatableType: A.Type = A.self, pathClosure: @escaping (Request) -> String) {
         self.pathClosure = pathClosure
     }
 
@@ -39,7 +32,7 @@ private final class RedirectMiddleware<A>: Middleware
             return next.respond(to: req)
         }
         
-        let redirectPath = pathClosure(req)
+        let redirectPath = self.pathClosure(req)
         
         let redirect = req.redirect(to: redirectPath)
         return req.eventLoop.makeSucceededFuture(redirect)
