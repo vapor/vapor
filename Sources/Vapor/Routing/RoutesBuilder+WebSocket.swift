bfe635f80825be8085c3e29a6f16f65938752d57
Simon Kempendorf <simon@code28.de>
2020-10-20 18:19:09 +0200

    Add API docs

--- Sources/Vapor/Routing/RoutesBuilder+WebSocket.swift
+++ Sources/Vapor/Routing/RoutesBuilder+WebSocket.swift
@@ -11,6 +11,15 @@ public struct WebSocketMaxFrameSize: ExpressibleByIntegerLiteral {
 }
 
 extension RoutesBuilder {
+    /// Adds a route for opening a web socket connection
+    /// - parameters:
+    ///   - path: Path components separated by commas.
+    ///   - maxFrameSize: The maximum allowed frame size. See `NIOWebSocketServerUpgrader`.
+    ///   - shouldUpgrade: Closure to apply before upgrade to web socket happens.
+    ///       Returns additional `HTTPHeaders` for response, `nil` to deny upgrading.
+    ///       See `NIOWebSocketServerUpgrader`.
+    ///   - onUpgrade: Closure to apply after web socket is upgraded successfully.
+    /// - returns: `Route` instance for newly created web socket endpoint
     @discardableResult
     public func webSocket(
         _ path: PathComponent...,
@@ -23,6 +32,15 @@ extension RoutesBuilder {
         return self.webSocket(path, maxFrameSize: maxFrameSize, shouldUpgrade: shouldUpgrade, onUpgrade: onUpgrade)
     }
 
+    /// Adds a route for opening a web socket connection
+    /// - parameters:
+    ///   - path: Array of path components.
+    ///   - maxFrameSize: The maximum allowed frame size. See `NIOWebSocketServerUpgrader`.
+    ///   - shouldUpgrade: Closure to apply before upgrade to web socket happens.
+    ///       Returns additional `HTTPHeaders` for response, `nil` to deny upgrading.
+    ///       See `NIOWebSocketServerUpgrader`.
+    ///   - onUpgrade: Closure to apply after web socket is upgraded successfully.
+    /// - returns: `Route` instance for newly created web socket endpoint
     @discardableResult
     public func webSocket(
         _ path: [PathComponent],
