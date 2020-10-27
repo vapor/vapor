2c653b1b6cdba12b608bf190aff40339237f5cd0
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2020-10-27 11:06:43 +0100

    Avoid creating new eventLoopGroup unnecessarily

--- Sources/Vapor/Application.swift
+++ Sources/Vapor/Application.swift
@@ -89,7 +89,7 @@ public final class Application {
         self.clients.use(.http)
         self.commands.use(self.servers.command, as: "serve", isDefault: true)
         self.commands.use(RoutesCommand(), as: "routes")
-        DotEnvFile.load(for: environment, on: eventLoopGroupProvider, logger: self.logger)
+        DotEnvFile.load(for: environment, on: .shared(self.eventLoopGroup), fileio: fileio, logger: self.logger)
     }
     
     public func run() throws {
