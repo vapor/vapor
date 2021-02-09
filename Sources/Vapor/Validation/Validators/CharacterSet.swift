e3fe4fdae3aea4c5638bddbea9d038f8d13792ca
Owain Hunt <owain@orh.cc>
2021-02-09 09:08:31 +0000

    Adds CharacterSet validator for string collections (#2556)
    Co-authored-by: Owain Hunt <owain.hunt@johnlewis.co.uk>
    Co-authored-by: Tim Condon <0xTim@users.noreply.github.com>

--- Sources/Vapor/Validation/Validators/CharacterSet.swift
+++ Sources/Vapor/Validation/Validators/CharacterSet.swift
@@ -58,6 +58,68 @@ extension ValidatorResults.CharacterSet: ValidatorResult {
     }
 }
 
+extension Validator where T == [String] {
+    /// Validates that all characters in elements of a `[String]` are ASCII (bytes 0..<128).
+    public static var ascii: Validator {
+        .characterSet(.ascii)
+    }
+
+    /// Validates that all characters in elements of a `[String]` are alphanumeric (a-z,A-Z,0-9).
+    public static var alphanumeric: Validator {
+        .characterSet(.alphanumerics)
+    }
+
+    /// Validates that all characters in elements of a `[String]` are in the supplied `CharacterSet`.
+    public static func characterSet(_ characterSet: Foundation.CharacterSet) -> Validator {
+        .init {
+            ValidatorResults.CollectionCharacterSet(strings: $0, characterSet: characterSet)
+        }
+    }
+}
+
+extension ValidatorResults {
+    /// `ValidatorResult` of a validator that validates that all elements of a `[String]` contain characters in a given `CharacterSet`.
+    public struct CollectionCharacterSet {
+        /// The validated string.
+        public let strings: [String]
+        
+        /// The set of characters the input is allowed to contain.
+        public let characterSet: Foundation.CharacterSet
+
+        /// On validation failure, the first substring of the input with characters not contained in `characterSet`.
+        var invalidRanges: [(Int, Swift.Range<String.Index>)?] {
+            return self.strings.enumerated().compactMap {
+                if let range = $1.rangeOfCharacter(from: self.characterSet.inverted) {
+                    return ($0, range)
+                }
+                return nil
+            }
+        }
+        
+        var allowedCharacterString: String {
+            self.characterSet.traits.joined(separator: ", ")
+        }
+    }
+}
+
+extension ValidatorResults.CollectionCharacterSet: ValidatorResult {
+    public var isFailure: Bool {
+        !self.invalidRanges.isEmpty
+    }
+    
+    public var successDescription: String? {
+        "contains only \(self.allowedCharacterString)"
+    }
+    
+    public var failureDescription: String? {
+        let disallowedCharacters = self.invalidRanges.compactMap { $0 }
+            .map { (invalidSlice) in
+                "string at index \(invalidSlice.0) contains '\(String(self.strings[invalidSlice.0][invalidSlice.1]))'"
+            }
+        return "\(disallowedCharacters.joined(separator: ", ")) (allowed: \(self.allowedCharacterString))"
+    }
+}
+
 /// Unions two character sets.
 ///
 ///     .characterSet(.alphanumerics + .whitespaces)
