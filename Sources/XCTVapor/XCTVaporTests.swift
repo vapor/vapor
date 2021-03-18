91aca96e153c8ebc316e616dd799229a923486a0
Max Howell <mxcl@me.com>
2021-03-18 12:16:43 -0400

    Restore previous XCTVaporTests behavior if necc.
    Some people were relying on the previous call order by coincidence and we should avoid punishing them.

--- Sources/XCTVapor/XCTVaporTests.swift
+++ Sources/XCTVapor/XCTVaporTests.swift
@@ -1,18 +1,29 @@
-public var app: (() throws -> Application) = {
-    fatalError("implement static app generator")
-}
+public var app: (() throws -> Application)!
 
 open class XCTVaporTests: XCTestCase {
     open var app: Application!
+
+    open override func setUp() {
+        super.setUp()
+        if self.app == nil, let _app = XCTVapor.app {
+            // this was the behavior of this class pre 4.41.5
+            // keeping for compatability
+            self.app = try! _app()
+        } else {
+            fatalError("implement static app generator")
+        }
+    }
     
     open override func setUpWithError() throws {
-        super.setUp()
-        self.app = try XCTVapor.app()
+        try super.setUpWithError()
+        if let _app = XCTVapor.app {
+            self.app = try _app()
+        }
     }
     
     open override func tearDown() {
         super.tearDown()
         self.app?.shutdown()
-        app = nil
+        self.app = nil
     }
 }
