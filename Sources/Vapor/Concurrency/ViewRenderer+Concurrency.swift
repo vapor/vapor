bc0700996c2627934cf4bdbbf52747933e9af887
Tim <0xtimc@gmail.com>
2021-04-28 17:31:34 +0100

    Add async APIs for ViewRenderer

---
+++ Sources/Vapor/Concurrency/ViewRenderer+Concurrency.swift
@@ -0,0 +1,22 @@
+#if compiler(>=5.5) && $AsyncAwait
+import _NIOConcurrency
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+public extension ViewRenderer {
+    func render<E>(_ name: String, _ context: E) async throws -> View where E: Encodable {
+        try await self.render(name, context).get()
+    }
+
+    func render(_ name: String) async throws -> View {
+        try await self.render(name).get()
+    }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+public extension View {
+    func encodeResponse(for request: Request) async throws -> Response {
+        try await self.encodeResponse(for: request).get()
+    }
+}
+
+#endif
