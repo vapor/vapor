ca7f7725016568bb48d576590501ce72d59f190c
Nikola Paunovic <nikola@gaiali.com>
2021-08-23 00:27:31 +0200

    Resolve issue #2650
    In order to prevent 5xx internal error we try to:
    
    - validate number of parameters in FormDataDecoder so we can detect when only boundary is present.

--- Sources/Vapor/Multipart/FormDataDecoder+Content.swift
+++ Sources/Vapor/Multipart/FormDataDecoder+Content.swift
@@ -5,6 +5,9 @@ extension FormDataDecoder: ContentDecoder {
         guard let boundary = headers.contentType?.parameters["boundary"] else {
             throw Abort(.unsupportedMediaType)
         }
+        if headers.contentType?.parameters.count ?? 1 == 1  {
+            throw Abort(.unprocessableEntity, identifier: "emptyMultipartFormData")
+        }
         var body = body
         let buffer = body.readBytes(length: body.readableBytes) ?? []
         return try self.decode(D.self, from: buffer, boundary: boundary)
