25244aad8638dcf0c3d488da611ad45f8ed6d523
Frizlab <fload@me.com>
2021-11-06 19:41:11 +0100

    Add a test for empty password in basic auth

--- Tests/VaporTests/AuthenticationTests.swift
+++ Tests/VaporTests/AuthenticationTests.swift
@@ -116,6 +116,45 @@ final class AuthenticationTests: XCTestCase {
         }
     }
     
+    func testBasicAuthenticatorWithEmptyPassword() throws {
+        struct Test: Authenticatable {
+            static func authenticator() -> Authenticator {
+                TestAuthenticator()
+            }
+
+            var name: String
+        }
+
+        struct TestAuthenticator: BasicAuthenticator {
+            typealias User = Test
+
+            func authenticate(basic: BasicAuthorization, for request: Request) -> EventLoopFuture<Void> {
+                if basic.username == "test" && basic.password == "" {
+                    let test = Test(name: "Vapor")
+                    request.auth.login(test)
+                }
+                return request.eventLoop.makeSucceededFuture(())
+            }
+        }
+        
+        let app = Application(.testing)
+        defer { app.shutdown() }
+
+        app.routes.grouped([
+            Test.authenticator(), Test.guardMiddleware()
+        ]).get("test") { req -> String in
+            return try req.auth.require(Test.self).name
+        }
+        
+        let basic = Data("test:".utf8).base64EncodedString()
+        try app.testable().test(.GET, "/test") { res in
+            XCTAssertEqual(res.status, .unauthorized)
+        }.test(.GET, "/test", headers: ["Authorization": "Basic \(basic)"]) { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Vapor")
+        }
+    }
+    
     func testBasicAuthenticatorWithRedirect() throws {
         struct Test: Authenticatable {
             static func authenticator() -> Authenticator {
