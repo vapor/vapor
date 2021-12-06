34bf1b303623de04801d2b9bef5b2ed48b3e9319
Jay Herron <30518755+NeedleInAJayStack@users.noreply.github.com>
2021-12-06 10:09:17 -0700

    Adds default file support to FileMiddleware (#2678)
    * Adds default file support to FileMiddleware
    
    * Adds support for absolute default files
    
    Also adds tests for this usage and refactors to improve code readability
    
    * Update Sources/Vapor/Middleware/FileMiddleware.swift
    
    Co-authored-by: YR Chen <stevapple@icloud.com>
    
    Co-authored-by: Tim Condon <0xTim@users.noreply.github.com>
    Co-authored-by: YR Chen <stevapple@icloud.com>

--- Sources/Vapor/Middleware/FileMiddleware.swift
+++ Sources/Vapor/Middleware/FileMiddleware.swift
@@ -2,13 +2,19 @@
 ///
 /// `FileMiddleware` will default to `DirectoryConfig`'s working directory with `"/Public"` appended.
 public final class FileMiddleware: Middleware {
-    /// The public directory.
-    /// - note: Must end with a slash.
+    /// The public directory. Guaranteed to end with a slash.
     private let publicDirectory: String
+    private let defaultFile: String?
 
     /// Creates a new `FileMiddleware`.
-    public init(publicDirectory: String) {
-        self.publicDirectory = publicDirectory.hasSuffix("/") ? publicDirectory : publicDirectory + "/"
+    ///
+    /// - parameters:
+    ///     - publicDirectory: The public directory to serve files from.
+    ///     - defaultFile: The name of the default file to look for and serve if a request hits any public directory. Starting with `/` implies
+    ///     an absolute path from the public directory root. If `nil`, no default files are served.
+    public init(publicDirectory: String, defaultFile: String? = nil) {
+        self.publicDirectory = publicDirectory.addTrailingSlash()
+        self.defaultFile = defaultFile
     }
 
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
@@ -18,26 +24,67 @@ public final class FileMiddleware: Middleware {
         }
 
         // path must be relative.
-        while path.hasPrefix("/") {
-            path = String(path.dropFirst())
-        }
+        path = path.removeLeadingSlashes()
 
         // protect against relative paths
         guard !path.contains("../") else {
             return request.eventLoop.makeFailedFuture(Abort(.forbidden))
         }
 
-        // create absolute file path
-        let filePath = self.publicDirectory + path
+        // create absolute path
+        var absPath = self.publicDirectory + path
 
-        // check if file exists and is not a directory
+        // check if path exists and whether it is a directory
         var isDir: ObjCBool = false
-        guard FileManager.default.fileExists(atPath: filePath, isDirectory: &isDir), !isDir.boolValue else {
+        guard FileManager.default.fileExists(atPath: absPath, isDirectory: &isDir) else {
             return next.respond(to: request)
         }
-
+        
+        if isDir.boolValue {
+            // If a directory, check for the default file
+            guard let defaultFile = defaultFile else {
+                return next.respond(to: request)
+            }
+            
+            if defaultFile.isAbsolute() {
+                absPath = self.publicDirectory + defaultFile.removeLeadingSlashes()
+            } else {
+                absPath = absPath.addTrailingSlash() + defaultFile
+            }
+            
+            // If the default file doesn't exist, pass on request
+            guard FileManager.default.fileExists(atPath: absPath) else {
+                return next.respond(to: request)
+            }
+        }
+        
         // stream the file
-        let res = request.fileio.streamFile(at: filePath)
+        let res = request.fileio.streamFile(at: absPath)
         return request.eventLoop.makeSucceededFuture(res)
+    }
+}
+
+fileprivate extension String {
+    /// Determines if input path is absolute based on a leading slash
+    func isAbsolute() -> Bool {
+        return self.hasPrefix("/")
+    }
+
+    /// Makes a path relative by removing all leading slashes
+    func removeLeadingSlashes() -> String {
+        var newPath = self
+        while newPath.hasPrefix("/") {
+            newPath.removeFirst()
+        }
+        return newPath
+    }
+
+    /// Adds a trailing slash to the path if one is not already present
+    func addTrailingSlash() -> String {
+        var newPath = self
+        if !newPath.hasSuffix("/") {
+            newPath += "/"
+        }
+        return newPath
     }
 }
--- Tests/VaporTests/FileTests.swift
+++ Tests/VaporTests/FileTests.swift
@@ -106,4 +106,48 @@ final class FileTests: XCTestCase {
             XCTAssertEqual(res.body.string, "bar\n")
         }
     }
+    
+    func testDefaultFileRelative() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+
+        let path = #file.split(separator: "/").dropLast().joined(separator: "/")
+        app.middleware.use(FileMiddleware(publicDirectory: "/" + path, defaultFile: "index.html"))
+
+        try app.test(.GET, "Utilities/") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "<h1>Root Default</h1>\n")
+        }.test(.GET, "Utilities/SubUtilities/") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "<h1>Subdirectory Default</h1>\n")
+        }
+    }
+    
+    func testDefaultFileAbsolute() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+
+        let path = #file.split(separator: "/").dropLast().joined(separator: "/")
+        app.middleware.use(FileMiddleware(publicDirectory: "/" + path, defaultFile: "/Utilities/index.html"))
+
+        try app.test(.GET, "Utilities/") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "<h1>Root Default</h1>\n")
+        }.test(.GET, "Utilities/SubUtilities/") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "<h1>Root Default</h1>\n")
+        }
+    }
+    
+    func testNoDefaultFile() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+
+        let path = #file.split(separator: "/").dropLast().joined(separator: "/")
+        app.middleware.use(FileMiddleware(publicDirectory: "/" + path))
+
+        try app.test(.GET, "Utilities/") { res in
+            XCTAssertEqual(res.status, .notFound)
+        }
+    }
 }
