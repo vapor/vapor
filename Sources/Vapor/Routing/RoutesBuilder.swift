6c257b7050e5dc439b9c37634da90372082b34ed
Stephen Celis <stephen.celis@gmail.com>
2022-04-18 12:22:10 -0400

    Do not conform UUID to LosslessStringConvertible
    Vapor should not extend a type it does not own with a protocol it does not own. Doing so can lead to conflicts with other code.

--- Sources/Vapor/Routing/RoutesBuilder.swift
+++ Sources/Vapor/Routing/RoutesBuilder.swift
@@ -1,9 +1,3 @@
 public protocol RoutesBuilder {
     func add(_ route: Route)
 }
-
-extension UUID: LosslessStringConvertible {
-    public init?(_ description: String) {
-        self.init(uuidString: description)
-    }
-}
