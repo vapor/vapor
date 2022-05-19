49c58c0cb9d7f1acc464654130b1da3278d86d42
Tim Condon <0xTim@users.noreply.github.com>
2022-05-19 16:04:05 +0100

    Update AHC version which allows us to enable missing tests (#2814)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // HTTP client library built on SwiftNIO
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.8.2"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.10.0"),
 
         // Sugary extensions for the SwiftNIO library
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
