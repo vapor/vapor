d09d0d33266f9e5bd60a80d18b320d2f76f06159
Tim Condon <0xTim@users.noreply.github.com>
2022-06-06 06:47:25 +0100

    Update depencendy as it's now required (#2842)

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
 
         // 🚍 High-performance trie-node router.
-        .package(url: "https://github.com/vapor/routing-kit.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/routing-kit.git", from: "4.5.0"),
 
         // 💥 Backtraces for Swift on Linux
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
