7b76fe01a8eb02aa7f61d9ca10624f98b25a5735
Jay Herron <30518755+NeedleInAJayStack@users.noreply.github.com>
2023-01-30 14:11:21 -0700

    Fix: FileIO headers (#2943)
    * feature: Formats the etag header correctly
    
    According to MDN documentation, the `ETag` header should start and end with double-quotes. Static files served by NGinx receive an `ETag` header in this format as well.
    
    * fix: Adds `Last-Modified` header to file responses
    
    * fix: include eTag and correct content-length headers on 304 response
    
    ---------
    
    Co-authored-by: Garrett Moseke <garrett@passivelogic.com>

--- Sources/Vapor/Utilities/FileIO.swift
+++ Sources/Vapor/Utilities/FileIO.swift
@@ -151,13 +151,20 @@ public struct FileIO {
         // Create empty headers array.
         var headers: HTTPHeaders = [:]
 
+        // Respond with lastModified header
+        headers.lastModified = HTTPHeaders.LastModified(value: modifiedAt)
+        
         // Generate ETag value, "HEX value of last modified date" + "-" + "file size"
-        let fileETag = "\(modifiedAt.timeIntervalSince1970)-\(fileSize)"
+        let fileETag = "\"\(modifiedAt.timeIntervalSince1970)-\(fileSize)\""
         headers.replaceOrAdd(name: .eTag, value: fileETag)
 
         // Check if file has been cached already and return NotModified response if the etags match
         if fileETag == request.headers.first(name: .ifNoneMatch) {
-            return Response(status: .notModified)
+            // Per RFC 9110 here: https://www.rfc-editor.org/rfc/rfc9110.html#status.304
+            // and here: https://www.rfc-editor.org/rfc/rfc9110.html#name-content-encoding
+            // A 304 response MUST include the ETag header and a Content-Length header matching what the original resource's content length would have been were this a 200 response.
+            headers.replaceOrAdd(name: .contentLength, value: fileSize.description)
+            return Response(status: .notModified, version: .http1_1, headersNoUpdate: headers, body: .empty)
         }
 
         // Create the HTTP response.
