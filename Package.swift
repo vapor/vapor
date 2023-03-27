220454f0c444b7f94fa42f93798f9e1c396d9296
M Persson <mteep@users.noreply.github.com>
2023-03-27 15:32:31 +0200

    Adjust Swift tools version to 5.5 to work with Xcode on Big Sur.
    Unfortunately, Xcode versions 13.1, 13.2, and 13.2.1 didn't increment the version of SwiftPM,
    despite incrementing the Swift version to 5.5.1, and 5.5.2, respectively.
    
    Since the next release of Xcode, 13.3, requires macOS 12 (Monterey), there's no released version
    of Xcode for macOS 11 (Big Sur) that can parse a Package.swift with swift-tools-version:5.5.2.
    
    SwiftPM 5.5.0 also refuses to process manifests with SwiftVersion.version("5.5.2"), so we have
    to rely on the compiler itself to require Swift 5.5.2.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,26 @@
-// swift-tools-version:5.5.2
+// swift-tools-version:5.5
 import PackageDescription
+
+// Workaround for Xcode 13.2(.1) with Swift 5.5.2, but SwiftPM 5.5.0
+#if swift(>=5.5.2)
+let platforms: [SupportedPlatform] = [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .tvOS(.v13),
+    .watchOS(.v6)
+]
+#else
+let platforms: [SupportedPlatform] = [
+    .macOS(.v12),
+    .iOS(.v15),
+    .tvOS(.v15),
+    .watchOS(.v8)
+]
+#endif
 
 let package = Package(
     name: "vapor",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .tvOS(.v13),
-        .watchOS(.v6)
-    ],
+    platforms: platforms,
     products: [
         .library(name: "Vapor", targets: ["Vapor"]),
         .library(name: "XCTVapor", targets: ["XCTVapor"]),
