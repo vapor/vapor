b34225d742ab1aaecc591baf4497ad4efe706b76
Tim <0xtimc@gmail.com>
2023-08-15 02:08:48 +0100

    More PR reviews

--- Package.swift
+++ Package.swift
@@ -98,15 +98,7 @@ let package = Package(
             dependencies: [
                 .target(name: "Vapor"),
             ],
-            resources: [.copy("Resources")],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
-                .unsafeFlags([
-                    "-cross-module-optimization"
-                ], .when(configuration: .release)),
-            ]
+            resources: [.copy("Resources")]
         ),
 
         // Testing
