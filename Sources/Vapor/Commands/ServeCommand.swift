1df0eecb69299759b9c3d7cba9867787cf9bf986
Tim <0xtimc@gmail.com>
2023-10-11 14:15:59 +0100

    Remove preconcurrency imports for ConsoleKit

--- Sources/Vapor/Commands/ServeCommand.swift
+++ Sources/Vapor/Commands/ServeCommand.swift
@@ -1,6 +1,6 @@
 import Foundation
 @preconcurrency import Dispatch
-@preconcurrency import ConsoleKit
+import ConsoleKit
 import NIOConcurrencyHelpers
 
 /// Boots the application's server. Listens for `SIGINT` and `SIGTERM` for graceful shutdown.
