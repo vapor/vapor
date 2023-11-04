1b17b7fc84b644da84fb7d272759c029ca29535b
Tim <0xtimc@gmail.com>
2023-11-04 16:16:33 +0000

    Migrate websockets

--- Sources/Vapor/Routing/RoutesBuilder+WebSocket.swift
+++ Sources/Vapor/Routing/RoutesBuilder+WebSocket.swift
@@ -28,6 +28,8 @@ extension RoutesBuilder {
     /// - returns: `Route` instance for newly created web socket endpoint
     @preconcurrency
     @discardableResult
+    @available(*, deprecated, message: "use SendableRoute instead")
+    @_disfavoredOverload
     public func webSocket(
         _ path: PathComponent...,
         maxFrameSize: WebSocketMaxFrameSize = .`default`,
@@ -50,6 +52,8 @@ extension RoutesBuilder {
     /// - returns: `Route` instance for newly created web socket endpoint
     @preconcurrency
     @discardableResult
+    @available(*, deprecated, message: "use SendableRoute instead")
+    @_disfavoredOverload
     public func webSocket(
         _ path: [PathComponent],
         maxFrameSize: WebSocketMaxFrameSize = .`default`,
@@ -62,6 +66,60 @@ extension RoutesBuilder {
             let res = Response(status: .switchingProtocols)
             res.upgrader = WebSocketUpgrader(maxFrameSize: maxFrameSize, shouldUpgrade: {
                 shouldUpgrade(request)                
+            }, onUpgrade: { ws in
+                onUpgrade(request, ws)
+            })
+            return res
+        }
+    }
+}
+
+extension RoutesBuilder {
+    /// Adds a route for opening a web socket connection
+    /// - parameters:
+    ///   - path: Path components separated by commas.
+    ///   - maxFrameSize: The maximum allowed frame size. See `NIOWebSocketServerUpgrader`.
+    ///   - shouldUpgrade: Closure to apply before upgrade to web socket happens.
+    ///       Returns additional `HTTPHeaders` for response, `nil` to deny upgrading.
+    ///       See `NIOWebSocketServerUpgrader`.
+    ///   - onUpgrade: Closure to apply after web socket is upgraded successfully.
+    /// - returns: `Route` instance for newly created web socket endpoint
+    @preconcurrency
+    @discardableResult
+    public func webSocket(
+        _ path: PathComponent...,
+        maxFrameSize: WebSocketMaxFrameSize = .`default`,
+        shouldUpgrade: @escaping (@Sendable (Request) -> EventLoopFuture<HTTPHeaders?>) = {
+            $0.eventLoop.makeSucceededFuture([:])
+        },
+        onUpgrade: @Sendable @escaping (Request, WebSocket) -> ()
+    ) -> SendableRoute {
+        return self.webSocket(path, maxFrameSize: maxFrameSize, shouldUpgrade: shouldUpgrade, onUpgrade: onUpgrade)
+    }
+
+    /// Adds a route for opening a web socket connection
+    /// - parameters:
+    ///   - path: Array of path components.
+    ///   - maxFrameSize: The maximum allowed frame size. See `NIOWebSocketServerUpgrader`.
+    ///   - shouldUpgrade: Closure to apply before upgrade to web socket happens.
+    ///       Returns additional `HTTPHeaders` for response, `nil` to deny upgrading.
+    ///       See `NIOWebSocketServerUpgrader`.
+    ///   - onUpgrade: Closure to apply after web socket is upgraded successfully.
+    /// - returns: `Route` instance for newly created web socket endpoint
+    @preconcurrency
+    @discardableResult
+    public func webSocket(
+        _ path: [PathComponent],
+        maxFrameSize: WebSocketMaxFrameSize = .`default`,
+        shouldUpgrade: @escaping (@Sendable (Request) -> EventLoopFuture<HTTPHeaders?>) = {
+            $0.eventLoop.makeSucceededFuture([:])
+        },
+        onUpgrade: @Sendable @escaping (Request, WebSocket) -> ()
+    ) -> SendableRoute {
+        return self.on(.GET, path) { request -> Response in
+            let res = Response(status: .switchingProtocols)
+            res.upgrader = WebSocketUpgrader(maxFrameSize: maxFrameSize, shouldUpgrade: {
+                shouldUpgrade(request)
             }, onUpgrade: { ws in
                 onUpgrade(request, ws)
             })
