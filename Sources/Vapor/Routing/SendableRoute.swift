326e450250755939e30af07b92df6cdee38309c1
Tim <0xtimc@gmail.com>
2023-11-06 13:54:44 +0000

    Add SendableRoute type

---
+++ Sources/Vapor/Routing/SendableRoute.swift
@@ -0,0 +1,37 @@
+import NIOHTTP1
+@preconcurrency import RoutingKit
+
+public struct SendableRoute: CustomStringConvertible, Sendable {
+    public var method: HTTPMethod
+    public var path: [PathComponent]
+    public var responder: Responder
+    public var requestType: Any.Type
+    public var responseType: Any.Type
+    public var userInfo: [String: Sendable]
+
+    public var description: String {
+        let path = self.path.map { "\($0)" }.joined(separator: "/")
+        return "\(self.method.string) /\(path)"
+    }
+    
+    public init(
+        method: HTTPMethod,
+        path: [PathComponent],
+        responder: Responder,
+        requestType: Any.Type,
+        responseType: Any.Type
+    ) {
+        self.method = method
+        self.path = path
+        self.responder = responder
+        self.requestType = requestType
+        self.responseType = responseType
+        self.userInfo = [:]
+    }
+       
+    @discardableResult
+    public mutating func description(_ string: String) -> SendableRoute {
+        self.userInfo["description"] = string
+        return self
+    }
+}
