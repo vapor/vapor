d682e05fdb64c9f7da01af096a73cd11bb7ab755
Tim Condon <0xTim@users.noreply.github.com>
2023-11-07 13:14:41 +0000

    Make Async Request Body actually work (#3096)
    * Add an actual failing test for async request body
    
    * Fix event loop crash in async body
    
    * Fix logic bug with backpressure in async request body
    
    * Add backpressure test
    
    * Update Tests/AsyncTests/AsyncRequestTests.swift
    
    Co-authored-by: Gwynne Raskind <gwynne@vapor.codes>
    
    * Try working out failing test
    
    * Tell the delegate we stopped
    
    * Add test to ensure we clean up correctly
    
    * Disable dodgy test
    
    ---------
    
    Co-authored-by: Gwynne Raskind <gwynne@vapor.codes>

--- Sources/Vapor/Concurrency/RequestBody+Concurrency.swift
+++ Sources/Vapor/Concurrency/RequestBody+Concurrency.swift
@@ -1,4 +1,3 @@
-#if compiler(>=5.7)
 import NIOCore
 import NIOConcurrencyHelpers
 
@@ -13,11 +12,12 @@ extension Request.Body {
     /// in `Request.Body/makeAsyncIterator()` method.
     fileprivate final class AsyncSequenceDelegate: @unchecked Sendable, NIOAsyncSequenceProducerDelegate {
         private enum State {
+            case notCalledYet
             case noSignalReceived
             case waitingForSignalFromConsumer(EventLoopPromise<Void>)
         }
 
-        private var _state: State = .noSignalReceived
+        private var _state: State = .notCalledYet
         private let eventLoop: any EventLoop
 
         init(eventLoop: any EventLoop) {
@@ -27,6 +27,9 @@ extension Request.Body {
         private func produceMore0() {
             self.eventLoop.preconditionInEventLoop()
             switch self._state {
+            case .notCalledYet:
+                // We can just return here to sign to the producer that we want more data
+                break
             case .noSignalReceived:
                 preconditionFailure()
             case .waitingForSignalFromConsumer(let promise):
@@ -38,6 +41,9 @@ extension Request.Body {
         private func didTerminate0() {
             self.eventLoop.preconditionInEventLoop()
             switch self._state {
+            case .notCalledYet:
+                // Means didn't hit the backpressure limits, so just return
+                break
             case .noSignalReceived:
                 // we will inform the producer, since the next write will fail.
                 break
@@ -50,7 +56,7 @@ extension Request.Body {
         func registerBackpressurePromise(_ promise: EventLoopPromise<Void>) {
             self.eventLoop.preconditionInEventLoop()
             switch self._state {
-            case .noSignalReceived:
+            case .noSignalReceived, .notCalledYet:
                 self._state = .waitingForSignalFromConsumer(promise)
             case .waitingForSignalFromConsumer:
                 preconditionFailure()
@@ -140,6 +146,7 @@ extension Request.Body: AsyncSequence {
                     // The consumer dropped the sequence.
                     // Inform the producer that we don't want more data
                     // by returning an error in the future.
+                    delegate.didTerminate()
                     return request.eventLoop.makeFailedFuture(CancellationError())
                 case .stopProducing:
                     // The consumer is too slow.
@@ -166,4 +173,3 @@ extension Request.Body: AsyncSequence {
         return AsyncIterator(underlying: producer.sequence.makeAsyncIterator())
     }
 }
-#endif
