f8701a9797cf2e0e24c5bd1f2c2fc6dbd75fc354
Marius Seufzer <marius.seufzer@protonmail.com>
2024-01-08 10:07:17 +1300

    update routing kit version

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "4.0.0"),
 
         // 🚍 High-performance trie-node router.
-        .package(url: "https://github.com/vapor/routing-kit.git", from: "4.8.2"),
+        .package(url: "https://github.com/vapor/routing-kit.git", from: "4.9.0"),
 
         // 💥 Backtraces for Swift on Linux
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
