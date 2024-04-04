bae2a293223239bddc45c7a5712492afb7da5c32
Tim <0xtimc@gmail.com>
2024-04-04 01:32:12 +0100

    Migrate over to async APIs for FileIO

--- Tests/VaporTests/FileTests.swift
+++ Tests/VaporTests/FileTests.swift
@@ -468,4 +468,20 @@ final class FileTests: XCTestCase {
             XCTAssertEqual(res.status, .badRequest)
         }
     }
+    
+    func testAsyncFileWrite() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        
+        let request = Request(application: app, on: app.eventLoopGroup.next())
+        
+        let data = "Hello"
+        let path = "/tmp/fileio_write.txt"
+        
+        try await request.fileio.writeFile(ByteBuffer(string: data), at: path)
+        defer { try? FileManager.default.removeItem(atPath: path) }
+        
+        let result = try String(contentsOfFile: path)
+        XCTAssertEqual(result, data)
+    }
 }
