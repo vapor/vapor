44cde3c09eefd8e82eb4171825c56eeaf568e9de
Gwynne Raskind <gwynne@vapor.codes>
2024-05-05 16:46:43 -0500

    Heavily revise ChannelResponseBodyStream's logic to eliminate races, atomics, and to handle errors and the promise completions more consistently. Especially affects async streaming, but eliminates error handling issues in non-async streaming as well.

--- Sources/Vapor/HTTP/Server/HTTPServerResponseEncoder.swift
+++ Sources/Vapor/HTTP/Server/HTTPServerResponseEncoder.swift
@@ -1,7 +1,6 @@
 import NIOCore
 import NIOHTTP1
 import NIOConcurrencyHelpers
-import Atomics
 
 final class HTTPServerResponseEncoder: ChannelOutboundHandler, RemovableChannelHandler {
     typealias OutboundIn = Response
@@ -109,13 +108,14 @@ private final class ChannelResponseBodyStream: BodyStreamWriter, AsyncBodyStream
     let handlerBox: NIOLoopBound<HTTPServerResponseEncoder>
     let promise: EventLoopPromise<Void>?
     let count: Int?
-    let currentCount: ManagedAtomic<Int>
-    let isComplete: ManagedAtomic<Bool>
+    let currentCount: NIOLoopBoundBox<Int>
+    let isComplete: NIOLockedValueBox<Bool>
     let eventLoop: EventLoop
 
     enum Error: Swift.Error {
         case tooManyBytes
         case notEnoughBytes
+        case apiMisuse // tried to send a buffer or end indication after already ending or erroring the stream
     }
 
     init(
@@ -124,52 +124,79 @@ private final class ChannelResponseBodyStream: BodyStreamWriter, AsyncBodyStream
         promise: EventLoopPromise<Void>?,
         count: Int?
     ) {
+        context.eventLoop.assertInEventLoop()
+        
         self.contextBox = .init(context, eventLoop: context.eventLoop)
         self.handlerBox = .init(handler, eventLoop: context.eventLoop)
         self.promise = promise
         self.count = count
-        self.currentCount = .init(0)
+        self.currentCount = .init(0, eventLoop: context.eventLoop)
         self.isComplete = .init(false)
         self.eventLoop = context.eventLoop
     }
     
     func write(_ result: BodyStreamResult) async throws {
-        // Explicitly adds the ELF because Swift 5.6 fails to infer the return type
-        try await self.eventLoop.flatSubmit { () -> EventLoopFuture<Void> in
-            let promise = self.eventLoop.makePromise(of: Void.self)
-            self.write(result, promise: promise)
-            return promise.futureResult
-        }.get()
+        let promise = self.eventLoop.makePromise(of: Void.self)
+        
+        self.eventLoop.execute { self.write(result, promise: promise) }
+        try await promise.futureResult.get()
     }
     
+    /// > Note: `self.promise` is the promise that completes the original write to `HTTPServerResponseEncoder` that
+    /// > triggers the streaming response; it should only be succeeded when the stream ends. The `promise` parameter
+    /// > of this method is specific to the particular invocation and signals that a buffer has finished writing or
+    /// > that the stream has been fully completed, and should always be completed or pending completion by the time
+    /// > this method returns. Both promises should be failed when an error occurs, unless otherwise specifically noted.
     func write(_ result: BodyStreamResult, promise: EventLoopPromise<Void>?) {
+        self.eventLoop.assertInEventLoop() // Only check in debug, just in case...
+
+        func finishStream() {
+            self.isComplete.withLockedValue { $0 = true }
+            self.contextBox.value.fireUserInboundEventTriggered(HTTPServerResponseEncoder.ResponseEndSentEvent())
+            // Don't forward the current promise (if any) to the write completion of the end-response signal, as we
+            // will be notified of errors through other paths and can get spurious I/O errors from this write that
+            // ought to be ignored.
+            self.contextBox.value.writeAndFlush(self.handlerBox.value.wrapOutboundOut(.end(nil)), promise: nil)
+        }
+
+        // See https://github.com/vapor/vapor/issues/2976 for why we do some of these checks.
         switch result {
         case .buffer(let buffer):
-            // See: https://github.com/vapor/vapor/issues/2976
-            self.contextBox.value.writeAndFlush(self.handlerBox.value.wrapOutboundOut(.body(.byteBuffer(buffer))), promise: promise)
-            if let count = self.count, self.currentCount.wrappingIncrementThenLoad(by: buffer.readableBytes, ordering: .sequentiallyConsistent) > count {
+            guard !self.isComplete.withLockedValue({ $0 }) else { // Don't try to send data if we already ended
+                return promise?.fail(Error.apiMisuse) ?? () // self.promise is already completed, so fail the local one and bail
+            }
+            if let count = self.count, (self.currentCount.value + buffer.readableBytes) > count {
                 self.promise?.fail(Error.tooManyBytes)
-                promise?.fail(Error.notEnoughBytes)
+                promise?.fail(Error.tooManyBytes)
+            } else {
+                self.currentCount.value += buffer.readableBytes
+                // Cascade the completion of the buffer write to the local promise (if any).
+                self.contextBox.value.writeAndFlush(self.handlerBox.value.wrapOutboundOut(.body(.byteBuffer(buffer))), promise: promise)
             }
         case .end:
-            // See: https://github.com/vapor/vapor/issues/2976
-            self.isComplete.store(true, ordering: .sequentiallyConsistent)
-            if let count = self.count, self.currentCount.load(ordering: .sequentiallyConsistent) < count {
-                self.promise?.fail(Error.notEnoughBytes)
-                promise?.fail(Error.notEnoughBytes)
+            if !self.isComplete.withLockedValue({ $0 }) { // Don't send the response end events more than once.
+                finishStream()
+                // check this only after sending the stream end; we want to make send that regardless
+                if let count = self.count, self.currentCount.value < count {
+                    self.promise?.fail(Error.notEnoughBytes)
+                    promise?.fail(Error.notEnoughBytes)
+                } else {
+                    self.promise?.succeed()
+                    promise?.succeed()
+                }
+            } else {
+                promise?.fail(Error.apiMisuse) // If we already ended, fail the local promise with API misuse
             }
-            self.contextBox.value.fireUserInboundEventTriggered(HTTPServerResponseEncoder.ResponseEndSentEvent())
-            self.contextBox.value.writeAndFlush(self.handlerBox.value.wrapOutboundOut(.end(nil)), promise: promise)
-            self.promise?.succeed(())
         case .error(let error):
-            self.isComplete.store(true, ordering: .sequentiallyConsistent)
-            self.contextBox.value.fireUserInboundEventTriggered(HTTPServerResponseEncoder.ResponseEndSentEvent())
-            self.contextBox.value.writeAndFlush(self.handlerBox.value.wrapOutboundOut(.end(nil)), promise: promise)
-            self.promise?.fail(error)
+            if !self.isComplete.withLockedValue({ $0 }) { // Don't send the response end events more than once.
+                finishStream()
+                self.promise?.fail(error)
+            }
+            promise?.fail(error) // We want to fail the local promise regardless. Echo the error back.
         }
     }
 
     deinit {
-        assert(self.isComplete.load(ordering: .sequentiallyConsistent), "Response body stream writer deinitialized before .end or .error was sent.")
+        assert(self.isComplete.withLockedValue { $0 }, "Response body stream writer deinitialized before .end or .error was sent.")
     }
 }
