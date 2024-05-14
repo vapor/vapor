15cf9059e1f849a5fd0f1a5661d9cc958f796d2e
Tim <0xtimc@gmail.com>
2024-05-14 14:23:55 +0200

    Add async functions on Lifecycle handler

--- Sources/Vapor/Utilities/LifecycleHandler.swift
+++ Sources/Vapor/Utilities/LifecycleHandler.swift
@@ -2,10 +2,25 @@ public protocol LifecycleHandler: Sendable {
     func willBoot(_ application: Application) throws
     func didBoot(_ application: Application) throws
     func shutdown(_ application: Application)
+    func willBootAsync(_ application: Application) async throws
+    func didBootAsync(_ application: Application) async throws
+    func shutdownAsync(_ application: Application) async
 }
 
 extension LifecycleHandler {
     public func willBoot(_ application: Application) throws { }
     public func didBoot(_ application: Application) throws { }
     public func shutdown(_ application: Application) { }
+
+    func willBootAsync(_ application: Application) async throws { 
+        try self.willBoot(application)
+    }
+    
+    func didBootAsync(_ application: Application) async throws {
+        try self.didBoot(application)
+    }
+    
+    func shutdownAsync(_ application: Application) async {
+        self.shutdown(application)
+    }
 }
