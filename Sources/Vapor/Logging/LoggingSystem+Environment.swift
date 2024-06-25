211c82416d2216e322ac24a46b9305bcf20ebf2a
Tim <0xtimc@gmail.com>
2024-06-25 11:36:58 -0700

    Add preconcurrency attribute to avoid adding a new warning

--- Sources/Vapor/Logging/LoggingSystem+Environment.swift
+++ Sources/Vapor/Logging/LoggingSystem+Environment.swift
@@ -2,6 +2,7 @@ import Logging
 import ConsoleKit
 
 extension LoggingSystem {
+    @preconcurrency
     public static func bootstrap(from environment: inout Environment, _ factory: @Sendable (Logger.Level) -> (@Sendable (String) -> LogHandler)) throws {
         let level = try Logger.Level.detect(from: &environment)
 
