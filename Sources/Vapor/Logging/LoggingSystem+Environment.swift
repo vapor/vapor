1f50f15e3efb7f076d1258788ddb1ad334ec1b01
Tim <0xtimc@gmail.com>
2024-06-25 11:34:01 -0700

    Fix a new Sendable warning

--- Sources/Vapor/Logging/LoggingSystem+Environment.swift
+++ Sources/Vapor/Logging/LoggingSystem+Environment.swift
@@ -2,7 +2,7 @@ import Logging
 import ConsoleKit
 
 extension LoggingSystem {
-    public static func bootstrap(from environment: inout Environment, _ factory: (Logger.Level) -> (String) -> LogHandler) throws {
+    public static func bootstrap(from environment: inout Environment, _ factory: @Sendable (Logger.Level) -> (@Sendable (String) -> LogHandler)) throws {
         let level = try Logger.Level.detect(from: &environment)
 
         // Bootstrap logger with a factory created by the factoryfactory.
