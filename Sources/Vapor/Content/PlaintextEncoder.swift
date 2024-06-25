a8838fe388988a710e4ca9aa256ca8a10e28d8cc
Tim <0xtimc@gmail.com>
2024-06-24 22:20:49 -0700

    5.7 cleanups

--- Sources/Vapor/Content/PlaintextEncoder.swift
+++ Sources/Vapor/Content/PlaintextEncoder.swift
@@ -75,10 +75,8 @@ private final class _PlaintextEncoder: Encoder, SingleValueEncodingContainer {
             // special case for data
 #if swift(>=6)
             let utf8Maybe = data.withUnsafeBytes({ $0.withMemoryRebound(to: CChar.self, { String(validatingCString: $0.baseAddress!) }) })
-#elseif swift(>=5.7.2)
-            let utf8Maybe = data.withUnsafeBytes({ $0.withMemoryRebound(to: CChar.self, { String(validatingUTF8: $0.baseAddress!) }) })
 #else
-            let utf8Maybe = data.withUnsafeBytes({ String(validatingUTF8: $0.bindMemory(to: CChar.self).baseAddress!) })
+            let utf8Maybe = data.withUnsafeBytes({ $0.withMemoryRebound(to: CChar.self, { String(validatingUTF8: $0.baseAddress!) }) })
 #endif
             if let utf8 = utf8Maybe {
                 self.plaintext = utf8
