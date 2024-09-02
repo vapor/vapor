707454b257ce3f2f4fd06053648635a986defcee
Tim <0xtimc@gmail.com>
2024-09-02 22:19:44 +0100

    Migrate some websockets

--- Sources/Vapor/Routing/Request+WebSocket.swift
+++ Sources/Vapor/Routing/Request+WebSocket.swift
@@ -5,14 +5,14 @@ import NIOHTTP1
 extension Request {
     public func webSocket(
          maxFrameSize: WebSocketMaxFrameSize = .`default`,
-         shouldUpgrade: @escaping (@Sendable (Request) -> EventLoopFuture<HTTPHeaders?>) = {
-             $0.eventLoop.makeSucceededFuture([:])
+         shouldUpgrade: @escaping (@Sendable (Request) async throws -> HTTPHeaders?) = { _ in
+             [:]
          },
          onUpgrade: @Sendable @escaping (Request, WebSocket) -> ()
      ) -> Response {
          let res = Response(status: .switchingProtocols)
          res.upgrader = WebSocketUpgrader(maxFrameSize: maxFrameSize, shouldUpgrade: {
-             shouldUpgrade(self)
+             try await shouldUpgrade(self)
          }, onUpgrade: { ws in
              onUpgrade(self, ws)
          })
