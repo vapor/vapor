ec8b6b2dfa7e69a26bdf70d226b640087d609ed6
Tim <0xtimc@gmail.com>
2024-09-02 18:35:56 +0100

    Start getting rid of ELFs

--- Sources/Vapor/HTTP/BasicResponder.swift
+++ Sources/Vapor/HTTP/BasicResponder.swift
@@ -3,31 +3,26 @@ import NIOCore
 /// A basic, closure-based `Responder`.
 public struct BasicResponder: Responder {
     /// The stored responder closure.
-    private let closure: @Sendable (Request) throws -> EventLoopFuture<Response>
+    private let closure: @Sendable (Request) async throws -> Response
 
     /// Create a new `BasicResponder`.
     ///
     ///     let notFound: Responder = BasicResponder { req in
-    ///         let res = req.response(http: .init(status: .notFound))
-    ///         return req.eventLoop.newSucceededFuture(result: res)
+    ///         return req.response(http: .init(status: .notFound))
     ///     }
     ///
     /// - parameters:
     ///     - closure: Responder closure.
     public init(
-        closure: @Sendable @escaping (Request) throws -> EventLoopFuture<Response>
+        closure: @Sendable @escaping (Request) async throws -> Response
     ) {
         self.closure = closure
     }
 
-    /// See `Responder`.
-    public func respond(to request: Request) -> EventLoopFuture<Response> {
-        do {
-            return try request.propagateTracingIfEnabled {
-                try closure(request)
-            }
-        } catch {
-            return request.eventLoop.makeFailedFuture(error)
+    // See `Responder`.
+    public func respond(to request: Request) async throws -> Response {
+        return try request.propagateTracingIfEnabled {
+            try await closure(request)
         }
     }
 }
