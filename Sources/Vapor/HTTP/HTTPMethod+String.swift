c3270ba12f8a4d20fc17732d2f0e70dcaa778e43
RandomHashTags <imrandomhashtags@gmail.com>
2024-11-01 11:07:36 -0500

    added deprecation annotation

--- Sources/Vapor/HTTP/HTTPMethod+String.swift
+++ Sources/Vapor/HTTP/HTTPMethod+String.swift
@@ -2,6 +2,7 @@ import NIOHTTP1
 
 extension HTTPMethod {
     /// `String` representation of this `HTTPMethod`.
+    @available(*, deprecated, message: "Use 'rawValue' instead")
     public var string: String {
         self.rawValue
     }
