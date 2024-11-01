808021d61c5c7fa668df84808fb0570d70cbbb62
Evan Anderson <ImRandomHashTags@gmail.com>
2024-11-01 10:30:57 -0500

    Update Sources/Vapor/HTTP/HTTPMethod+String.swift
    Makes sense
    
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>

--- Sources/Vapor/HTTP/HTTPMethod+String.swift
+++ Sources/Vapor/HTTP/HTTPMethod+String.swift
@@ -3,10 +3,6 @@ import NIOHTTP1
 extension HTTPMethod {
     /// `String` representation of this `HTTPMethod`.
     public var string: String {
-        switch self {
-        case .GET: return "GET"
-        case .RAW(let value): return value
-        default: return "\(self)"
-        }
+        self.rawValue
     }
 }
