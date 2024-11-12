36e5a8008a7474356351ac511fbe9091c4fe5ede
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-11-11 22:51:20 -0700

    build: Adds tracing dependencies

--- Package.swift
+++ Package.swift
@@ -47,6 +47,12 @@ let package = Package(
 
         // Swift metrics API
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.5.0"),
+        
+        // Swift tracing API
+        .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.0.0"),
+        
+        // Swift service context
+        .package(url: "https://github.com/apple/swift-service-context.git", from: "1.0.0"),
 
         // Swift collection algorithms
         .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.0.0"),
@@ -74,6 +80,8 @@ let package = Package(
                 .product(name: "ConsoleKit", package: "console-kit"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "Metrics", package: "swift-metrics"),
+                .product(name: "Tracing", package: "swift-distributed-tracing"),
+                .product(name: "ServiceContextModule", package: "swift-service-context"),
                 .product(name: "NIO", package: "swift-nio"),
                 .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
                 .product(name: "NIOCore", package: "swift-nio"),
