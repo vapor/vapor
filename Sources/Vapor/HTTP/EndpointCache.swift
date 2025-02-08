e6b6748bb9cd46b15a203ea0a218e86bd75fbcba
Tim <0xtimc@gmail.com>
2025-02-08 02:14:29 +0000

    Migrate endopint cache to async

--- Sources/Vapor/HTTP/EndpointCache.swift
+++ Sources/Vapor/HTTP/EndpointCache.swift
@@ -10,11 +10,10 @@ public enum EndpointCacheError: Swift.Error {
 }
 
 /// Handles the complexities of HTTP caching.
-public final class EndpointCache<T>: Sendable where T: Decodable & Sendable {
-    private let cached: NIOLockedValueBox<(T?, Date?)>
-    private let request: NIOLockedValueBox<EventLoopFuture<T>?>
-    private let headers: NIOLockedValueBox<HTTPHeaders?>
-    private let sync: NIOLock
+public actor EndpointCache<T>: Sendable where T: Decodable & Sendable {
+    private var cached: (T?, Date?)
+    private var request: T?
+    private var headers: HTTPHeaders?
     private let uri: URI
 
     /// The designated initializer.
@@ -22,18 +21,17 @@ public final class EndpointCache<T>: Sendable where T: Decodable & Sendable {
     ///   - uri: The `URI` of the resource to be downloaded.
     public init(uri: URI) {
         self.uri = uri
-        self.sync = .init()
-        self.request = .init(nil)
-        self.headers = .init(nil)
-        self.cached = .init((nil, nil))
+        self.request = nil
+        self.headers = nil
+        self.cached = (nil, nil)
     }
 
     /// Downloads the resource.
     /// - Parameters:
     ///   - request: The `Request` which is initiating the download.
     ///   - logger: An optional logger
-    public func get(on request: Request, logger: Logger? = nil) -> EventLoopFuture<T> {
-        return self.download(on: request.eventLoop, using: request.client, logger: logger ?? request.logger)
+    public func get(on request: Request, logger: Logger? = nil) async throws -> T {
+        try await self.download(on: request.eventLoop, using: request.client, logger: logger ?? request.logger)
     }
 
     /// Downloads the resource.
@@ -41,147 +39,121 @@ public final class EndpointCache<T>: Sendable where T: Decodable & Sendable {
     ///   - eventLoop: The `EventLoop` to use for the download.
     ///   - client: The `Client` which will perform the download.
     ///   - logger: An optional logger
-    public func get(using client: Client, logger: Logger? = nil, on eventLoop: EventLoop) -> EventLoopFuture<T> {
-        self.sync.lock()
-        defer { self.sync.unlock() }
-
-        let cachedData = self.cached.withLockedValue { $0 }
-        if let cached = cachedData.0, let cacheUntil = cachedData.1, Date() < cacheUntil {
+    public func get(using client: Client, logger: Logger? = nil, on eventLoop: EventLoop) async throws -> T {
+        if let cached = self.cached.0, let cacheUntil = self.cached.1, Date() < cacheUntil {
             // If no-cache was set on the header, you *always* have to validate with the server.
             // must-revalidate does not require checking with the server until after it expires.
-            let cachedHeaders = self.headers.withLockedValue { $0 }
-            if cachedHeaders == nil || cachedHeaders?.cacheControl == nil || cachedHeaders?.cacheControl?.noCache == false {
-                return eventLoop.makeSucceededFuture(cached)
+            if self.headers == nil || self.headers?.cacheControl == nil || self.headers?.cacheControl?.noCache == false {
+                return cached
             }
         }
 
         // Don't make a new request if one is already running.
-        if let request = self.request.withLockedValue({ $0 }) {
+        if let request {
             // The current request may be happening on a different event loop.
-            return request.hop(to: eventLoop)
+            return request
         }
 
         logger?.debug("Requesting data from \(self.uri)")
 
-        let request = self.download(on: eventLoop, using: client, logger: logger)
-        self.request.withLockedValue { $0 = request }
+        let newRequest = try await self.download(on: eventLoop, using: client, logger: logger)
+        self.request = newRequest
 
+        let request = try await self.download(on: eventLoop, using: client, logger: logger)
         // Once the request finishes, clear the current request and return the data.
-        return request.map { data in
-            // Synchronize access to shared state
-            self.sync.lock()
-            defer { self.sync.unlock() }
-            self.request.withLockedValue { $0 = nil }
+        self.request = nil
 
-            return data
-        }
+        return request
     }
 
-    private func download(on eventLoop: EventLoop, using client: Client, logger: Logger?) -> EventLoopFuture<T> {
-        return eventLoop.makeFutureWithTask {
-            // https://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.3.4
-            var headers: HTTPHeaders = [:]
-            self.headers.withLockedValue { cachedHeaders in
-                if let eTag = cachedHeaders?.first(name: .eTag) {
-                    headers.add(name: .ifNoneMatch, value: eTag)
-                }
+    private func download(on eventLoop: EventLoop, using client: Client, logger: Logger?) async throws -> T {
+        // https://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.3.4
+        var headers: HTTPHeaders = [:]
 
-                if let lastModified = cachedHeaders?.lastModified {
-                    // TODO: If using HTTP/1.0 then this should be .ifUnmodifiedSince instead. Don't know
-                    // how to determine that right now.
-                    headers.add(name: .ifModifiedSince, value: lastModified.serialize())
-                }
+        if let eTag = self.headers?.first(name: .eTag) {
+            headers.add(name: .ifNoneMatch, value: eTag)
+        }
+
+        if let lastModified = self.headers?.lastModified {
+            // TODO: If using HTTP/1.0 then this should be .ifUnmodifiedSince instead. Don't know
+            // how to determine that right now.
+            headers.add(name: .ifModifiedSince, value: lastModified.serialize())
+        }
+
+        // Cache-Control max-age is calculated against the request date.
+        let requestSentAt = Date()
+
+        do {
+            let response = try await client.get(self.uri, headers: headers)
+            if !(response.status == .notModified || response.status == .ok) {
+                throw EndpointCacheError.unexpctedResponseStatus(response.status, uri: self.uri)
             }
 
-            // Cache-Control max-age is calculated against the request date.
-            let requestSentAt = Date()
-
-            do {
-                let response = try await client.get(self.uri, headers: headers)
-                if !(response.status == .notModified || response.status == .ok) {
-                    throw EndpointCacheError.unexpctedResponseStatus(response.status, uri: self.uri)
-                }
-                // Synchronize access to shared state.
-                self.sync.lock()
-                defer { self.sync.unlock() }
-
-                if let cacheControl = response.headers.cacheControl, cacheControl.noStore == true {
-                    // The server *shouldn't* give an expiration with no-store, but...
-                    self.clearCache()
-                } else {
-                    self.headers.withLockedValue { headers in
-                        headers = response.headers
-                        self.cached.withLockedValue { $0.1 = headers?.expirationDate(requestSentAt: requestSentAt) }
-                    }
-                }
-
-                switch response.status {
-                case .notModified:
-                    logger?.debug("Cached data is still valid.")
-
-                    let cachedData = self.cached.withLockedValue({ $0 })
-                    guard let cached = cachedData.0 else {
-                        // This shouldn't actually be possible, but just in case.
-                        self.clearCache()
-                        return try await self.download(on: eventLoop, using: client, logger: logger).get()
-                    }
-
-                    return cached
-
-                case .ok:
-                    logger?.debug("New data received")
-
-                    let data: T
-
-                    do {
-                        data = try response.content.decode(T.self)
-                    } catch {
-                        throw EndpointCacheError.contentDecodeFailure(error)
-                    }
-
-                    self.cached.withLockedValue { cachedData in
-                        if cachedData.1 != nil {
-                            cachedData.0 = data
-                        }
-                    }
-
-                    return data
-
-                default:
-                    // This shouldn't ever happen
-                    throw Abort(.internalServerError)
-                }
-            } catch {
-                // Synchronize access to shared state.
-                self.sync.lock()
-                defer { self.sync.unlock() }
-
-                let cachedData = self.cached.withLockedValue { $0 }
-                guard let headers = self.headers.withLockedValue({ $0 }), let cached = cachedData.0 else {
-                    throw error
-                }
-
-                if let cacheControl = headers.cacheControl, let cacheUntil = cachedData.1 {
-                    if let staleIfError = cacheControl.staleIfError,
-                        cacheUntil.addingTimeInterval(Double(staleIfError)) > Date() {
-                        // Can use the data for staleIfError seconds past expiration when the server is non-responsive
-                        return cached
-                    } else if cacheControl.noCache == true && cacheUntil > Date() {
-                        // The spec isn't very clear here.  If no-cache is present you're supposed to validate with the
-                        // server. However, if the server doesn't respond, but I'm still within the expiration time, I'm
-                        // opting to say that the cache should be considered usable.
-                        return cached
-                    }
-                }
-
+            if let cacheControl = response.headers.cacheControl, cacheControl.noStore == true {
+                // The server *shouldn't* give an expiration with no-store, but...
                 self.clearCache()
+            } else {
+                headers = response.headers
+                self.cached.1 = headers.expirationDate(requestSentAt: requestSentAt)
+            }
+
+            switch response.status {
+            case .notModified:
+                logger?.debug("Cached data is still valid.")
+                guard let cached = self.cached.0 else {
+                    // This shouldn't actually be possible, but just in case.
+                    self.clearCache()
+                    return try await self.download(on: eventLoop, using: client, logger: logger)
+                }
+
+                return cached
+
+            case .ok:
+                logger?.debug("New data received")
+
+                let data: T
+
+                do {
+                    data = try response.content.decode(T.self)
+                } catch {
+                    throw EndpointCacheError.contentDecodeFailure(error)
+                }
+
+                if self.cached.1 != nil {
+                    self.cached.0 = data
+                }
+
+                return data
+
+            default:
+                // This shouldn't ever happen
+                throw Abort(.internalServerError)
+            }
+        } catch {
+            guard let headers = self.headers, let cached = self.cached.0 else {
                 throw error
             }
+
+            if let cacheControl = headers.cacheControl, let cacheUntil = self.cached.1 {
+                if let staleIfError = cacheControl.staleIfError,
+                    cacheUntil.addingTimeInterval(Double(staleIfError)) > Date() {
+                    // Can use the data for staleIfError seconds past expiration when the server is non-responsive
+                    return cached
+                } else if cacheControl.noCache == true && cacheUntil > Date() {
+                    // The spec isn't very clear here.  If no-cache is present you're supposed to validate with the
+                    // server. However, if the server doesn't respond, but I'm still within the expiration time, I'm
+                    // opting to say that the cache should be considered usable.
+                    return cached
+                }
+            }
+
+            self.clearCache()
+            throw error
         }
     }
 
     private func clearCache() {
-        self.cached.withLockedValue { $0 = (nil, nil) }
-        self.headers.withLockedValue { $0 = nil }
+        self.cached = (nil, nil)
+        self.headers = nil
     }
 }
--- Tests/VaporTests/EndpointCacheTests.swift
+++ Tests/VaporTests/EndpointCacheTests.swift
@@ -1,20 +1,10 @@
-import XCTVapor
-import XCTest
+import VaporTesting
+import Testing
 import Vapor
 import NIOCore
 
-final class EndpointCacheTests: XCTestCase {
-
-    var app: Application!
-
-    override func setUp() async throws {
-        app = try await Application(.testing)
-    }
-
-    override func tearDown() async throws {
-        try await app.shutdown()
-    }
-
+@Suite("Endpoint Cache Tests")
+struct EndpointCacheTests {
     actor CurrentActor {
         var current = 0
         
@@ -27,83 +17,88 @@ final class EndpointCacheTests: XCTestCase {
         }
     }
     
-    
-    func testEndpointCacheNoCache() throws {
+    @Test("Test cache is filled when there is no cache entry yet")
+    func endpointCacheNoCache() async throws {
         let currentActor = CurrentActor()
         struct Test: Content {
             let number: Int
         }
 
-        app.get("number") { req -> Test in
-            let current = await currentActor.getCurrent()
-            await currentActor.increment()
-            return Test(number: current)
-        }
+        try await withApp { app in
+            app.get("number") { req -> Test in
+                let current = await currentActor.getCurrent()
+                await currentActor.increment()
+                return Test(number: current)
+            }
 
-        app.clients.use(.responder)
+            app.clients.use(.responder)
 
-        let cache = EndpointCache<Test>(uri: "/number")
-        do {
-            let test = try cache.get(
-                using: app.client,
-                logger: app.logger,
-                on: app.eventLoopGroup.next()
-            ).wait()
-            XCTAssertEqual(test.number, 0)
-        }
-        do {
-            let test = try cache.get(
-                using: app.client,
-                logger: app.logger,
-                on: app.eventLoopGroup.next()
-            ).wait()
-            XCTAssertEqual(test.number, 1)
+            let cache = EndpointCache<Test>(uri: "/number")
+            do {
+                let test = try await cache.get(
+                    using: app.client,
+                    logger: app.logger,
+                    on: app.eventLoopGroup.next()
+                )
+                #expect(test.number == 0)
+            }
+            do {
+                let test = try await cache.get(
+                    using: app.client,
+                    logger: app.logger,
+                    on: app.eventLoopGroup.next()
+                )
+                #expect(test.number == 1)
+            }
         }
     }
 
-    func testEndpointCacheMaxAge() throws {
-        let currentActor = CurrentActor()
-        struct Test: Content {
-            let number: Int
-        }
+    @Test("Test cache is refreshed when cache age is expired")
+    func testEndpointCacheMaxAge() async throws {
+        try await withApp { app in
+            let currentActor = CurrentActor()
+            struct Test: Content {
+                let number: Int
+            }
 
-        app.clients.use(.responder)
+            app.clients.use(.responder)
 
-        app.get("number") { req -> Response in
-            let res = Response()
-            let current = await currentActor.getCurrent()
-            try res.content.encode(Test(number: current))
-            res.headers.cacheControl = .init(maxAge: 1)
-            await currentActor.increment()
-            return res
-        }
+            app.get("number") { req -> Response in
+                let res = Response()
+                let current = await currentActor.getCurrent()
+                try res.content.encode(Test(number: current))
+                res.headers.cacheControl = .init(maxAge: 1)
+                await currentActor.increment()
+                return res
+            }
 
-        let cache = EndpointCache<Test>(uri: "/number")
-        do {
-            let test = try cache.get(
-                using: app.client,
-                logger: app.logger,
-                on: app.eventLoopGroup.next()
-            ).wait()
-            XCTAssertEqual(test.number, 0)
-        }
-        do {
-            let test = try cache.get(
-                using: app.client,
-                logger: app.logger,
-                on: app.eventLoopGroup.next()
-            ).wait()
-            XCTAssertEqual(test.number, 0)
-        }
-        // wait for expiry
-        sleep(1)
-        do {
-            let test = try cache.get(
-                using: app.client,
-                logger: app.logger,
-                on: app.eventLoopGroup.next()
-            ).wait()
-            XCTAssertEqual(test.number, 1)
+            let cache = EndpointCache<Test>(uri: "/number")
+            do {
+                let test = try await cache.get(
+                    using: app.client,
+                    logger: app.logger,
+                    on: app.eventLoopGroup.next()
+                )
+                #expect(test.number == 0)
+            }
+            do {
+                let test = try await cache.get(
+                    using: app.client,
+                    logger: app.logger,
+                    on: app.eventLoopGroup.next()
+                )
+                #expect(test.number == 0)
+            }
+            // wait for expiry
+            try await Task.sleep(for: .seconds(1))
+            do {
+                let test = try await cache.get(
+                    using: app.client,
+                    logger: app.logger,
+                    on: app.eventLoopGroup.next()
+                )
+                #expect(test.number == 1)
+            }
         }
     }
 }
