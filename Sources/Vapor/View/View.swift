5c393becc49d0fc3b1cbe08c9c7e2da0d3510a85
Tim <0xtimc@gmail.com>
2025-02-08 01:11:12 +0000

    Remove futures from View

--- Sources/Vapor/View/View.swift
+++ Sources/Vapor/View/View.swift
@@ -1,18 +1,18 @@
 import NIOCore
 
-public struct View: ResponseEncodable, Sendable {
+public struct View: AsyncResponseEncodable, Sendable {
     public var data: ByteBuffer
 
     public init(data: ByteBuffer) {
         self.data = data
     }
 
-    public func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
+    public func encodeResponse(for request: Request) async throws -> Response {
         let response = Response()
         response.responseBox.withLockedValue { box in
             box.headers.contentType = .html
-            box.body = .init(buffer: self.data, byteBufferAllocator: request.byteBufferAllocator)
+            box.body = .init(buffer: self.data)
         }
-        return request.eventLoop.makeSucceededFuture(response)
+        return response
     }
 }
