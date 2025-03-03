c9b4352d652a6812dca3ec10afdcef32802549ef
Tim <0xtimc@gmail.com>
2025-03-03 02:08:45 +0000

    Stop the tracing tests from hanging

--- Tests/VaporTests/MiddlewareTests.swift
+++ Tests/VaporTests/MiddlewareTests.swift
@@ -152,41 +152,73 @@ struct MiddlewareTests2 {
                 return "done"
             }
 
-            try await app.server.start(address: .hostname("127.0.0.1", port: 0))
+            try await app.testing(method: .running).test(.get, "/testTracing?foo=bar", beforeRequest: {
+                $0.headers[.userAgent] = "test"
+                $0.headers[TestTracer.extractKey] = "extracted"
+            }) { response in
+                #expect(response.status == .ok)
+                #expect(response.body.string == "done")
 
-            let port = try #require(app.http.server.shared.localAddress?.port, "Failed to get port")
-            let response = try await app.client.get("http://localhost:\(port)/testTracing?foo=bar") { req in
-                req.headers[.userAgent] = "test"
-                req.headers[TestTracer.extractKey] = "extracted"
+                let span = try #require(tracer.spans.first)
+                #expect(span.operationName == "GET /testTracing")
+
+                #expect(span.attributes["http.request.method"]?.toSpanAttribute() == "GET")
+                #expect(span.attributes["url.path"]?.toSpanAttribute() == "/testTracing")
+                #expect(span.attributes["url.scheme"]?.toSpanAttribute() == nil)
+
+                #expect(span.attributes["http.route"]?.toSpanAttribute() == "/testTracing")
+                #expect(span.attributes["network.protocol.name"]?.toSpanAttribute() == "http")
+                #expect(span.attributes["server.address"]?.toSpanAttribute() == "127.0.0.1")
+                let port = try #require(app.sharedNewAddress.withLockedValue({ $0 })?.port, "Failed to get port")
+                #expect(span.attributes["server.port"]?.toSpanAttribute() == port.toSpanAttribute())
+                #expect(span.attributes["url.query"]?.toSpanAttribute() == "foo=bar")
+
+                #expect(span.attributes["client.address"]?.toSpanAttribute() == "127.0.0.1")
+                #expect(span.attributes["network.peer.address"]?.toSpanAttribute() == "127.0.0.1")
+                #expect(span.attributes["network.peer.port"]?.toSpanAttribute() != nil)
+                #expect(span.attributes["network.protocol.version"]?.toSpanAttribute() == "1.1")
+                #expect(span.attributes["user_agent.original"]?.toSpanAttribute() == "test")
+
+                #expect(span.attributes["custom"]?.toSpanAttribute() == "custom")
+
+                #expect(span.attributes["http.response.status_code"]?.toSpanAttribute() == 200)
             }
 
-            #expect(response.status == .ok)
-            #expect(response.body?.string == "done")
-
-            let span = try #require(tracer.spans.first)
-            #expect(span.operationName == "GET /testTracing")
-
-            #expect(span.attributes["http.request.method"]?.toSpanAttribute() == "GET")
-            #expect(span.attributes["url.path"]?.toSpanAttribute() == "/testTracing")
-            #expect(span.attributes["url.scheme"]?.toSpanAttribute() == nil)
-
-            #expect(span.attributes["http.route"]?.toSpanAttribute() == "/testTracing")
-            #expect(span.attributes["network.protocol.name"]?.toSpanAttribute() == "http")
-            #expect(span.attributes["server.address"]?.toSpanAttribute() == "127.0.0.1")
-            #expect(span.attributes["server.port"]?.toSpanAttribute() == port.toSpanAttribute())
-            #expect(span.attributes["url.query"]?.toSpanAttribute() == "foo=bar")
-
-            #expect(span.attributes["client.address"]?.toSpanAttribute() == "127.0.0.1")
-            #expect(span.attributes["network.peer.address"]?.toSpanAttribute() == "127.0.0.1")
-            #expect(span.attributes["network.peer.port"]?.toSpanAttribute() != nil)
-            #expect(span.attributes["network.protocol.version"]?.toSpanAttribute() == "1.1")
-            #expect(span.attributes["user_agent.original"]?.toSpanAttribute() == "test")
-
-            #expect(span.attributes["custom"]?.toSpanAttribute() == "custom")
-
-            #expect(span.attributes["http.response.status_code"]?.toSpanAttribute() == 200)
-
-            try await app.server.shutdown()
+//            try await app.server.start(address: .hostname("127.0.0.1", port: 0))
+//
+//            let port = try #require(app.sharedNewAddress.withLockedValue({ $0 })?.port, "Failed to get port")
+//            let response = try await app.client.get("http://localhost:\(port)/testTracing?foo=bar") { req in
+//                req.headers[.userAgent] = "test"
+//                req.headers[TestTracer.extractKey] = "extracted"
+//            }
+//
+//            #expect(response.status == .ok)
+//            #expect(response.body?.string == "done")
+//
+//            let span = try #require(tracer.spans.first)
+//            #expect(span.operationName == "GET /testTracing")
+//
+//            #expect(span.attributes["http.request.method"]?.toSpanAttribute() == "GET")
+//            #expect(span.attributes["url.path"]?.toSpanAttribute() == "/testTracing")
+//            #expect(span.attributes["url.scheme"]?.toSpanAttribute() == nil)
+//
+//            #expect(span.attributes["http.route"]?.toSpanAttribute() == "/testTracing")
+//            #expect(span.attributes["network.protocol.name"]?.toSpanAttribute() == "http")
+//            #expect(span.attributes["server.address"]?.toSpanAttribute() == "127.0.0.1")
+//            #expect(span.attributes["server.port"]?.toSpanAttribute() == port.toSpanAttribute())
+//            #expect(span.attributes["url.query"]?.toSpanAttribute() == "foo=bar")
+//
+//            #expect(span.attributes["client.address"]?.toSpanAttribute() == "127.0.0.1")
+//            #expect(span.attributes["network.peer.address"]?.toSpanAttribute() == "127.0.0.1")
+//            #expect(span.attributes["network.peer.port"]?.toSpanAttribute() != nil)
+//            #expect(span.attributes["network.protocol.version"]?.toSpanAttribute() == "1.1")
+//            #expect(span.attributes["user_agent.original"]?.toSpanAttribute() == "test")
+//
+//            #expect(span.attributes["custom"]?.toSpanAttribute() == "custom")
+//
+//            #expect(span.attributes["http.response.status_code"]?.toSpanAttribute() == 200)
+//
+//            try await app.server.shutdown()
         }
     }
 }
