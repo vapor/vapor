3313d2ec1bea23b27021b72dead5ce181dd8bdba
vamsii777 <vamsi.madduluri@icloud.com>
2025-06-16 20:20:23 +0530

    Add conditional Sendable conformance to RouteCollection for Swift 6

--- Sources/Vapor/Routing/RouteCollection.swift
+++ Sources/Vapor/Routing/RouteCollection.swift
@@ -1,4 +1,13 @@
 /// Groups collections of routes together for adding to a router.
+#if compiler(>=6.0)
+public protocol RouteCollection: Sendable {
+    /// Registers routes to the incoming router.
+    ///
+    /// - parameters:
+    ///     - routes: RoutesBuilder to register any new routes to.
+    func boot(routes: RoutesBuilder) throws
+}
+#else
 public protocol RouteCollection {
     /// Registers routes to the incoming router.
     ///
@@ -6,6 +15,7 @@ public protocol RouteCollection {
     ///     - routes: `RoutesBuilder` to register any new routes to.
     func boot(routes: RoutesBuilder) throws
 }
+#endif
 
 extension RoutesBuilder {
     /// Registers all of the routes in the group to this router.
