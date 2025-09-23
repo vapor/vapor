f11455a5702d1ba7c4f33951f26a2007e01d6057
Tim <0xtimc@gmail.com>
2025-09-23 20:59:22 +0100

    Fix merge issue

--- Tests/VaporTests/ApplicationCreationTests.swift
+++ Tests/VaporTests/ApplicationCreationTests.swift
@@ -1,22 +1,23 @@
 import Vapor
 import XCTVapor
 import XCTest
+import Logging
 
 final class ApplicationCreationTests: XCTestCase {
     var app: Application!
 
     override func tearDown() async throws {
-        try await app.asyncShutdown()
+        try await app.shutdown()
     }
 
     func testCreateAsyncDefaultLogger() async throws {
-        app = try await Application.make(.testing)
+        app = try await Application(.testing)
         XCTAssertEqual(app.logger.label, "codes.vapor.application")
     }
 
     func testCreateAsyncCustomLogger() async throws {
         let logger = Logger(label: "custom")
-        app = try await Application.make(.testing, logger: logger)
+        app = try await Application(.testing, services: .init(logger: .provided(logger)))
         XCTAssertEqual(app.logger.label, "custom")
     }
 }
