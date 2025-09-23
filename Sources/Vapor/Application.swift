307c6c384c0d814f1dcef0b04c31c08d42195092
Tim <0xtimc@gmail.com>
2025-09-23 20:57:06 +0100

    Fix compiler errors

--- Sources/Vapor/Application.swift
+++ Sources/Vapor/Application.swift
@@ -268,7 +268,6 @@ public final class Application: Sendable, Service {
         self._locks = .init(.init())
         self._didShutdown = .init(false)
         self._logger = .init(logger)
-        self._traceAutoPropagation = .init(false)
         self._storage = .init(.init(logger: logger))
         self._lifecycle = .init(.init())
         self.isBooted = .init(false)
@@ -312,8 +311,6 @@ public final class Application: Sendable, Service {
 
         self.responder = services.responder
         self.routes = Routes()
-        self._logger = .init(logger)
-
         self.core.initialize()
         self.sessions.initialize()
         self.sessions.use(.memory)
