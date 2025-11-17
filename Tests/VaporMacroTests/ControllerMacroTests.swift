059a11b6d14d841e95028abf160ec778f6e3faab
Tim <0xtimc@gmail.com>
2025-11-17 01:22:21 +0000

    First controller test working

--- Tests/VaporMacroTests/ControllerMacroTests.swift
+++ Tests/VaporMacroTests/ControllerMacroTests.swift
@@ -5,6 +5,38 @@ import Testing
 struct ControllerMacroTests {
     @Test("Test Basic Controller")
     func testBasicController() async throws {
-
+        assertMacroExpansion(
+            """
+            @Controller
+            struct UserController {
+                @GET("api", "macros", "users")
+                func getUsers(req: Request) async throws -> String {
+                    return "Users"
+                }
+            }
+            """,
+            expandedSource: """
+            struct UserController {
+                func getUsers(req: Request) async throws -> String {
+                    return "Users"
+                }
+            
+                func _route_getUsers(req: Request) async throws -> Response {
+                    let result = try await getUsers(req: req)
+                    return try await result.encodeResponse(for: req)
+                }
+            }
+            
+            extension UserController: RouteCollection {
+                func boot(routes: any RoutesBuilder) throws {
+                routes.get("api", "macros", "users") { req async throws in
+                        try await self.getUsers(req: req)
+                    }
+                }
+            }
+            """,
+            macroSpecs: testMacros,
+            failureHandler: FailureHandler.instance
+        )
     }
 }
--- Tests/VaporMacroTests/Macros+SwiftTesting.swift
+++ Tests/VaporMacroTests/Macros+SwiftTesting.swift
@@ -1,5 +1,7 @@
 import Testing
 import SwiftSyntaxMacrosGenericTestSupport
+import SwiftSyntaxMacroExpansion
+import VaporMacrosPlugin
 
 enum FailureHandler {
     static func instance(_ spec: TestFailureSpec) {
@@ -14,3 +16,13 @@ enum FailureHandler {
         )
     }
 }
+
+let testMacros: [String: MacroSpec] = [
+    "GET": MacroSpec(type: HTTPGetMacro.self),
+    "POST": MacroSpec(type: HTTPPostMacro.self),
+    "PUT": MacroSpec(type: HTTPPutMacro.self),
+    "DELETE": MacroSpec(type: HTTPDeleteMacro.self),
+    "PATCH": MacroSpec(type: HTTPPatchMacro.self),
+    "HTTP": MacroSpec(type: HTTPMethodMacro.self),
+    "Controller": MacroSpec(type: ControllerMacro.self),
+]
