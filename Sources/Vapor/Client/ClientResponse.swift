6c28d01af078daf9199f2bb265d07558bbf1e31c
Tim <0xtimc@gmail.com>
2026-08-28 22:28:05 +0100

    Migrate ClientResponse to streaming bodies

--- Sources/Vapor/Client/ClientResponse.swift
+++ Sources/Vapor/Client/ClientResponse.swift
@@ -1,6 +1,3 @@
-#warning("Make this internal")
-public import NIOCore
-import NIOFoundationEssentialsCompat
 public import HTTPTypes
 #if canImport(FoundationEssentials)
 import FoundationEssentials
@@ -11,24 +8,25 @@ import Foundation
 public struct ClientResponse: Sendable {
     public var status: HTTPResponse.Status
     public var headers: HTTPFields
-    public var body: ByteBuffer?
-    private let byteBufferAllocator: ByteBufferAllocator
+    public var body: Response.Body {
+        didSet {
+            self.headers.updateContentLength(body.count)
+        }
+    }
     private let contentConfiguration: ContentConfiguration
 
-    public init(status: HTTPResponse.Status = .ok, headers: HTTPFields = [:], body: ByteBuffer? = nil, contentConfiguration: ContentConfiguration = .default()) {
+    public init(status: HTTPResponse.Status = .ok, headers: HTTPFields = [:], body: Response.Body, contentConfiguration: ContentConfiguration = .default()) {
         self.status = status
         self.headers = headers
         self.body = body
-        self.byteBufferAllocator = ByteBufferAllocator()
         self.contentConfiguration = contentConfiguration
     }
 }
 
 extension ClientResponse {
     private struct _ContentContainer: ContentContainer {
-        var body: Data?
+        var body: Response.Body
         var headers: HTTPFields
-        let allocator: ByteBufferAllocator
         let contentConfiguration: ContentConfiguration
 
         var contentType: HTTPMediaType? {
@@ -36,13 +34,13 @@ extension ClientResponse {
         }
 
         mutating func encode<E>(_ encodable: E, using encoder: any ContentEncoder) throws where E : Encodable {
-            var body = Data()
-            try encoder.encode(encodable, to: &body, headers: &self.headers, userInfo: [:])
-            self.body = body
+            var data = Data()
+            try encoder.encode(encodable, to: &data, headers: &self.headers, userInfo: [:])
+            self.body = .init(data: data)
         }
 
         func decode<D>(_ decodable: D.Type, using decoder: any ContentDecoder) throws -> D where D : Decodable {
-            guard let body = self.body else {
+            guard let body = self.body.data else {
                 throw Abort(.lengthRequired)
             }
             return try decoder.decode(D.self, from: body, headers: self.headers, userInfo: [:])
@@ -53,11 +51,11 @@ extension ClientResponse {
             var content = content
             try content.beforeEncode()
             try encoder.encode(content, to: &body, headers: &self.headers, userInfo: [:])
-            self.body = body
+            self.body = .init(data: body)
         }
 
         func decode<C>(_ content: C.Type, using decoder: any ContentDecoder) throws -> C where C : Content {
-            guard let body = self.body else {
+            guard let body = self.body.data else {
                 throw Abort(.lengthRequired)
             }
             var decoded = try decoder.decode(C.self, from: body, headers: self.headers, userInfo: [:])
@@ -68,11 +66,11 @@ extension ClientResponse {
 
     public var content: any ContentContainer {
         get {
-            _ContentContainer(body: body?.getData(at: 0, length: body?.readableBytes ?? 0), headers: self.headers, allocator: self.byteBufferAllocator, contentConfiguration: self.contentConfiguration)
+            _ContentContainer(body: self.body, headers: self.headers, contentConfiguration: self.contentConfiguration)
         }
         set {
             let container = (newValue as! _ContentContainer)
-            self.body = ByteBuffer(data: container.body ?? Data())
+            self.body = container.body
             self.headers = container.headers
         }
     }
@@ -82,9 +80,8 @@ extension ClientResponse: CustomStringConvertible {
     public var description: String {
         var desc = ["HTTP/1.1 \(status.code) \(status.reasonPhrase)"]
         desc += self.headers.map { "\($0.name): \($0.value)" }
-        if var body = self.body {
-            let string = body.readString(length: body.readableBytes) ?? ""
-            desc += ["", string]
+        if let body = self.body.string {
+            desc += ["", body]
         }
         return desc.joined(separator: "\n")
     }
