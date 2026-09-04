02f760f5e3f57bf024bddac060a59deac692f123
Tim <0xtimc@gmail.com>
2026-09-04 13:32:15 +0100

    Add the initial LiveClient

--- Sources/VaporTesting/TestClient.swift
+++ Sources/VaporTesting/TestClient.swift
@@ -1,5 +1,8 @@
-public import Vapor
+@testable public import Vapor
 import Foundation
+#warning("Migrate to our own SocketAddress")
+import NIOCore
+import AsyncHTTPClient
 
 public protocol TestClient: Client {
     var baseURL: URI? { get }
@@ -32,5 +35,30 @@ struct InMemoryTestClient: TestClient {
             maxBodySize: clientRequest.maxResponseBodySize,
             contentConfiguration: self.app.contentConfiguration
         )
+    }
+}
+
+struct LiveTestClient: TestClient {
+    let app: Application
+    let address: SocketAddress
+//    let options: LiveClientOptions
+    let http: HTTPClient
+
+    var port: Int { self.address.port! }
+    var baseURL: URI? {
+        URI(scheme: self.app.serverConfiguration.isTLSEnabled ? "https" : "http",
+            host: self.address.ipAddress, port: self.port, path: "/")
+    }
+    var contentConfiguration: ContentConfiguration {
+        self.app.contentConfiguration
+    }
+
+    func send(_ clientRequest: ClientRequest) async throws -> ClientResponse {
+        var request = clientRequest
+        request.url = clientRequest.url
+//        request.timeout = self.options.timeout
+
+        return try await VaporHTTPClient(http: self.http, contentConfiguration: self.contentConfiguration)
+            .send(request)
     }
 }
