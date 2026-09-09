2b2fa206ce18d63e3774c77d52d5ea83afa40f98
Tim Condon <0xTim@users.noreply.github.com>
2026-09-09 03:34:27 +0100

    [Discussion Welcome] New View Type (#3551)
    * Migrate View to Data
    
    * Migrate the tests
    
    * PR feedback

--- Sources/Vapor/View/View.swift
+++ Sources/Vapor/View/View.swift
@@ -1,22 +1,19 @@
-#warning("Make internal")
-public import NIOCore
-import NIOFoundationEssentialsCompat
 import HTTPTypes
 #if canImport(FoundationEssentials)
-import FoundationEssentials
+public import FoundationEssentials
 #else
-import Foundation
+public import Foundation
 #endif
 
 public struct View: ResponseEncodable, Sendable {
-    public var data: ByteBuffer
+    public var data: Data
 
-    public init(data: ByteBuffer) {
+    public init(data: Data) {
         self.data = data
     }
 
     public func encodeResponse(for request: Request) async throws -> Response {
-        let response = Response(headers: .init(dictionaryLiteral: (.contentType, HTTPMediaType.html.serialize())), body: .init(data: self.data.getData(at: 0, length: self.data.readableBytes) ?? Data()))
+        let response = Response(headers: .init(dictionaryLiteral: (.contentType, HTTPMediaType.html.serialize())), body: .init(data: self.data))
         return response
     }
 }
